229a7cd1706a10fa6723be9e3eb774fe54873ec0
Emilio <emilio.gamba@outlook.be>
2020-08-19 08:42:27 +0200

    Pysat: - cnf_to_pysat:     - Transform output to list of sets

--- cppy/solver_interfaces/pysat_tools.py
+++ cppy/solver_interfaces/pysat_tools.py
@@ -24,4 +24,5 @@ def cnf_to_pysat(constraints, output=None):
             raise f"ci: {ci} not handled"
         py_cnf.append(formula)
 
-    return py_cnf
+    cnf_sets = [set(clause) for clause in py_cnf]
+    return cnf_sets
