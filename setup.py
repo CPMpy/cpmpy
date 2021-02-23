9b10695fa5cc059c8be545227c42e3a0a1bb4a1c
Ahmed K.A. Abdullah <mr.antemo@gmail.com>
2021-02-23 14:47:15 +0100

    modifying version number and packaging

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 setup(
-    name='CP_Test_PY',
-    version='0.0.8',
+    name='cpmpy',
+    version='0.5.0',
     author='Tias Guns',
     author_email="tias.guns@kuleuven.be",
     description='A numpy-based light-weight Python library for conveniently modeling constraint problems in Python',
@@ -16,8 +16,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'ortools>=7.2',
-        'numpy>=1.9',
+        'ortools>=5.0',
+        'numpy>=1.5',
     ],
     #extra dependency, only needed if minizinc is to be used.
     extras_require={
