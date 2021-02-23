1120ea1e7d009ab66047aed3f458a0698b0b7e5d
Emilio <emilio.gamba@outlook.be>
2021-02-23 11:10:29 +0100

    Added version downgrade

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, Tias Guns'
 author = 'Tias Guns'
 
 # The full version, including alpha/beta/rc tags
-release = '1'
+release = '0.5'
 
 # variables to be accessed from html
 html_context = {
