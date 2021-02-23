0b3b3adff129a4159602df0061e8f13993985bbe
Tias Guns <tias@ulyssis.org>
2021-02-23 11:34:07 +0100

    ortools: make recursive conversion

--- cpmpy/globalconstraints.py
+++ cpmpy/globalconstraints.py
@@ -3,7 +3,7 @@ from .expressions import *
 # in one file for easy overview, does not include interpretation
 
 def alldifferent(variables):
-    return GlobalConstraint("alldifferent", [variables])
+    return GlobalConstraint("alldifferent", variables)
 
 def circuit(variables):
-    return GlobalConstraint("circuit", [variables])
+    return GlobalConstraint("circuit", variables)
