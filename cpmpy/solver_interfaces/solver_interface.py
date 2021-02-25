7609500ba5ce9cf499bd2f6f70a042f6addc5b73
Emilio <emilio.gamba@outlook.be>
2021-02-25 15:59:24 +0100

    SOlver interface refractoring with current implemented solvers

---
+++ cpmpy/solver_interfaces/solver_interface.py
@@ -0,0 +1,83 @@
+"""
+    ===============
+    List of classes
+    ===============
+
+    .. autosummary::
+        :nosignatures:
+
+        SolverInterface
+        SolverStats
+        ExitStatus
+
+    ==================
+    Module description
+    ==================
+    bla blabla
+
+    ==============
+    Module details
+    ==============
+
+"""
+#
+#==============================================================================
+from enum import Enum
+import time
+
+#
+#==============================================================================
+class SolverInterface:
+    """
+        Abstract class for defining solver interfaces. All classes implementing
+        the ``SolverInterface``
+    """
+    def __init__(self):
+        self.name = "dummy"
+
+    def supported(self):
+        """
+            Check for support in current system setup. Return True if the system
+            has package installed or supports solver else returns False.
+
+        Returns:
+            [bool]: Solver support by current system setup.
+        """
+        return True
+
+    def solve(self, model):
+        """
+            Build the CPMpy model into solver-supported model ready for solving
+            and returns the solver statistics generated during model solving.
+
+        :param model: CPMpy model to be parsed.
+        :type model: Model
+
+        :return: an object of :class:`SolverStats`
+        """
+        return SolverStats()
+
+#
+#==============================================================================
+class ExitStatus(Enum):
+    NOT_RUN = 1
+    OPTIMAL = 2
+    FEASIBLE = 3
+    UNSATISFIABLE = 4
+    ERROR = 5
+
+#
+#==============================================================================
+class SolverStats(object):
+    """
+        Statistics on the solved model
+    """
+    status: ExitStatus
+    runtime: time
+
+    def __init__(self):
+        self.status = ExitStatus.NOT_RUN
+        self.runtime = None
+
+    def __repr__(self):
+        return "{} ({} seconds)".format(self.status, self.runtime)
