03a1a8a2b5a66da208f279e38981d70d3135f66d
Tias Guns <tias@ulyssis.org>
2021-02-27 00:01:14 +0100

    flatten: clean up code

--- cpmpy/model_tools/flatten_model.py
+++ cpmpy/model_tools/flatten_model.py
@@ -96,160 +96,212 @@ def flatten_constraint(con):
     else:
         return [con] # TODO
 
+
+def __is_flat_var(arg):
+    """ True if the variable is a numeric constant, or a NumVarImpl (incl subclasses)
+    """
+    return is_num(arg) or isinstance(arg, NumVarImpl)
+
+
 def flatten_objective(expr):
     """
         input: expression of type:
             * is_num()
             * NumVarImpl
-            * Operator with is_type_num()
+            * Operator with not is_bool()
             * Element with len(args)==2
         output: tuple (base_expr, base_cons) with:
             base_expr one of:
                 * is_num()
                 * NumVarImpl
-                * Operator with is_type_num(), all args are: is_num() or NumVarImpl
-                * Operator 'sum', all args are: is_num() or NumVarImpl or Operator '*'[is_num(), NumVarImpl]
-            base_cons: list of flattened constraints (with flatten_constraint(con))
+                * Operator with not is_bool(), all args are: is_num() or NumVarImpl
+                * Operator 'sum', all args are: is_num() or NumVarImpl or Operator '*'[is_num(), NumVarImpl] # TODO
+            base_cons: list of additional flattened constraints
     """
-    if is_num(expr) or isinstance(expr, NumVarImpl):
+    if __is_flat_var(expr):
         return (expr, [])
 
-    basecons = []
     if isinstance(expr, Operator):
-        # only Numeric operators allowed
-        if expr.is_bool():
-            raise Exception("Boolean operator '{}' not allowed in objective".format(expr.name)) # or bug
+        assert(not expr.is_bool()) # only non-logic operators allowed
 
-        flat_args = [flatten_numexpr(e) for e in expr.args]
-        if all(arg is flatarg[0] for (arg,flatarg) in zip(expr.args, flat_args)):
+        if all(__is_flat_var(arg) for arg in expr.args):
             return (expr, [])
         else:
-            # one of the args was changed
-            newargs = [flatarg[0] for flatarg in flat_args]
-            new_expr = Operator(expr.name, newargs)
-            return (new_expr, [c for flatarg in flat_args for c in flatarg[1]])
+            # one of the arguments is not flat, flatten all
+            flatvars, flatcons = zip(*[flatten_numexpr(arg) for arg in expr.args])
+            newexpr = Operator(expr.name, flatvars)
+            return (newexpr, [c for con in flatcons for c in con])
 
     elif isinstance(expr, Element):
-        if len(expr.args) != 2:
-            raise Exception("Only Element expr of type Arr[Var] allowed in objective".format(expr.name))
+        assert(len(expr.args) == 2) # Arr[Idx]
         return flatten_numexpr(expr)
 
     raise Exception("Expression '{}' not allowed in objective".format(expr)) # or bug
+
         
 def flatten_numexpr(expr):
     """
         input: expression of type:
             * is_num()
             * NumVarImpl
-            * Operator with is_type_num()
+            * Operator with not is_bool()
             * Element with len(args)==2
         output: tuple (base_expr, base_cons) with:
             base_expr one of:
                 * is_num()
                 * NumVarImpl
-                * Operator with is_type_num(), all args are: is_num() or NumVarImpl
-                * Operator 'sum', all args are: is_num() or NumVarImpl or Operator '*'[is_num(), NumVarImpl]
             base_cons: list of flattened constraints (with flatten_constraint(con))
     """
-    if is_num(expr) or isinstance(expr, NumVarImpl):
+    if __is_flat_var(expr):
         return (expr, [])
-    args = expr.args
+
     if isinstance(expr, Operator):
-        # only Numeric operators allowed
-        # unary int: '-', 'abs'
-        # binary int: 'sub', 'mul', 'div', 'mod', 'pow'
-        # nary int: 'sum'
-        if expr.is_bool():
-            raise Exception("Operator '{}' not allowed as numexpr".format(expr.name))
+        assert(not expr.is_bool()) # only non-logic operators allowed
 
-        # TODO: actually need to flatten THIS expression (and recursively the arguments)
-        
-        arity = Operator.allowed[expr.name][0]
-        # unary int op
-        if arity == 1:
-            var, bcon = flatten_numexpr(args[0])
-            basecons = [bcon]
-            if expr.name == 'abs':
-                ivar = IntVarImpl(0, var.ub)
-            else: # '-'
-                ivar = IntVarImpl(-var.ub, -var.lb)
+        flatvars, flatcons = zip(*[flatten_numexpr(arg) for arg in expr.args])
+        lbs = [var.lb if isinstance(var, NumVarImpl) else var for var in flatvars]
+        ubs = [var.ub if isinstance(var, NumVarImpl) else var for var in flatvars]
 
-            basecons = [Operator(expr.name, [var]) == ivar]
-            return ivar, basecons
+        if expr.name == 'abs': # unary
+            ivar = IntVarImpl(0, ubs[0])
+        elif expr.name == '-': # unary
+            ivar = IntVarImpl(-ubs[0], -lbs[0])
+        elif expr.name == 'mul': # binary
+            ivar = IntVarImpl(lbs[0] * lbs[1], ubs[0] * ubs[1]) 
+        elif expr.name == 'div': # binary
+            ivar = IntVarImpl(lbs[0] // ubs[1], ubs[0] // lbs[1] )
+        elif expr.name == 'mod': # binary 
+            ivar = IntVarImpl(0, ubs[0])
+        elif expr.name == 'pow': # binary
+            ivar = IntVarImpl(lbs[0] ** lbs[1], ubs[0] ** ubs[1])
+        elif expr.name == 'sum': # n-ary
+            ivar = IntVarImpl(sum(lbs), sum(ubs)) 
+        else:
+            raise Exception("Operator '{}' not known in numexpr".format(expr.name)) # or bug
 
-        # binary int op 
-        elif arity == 2:
-            var1, bcon1 = flatten_numexpr(args[0])
-            var2, bcon2 = flatten_numexpr(args[1])
-            basecons = bcon1 + bcon2
-            lb = ([var.lb if isinstance(var, NumVarImpl) else var for var in [var1,var2]]) 
-            ub = ([var.ub if isinstance(var, NumVarImpl) else var for var in [var1,var2]])
-            if expr.name == 'mul': 
-                ivar = IntVarImpl(lb[0] * lb[1], ub[0] * ub[1]) 
-            elif expr.name == 'div':
-                ivar = IntVarImpl(lb[0] // ub[1], ub[0] // lb[1] )
-            elif expr.name == 'mod': 
-                ivar = IntVarImpl(0, ub[0])
-            elif expr.name == 'pow':
-                ivar = IntVarImpl(lb[0] ** lb[1], ub[0] ** lb[1])
-            
-            basecons += [Operator(expr.name, [var1, var2]) == ivar]
-            return ivar, basecons
-
-        else: # arity > 2 (sum)
-            varrs, bcons = zip(*[flatten_numexpr(arg) for arg in args])
-            basecons = [c for con in bcons for c in con] # flatten
-            lb = sum([var.lb if isinstance(var, NumVarImpl) else var for var in varrs]) 
-            ub = sum([var.ub if isinstance(var, NumVarImpl) else var for var in varrs])
-            ivar = IntVarImpl(lb, ub) 
-
-            return (ivar, [Operator('sum', varrs) == ivar]+basecons)
+        newexpr = (Operator(expr.name, flatvars) == ivar)
+        return (ivar, [newexpr]+[c for con in flatcons for c in con])
 
     elif isinstance(expr, Element):
-        if len(expr.args) != 2:
-            raise Exception("Only Element expr of type Arr[Var] allowed in objective".format(expr.name))
-        arr = expr.args[0]
-        idx = expr.args[1]
-        if __is_flatten_var(idx) and \
-           all(__is_flatten_var(ai) for ai in arr):
-            # expression is flat
-            lb = min([var.lb if isinstance(var, NumVarImpl) else var for var in arr]) 
-            ub = max([var.ub if isinstance(var, NumVarImpl) else var for var in arr])
-            ivar = IntVarImpl(lb, ub)
-            return (ivar, [Element([arr, idx, ivar])]) # need to make new one as 'expr == ivar' would overwrite expr
-        else:
-            # one of the args was changed
-            new_idx, icons = flatten_numexpr(idx)
-            new_arr, acons = zip(*[flatten_numexpr(e) for e in arr])
+        assert(len(expr.args) == 2) # Arr[Idx]
 
-            lb = min([var.lb if isinstance(var, NumVarImpl) else var for var in new_arr]) 
-            ub = max([var.ub if isinstance(var, NumVarImpl) else var for var in new_arr])
-            ivar = IntVarImpl(lb, ub)
-            return (ivar, [Element([new_arr, new_idx, ivar])]+icons+[c for con in acons for c in con])
+        arr,idx = expr.args
+        basecons = []
+        if not __is_flat_var(idx) or \
+           any(not __is_flat_var(ai) for ai in arr):
+            # one of the arguments is not flat, flatten all
+            idx, icons = flatten_numexpr(idx)
+            arr, acons = zip(*[flatten_numexpr(e) for e in arr])
+            basecons = icons+[c for con in acons for c in con]
+            
+        lb = min([var.lb if isinstance(var, NumVarImpl) else var for var in arr]) 
+        ub = max([var.ub if isinstance(var, NumVarImpl) else var for var in arr])
+        ivar = IntVarImpl(lb, ub)
+
+        newexpr = Element([arr, idx, ivar])
+        return (ivar, [newexpr]+basecons)
 
     raise Exception("Operator '{}' not allowed as numexpr".format(expr.name)) # or bug
 
 
-def __is_flatten_var(arg):
-    return is_num(arg) or isinstance(arg, NumVarImpl)
+def flatten_boolexpr(expr):
+    """
+        input: expression of type:
+            * True/False
+            * BoolVar
+            * Operator with is_bool()
+            * Comparison
+        output: tuple (base_expr, base_cons) with:
+            base_expr one of:
+                * True/False
+                * BoolVar
+            base_cons: list of flattened constraints (with flatten_constraint(con))
+    """
+    if __is_flat_var(expr):
+        return (expr, [])
+
+    if isinstance(expr, Operator):
+        assert(expr.is_bool()) # and, or, xor, ->
+
+        # apply De Morgan's transform for "implies"
+        if expr.name is '->':
+            return flatten_boolexpr(~args[0] | args[1])
+
+        bvar = BoolVarImpl()
+        if all(__is_flat_var(arg) for arg in expr.args):
+            return (bvar, [expr == bvar])
+        else:
+            # recursively flatten all children, which are boolexpr
+            flatvars, flatcons = zip(*[flatten_boolexpr(arg) for arg in expr.args])
+
+            newexpr = (Operator(expr.name, flatvars) == bvar)
+            return (bvar, [newexpr]+[c for con in flatcons for c in con])
+
+    if isinstance(expr, Comparison):
+        bvar = BoolVarImpl()
+        if all(__is_flat_var(arg) for arg in expr.args):
+            return (bvar, [expr == bvar])
+        else:
+            # TODO: special case of <expr> == 0
+            #bvar = args[0] if __is_flat_var(args[0]) else args[1]
+            #base_cons = [ __check_or_flip_base_const(subexpr)]
+
+            # recursively flatten children, which may be boolexpr or numexpr
+            # TODO: actually one side can be a linexpr, then sufficient to flatten_objective...
+            # e.g. flatten_boolexpr(a + b > c) does not need splitting a+b away
+            (var0, bco0) = flatten_subexpr(expr.args[0])
+            (var1, bco1) = flatten_subexpr(expr.args[1])
+
+            newexpr = (Comparison(expr.name, var0, var1) == bvar)
+            return (bvar, [newexpr]+bco0+bco1)
+
+
+def flatten_subexpr(expr):
+    """
+        can be both nested boolexpr or linexpr
+    """
+    if __is_numexpr(expr):
+        return flatten_numexpr(expr)
+    else:
+        return flatten_boolexpr(expr)
+
+def __is_numexpr(expr):
+    """
+        True if:
+            * is_num()
+            * NumVarImpl
+            * Operator with not is_bool()
+            * Element with len(args)==2
+    """
+    if __is_flat_var(expr):
+        return True
+    if isinstance(expr, Operator) and not expr.is_bool():
+        return True
+    if isinstance(expr, Element) and len(expr.args) == 2:
+        return True
+    return False
+
+    
+
+######## Unused code, detects linexp and swap Comparison so Operator is on left side...
 
 def __is_nested_linexpr(subexpr):
-    if __is_flatten_var(subexpr):
+    if __is_flat_var(subexpr):
         return False
     if isinstance(subexpr, Operator):
         # extend definition to consider Arithmetic operator expression as valid
-        return subexpr.name is 'sum' and not all([__is_flatten_var(arg) for arg in subexpr.args])
-    return not all([__is_flatten_var(arg) for arg in subexpr.args])
+        return subexpr.name is 'sum' and not all([__is_flat_var(arg) for arg in subexpr.args])
+    return not all([__is_flat_var(arg) for arg in subexpr.args])
 
 def __check_lincons(subexpr):
     LHS, RHS = subexpr.args
     # LHS or RHS is a Var (IntVar, BoolVar, Num)
-    if not (__is_flatten_var(LHS) or __is_flatten_var(RHS)):
+    if not (__is_flat_var(LHS) or __is_flat_var(RHS)):
         return False 
     
     # LHS is a Var :: RHS does not have nested expr
-    elif __is_flatten_var(LHS):
-        if __is_flatten_var(RHS):
+    elif __is_flat_var(LHS):
+        if __is_flat_var(RHS):
             return True 
         # RHS is an LinExp and that's ok
         # as long as it does not have more nested expression 
@@ -259,8 +311,8 @@ def __check_lincons(subexpr):
 
     # Same logic if RHS is a Var
     #TODO: Refactor to avoid code dupcliation
-    elif __is_flatten_var(RHS):
-        if __is_flatten_var(LHS):
+    elif __is_flat_var(RHS):
+        if __is_flat_var(LHS):
             return True 
         if any([__is_nested_linexpr(arg) for arg in LHS.args]):
             return False
@@ -270,88 +322,9 @@ def __check_lincons(subexpr):
 
 def __check_or_flip_base_const(subexpr):
     assert __check_lincons(subexpr)
-    if __is_flatten_var(subexpr.args[1]):
+    if __is_flat_var(subexpr.args[1]):
         return subexpr
     # flip the base constraint to have 
     # BoolExp == boolvar format only 
     return Comparison(subexpr.name, subexpr.args[1], subexpr.args[0])
-
-def flatten_boolexpr(subexpr):
-    """
-        input: expression of type:
-            * True/False
-            * BoolVar
-            * Operator with is_type_bool()
-            * Comparison
-        output: tuple (base_expr, base_cons) with:
-            base_expr one of:
-                * True/False
-                * BoolVar
-            base_cons: list of flattened constraints (with flatten_constraint(con))
-    """
-
-    if isinstance(subexpr, BoolVarImpl) or isinstance(subexpr, bool):
-        # base case: single boolVar
-        return (subexpr, [])
-
-    if isinstance(subexpr, Comparison):
-        args = subexpr.args
-        if __is_flatten_var(args[0]) and __is_flatten_var(args[1]):
-            # base constraint, no need to flatten children
-            bvar = BoolVarImpl()
-            return (bvar, [subexpr == bvar])
-        else:
-            # TODO: special case of <expr> == 0
-            #bvar = args[0] if __is_flatten_var(args[0]) else args[1]
-            #base_cons = [ __check_or_flip_base_const(subexpr)]
-
-            # recursively flatten children, which may be boolexpr or numexpr
-            (var0, bco0) = check_or_flatten_subexpr(args[0])
-            (var1, bco1) = check_or_flatten_subexpr(args[1])
-            newcomp = Comparison(subexpr.name, var0, var1)
-            bvar = BoolVarImpl()
-            return (bvar, [newcomp == bvar]+bco0+bco1)
-
-    elif isinstance(subexpr, Operator):
-        assert(subexpr.is_bool()) # and, or, xor, ->
-
-        # apply De Morgan's transform for "implies"
-        if subexpr.name is '->':
-            return flatten_boolexpr(~args[0] | args[1])
-
-        args = subexpr.args
-        if isinstance(args[0], BoolVarImpl) and isinstance(args[1], BoolVarImpl):
-            bvar = BoolVarImpl()
-            return (bvar, [subexpr == bvar])
-        else:
-            # recursively flatten all children, which are boolexpr
-            flatres = [flatten_boolexpr(arg) for arg in args]
-            flat_vars = [v for (v,_) in flatres]
-            flat_cons = [c for (_,cons) in flatres for c in cons] # flatten
-            newop = Operator(subexpr.name, flat_vars)
-            bvar = BoolVarImpl()
-            return (bvar, [newop == bvar]+flat_cons)
-
-def check_or_flatten_subexpr(subexpr):
-    """
-        can be both nested boolexpr or linexpr
-    """
-    if isinstance(subexpr, BoolVarImpl) or isinstance(subexpr, bool):
-        # base case: single boolVar
-        return (subexpr, [])
-
-    if __is_numexpr(subexpr):
-        return flatten_numexpr(subexpr)
-    else:
-        return flatten_boolexpr(subexpr)
-
-def __is_numexpr(expr):
-    if isinstance(expr, Operator) and not expr.is_bool():
-        return True
-    if isinstance(expr, Element) and len(expr.args) == 2:
-        return True
-    return False
-
-    
-
     
