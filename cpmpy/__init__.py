8ba14f409e8faef40a2cf29bbda8b1db97b78a3d
Ahmed K.A. Abdullah <mr.antemo@gmail.com>
2021-02-26 16:05:10 +0100

    Release 0.5.3: Updating the version and supported python versions

--- cpmpy/__init__.py
+++ cpmpy/__init__.py
@@ -1,10 +1,11 @@
 # Tias Guns, 2019
 # current version
 #==============================================================================
-VERSION = (0, 5, 2, "dev")
+# VERSION = (0, 5, 2, "dev")
 
-__version__ = "%d.%d.%d.%s" % VERSION if len(VERSION) == 4 else \
-              "%d.%d.%d" % VERSION
+# __version__ = "%d.%d.%d.%s" % VERSION if len(VERSION) == 4 else \
+#               "%d.%d.%d" % VERSION
+__version__ = "0.5.3"
 
 from .variables import *
 from .expressions import *
