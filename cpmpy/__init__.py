ec82ad1fd6140ca0d2ab7c073ba12f6d20c3c126
Emilio <emilio.gamba@outlook.be>
2021-02-26 15:57:26 +0100

    Further documentation of main

--- cpmpy/__init__.py
+++ cpmpy/__init__.py
@@ -3,6 +3,9 @@
 #==============================================================================
 VERSION = (0, 5, 2, "dev")
 
+__version__ = "%d.%d.%d.%s" % VERSION if len(VERSION) == 4 else \
+              "%d.%d.%d" % VERSION
+
 from .variables import *
 from .expressions import *
 from .globalconstraints import *
