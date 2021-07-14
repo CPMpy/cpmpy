e9bb31b760852932305708514adbdb16df1ed719
Tias Guns <tias@ulyssis.org>
2021-07-14 14:29:39 +0200

    solvers module init

--- cpmpy/solvers/__init__.py
+++ cpmpy/solvers/__init__.py
@@ -1,1 +1,12 @@
-__all__ = ['solver_interface', 'minizinc', 'minizinc_text', 'ortools', 'utils']
+"""
+    CPMpy interfaces to (the Python API interface of) solvers
+
+    Solvers typically use some of the generic transformations in
+    `transformations` as well as specific reformulations to map the
+    CPMpy expression to the solver's Python API
+"""
+
+from .utils import builtin_solvers, get_supported_solvers
+from .ortools import CPM_ortools
+from .pysat import CPM_pysat
+# from minizinc import CPMpyMiniZinc # closed for maintenance
