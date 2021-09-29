17fd8710f0b2e78c6adfee9f0117c528d6e32a1b
CryoCardiogram <menalewa@live.com>
2021-09-29 18:03:06 +0200

    hybrid2

--- examples/advanced/visual_sudoku.py
+++ examples/advanced/visual_sudoku.py
@@ -5,48 +5,77 @@ Visual Sudoku problem in CPMpy
 from cpmpy import * 
 import numpy as np 
 from scipy.stats import poisson
+from cpmpy.solvers.ortools import CPM_ortools
+from cpmpy.solvers.solver_interface import ExitStatus
 
-def solve_vizsudoku_cppy(logprobs, is_given, baseline=False):
-        n = len(is_given)
-        b = np.sqrt(n).astype(int)
+PRECISION = 1e-3
 
-        scale = 1e-3
+def sudoku_model(grid):
+    n = len(grid)
+    b = np.sqrt(n).astype(int)
 
-        # decision vars
-        puzvar = IntVar(1,n, shape=(n,n), name='cell')
-        
-        # alldiff constraint
-        constraint = []
-        constraint += [alldifferent(row) for row in puzvar]
-        constraint += [alldifferent(col) for col in puzvar.T]
-        for i in range(0,n,b):
-            for j in range(0,n,b):
-                constraint += [alldifferent(puzvar[i: i +b, j:j+b])]
+    # decision vars
+    puzvar = IntVar(1,n, shape=grid.shape, name='cell')
+    
+    # alldiff constraint
+    constraints = []
+    constraints += [alldifferent(row) for row in puzvar]
+    constraints += [alldifferent(col) for col in puzvar.T]
+    for i in range(0,n,b):
+        for j in range(0,n,b):
+            constraints += [alldifferent(puzvar[i: i +b, j:j+b])]
+    
+    return puzvar, constraints 
+
+
+def solve_vizsudoku_baseline(puzvar, constraints, logprobs, is_given):
+        #puzvar, constraints = sudoku_model(is_given)
 
         # Baseline: take most likely digit as deterministic input
-        if baseline:
-            givens = np.argmax(logprobs, axis=2)
-            constraint += [puzvar[givens>0] == givens[givens>0]]
-            model = Model(constraint)
-        else:
-            # objective function: max log likelihood of all digits
-            logprobs = np.array(-logprobs/scale).astype(int)
-            #TODO need for broadcast? line would look like:
-            #obj = sum(Element([logprobs[is_given], puzvar[is_given]]))
-            #obj = sum(Element([logprobs[i,j], puzvar[i,j]]) for i in range(n) for j in range(n) if is_given[i,j])
-            terms = []
-            for i in range(n):
-                for j in range(n): 
-                    if is_given[i,j]:
-                        terms.append(Element(logprobs[i,j], puzvar[i,j]))
-            obj = sum(terms)
-            model = Model(constraint, minimize=obj)
-        
-        #print(model)
+        givens = np.argmax(logprobs, axis=2)
+        constraints += [puzvar[is_given] == givens[is_given]]
+        model = Model(constraints)
         if model.solve():
             return puzvar.value()
         else:
             return np.zeros_like(puzvar)
+
+
+def solve_vizsudoku_hybrid1(puzvar, constraints, logprobs, is_given):
+    #puzvar, constraints = sudoku_model(is_given)
+    # objective function: max log likelihood of all digits
+    lprobs = np.array(-logprobs/PRECISION).astype(int)
+    obj = sum(Element(lp, v) for lp,v in zip(lprobs[is_given], puzvar[is_given]))
+    #obj = sum(Element(logprobs[i,j], puzvar[i,j]) for i in range(n) for j in range(n) if is_given[i,j])
+    model = Model(constraints, minimize=obj)
+
+    if model.solve():
+            return puzvar.value()
+    else:
+        return np.zeros_like(puzvar)
+
+def has_unique_solution(solution, is_given):
+    puzvar, constraints = sudoku_model(solution)
+    constraints += [puzvar[is_given] == solution[is_given]]
+    # forbid current solution 
+    constraints += [puzvar != solution]
+    model= CPM_ortools(Model(constraints))
+    return model.solve(stop_after_first_solution=True) == ExitStatus.UNSATISFIABLE
+
+
+def solve_vizsudoku_hybrid2(puzvar, constraints, logprobs, is_given, max_iter=100):
+    #puzvar, constraints = sudoku_model(is_given)
+    solution = solve_vizsudoku_hybrid1(puzvar, constraints, logprobs, is_given)
+    i = 0
+    while not has_unique_solution(solution, is_given):
+        if i == max_iter:
+            break 
+        # forbid current solution
+        constraints += [puzvar[is_given>0] != solution[is_given>0]]
+        solution = solve_vizsudoku_hybrid1(puzvar, constraints, logprobs, is_given)
+        i += 1
+    print(i)
+    return solution
 
 
 def cnn_output_simulation(puzzle):
@@ -76,8 +105,17 @@ if __name__ == '__main__':
     is_given = puzzle > 0
     probs = cnn_output_simulation(puzzle) 
     probs[puzzle==0][0] = 0.8
-    logprobs = np.log(probs)
+    logprobs = np.log(np.maximum(probs, PRECISION))
     print(logprobs[0,3])
 
-    sol4 = solve_vizsudoku_cppy(logprobs, is_given)
-    print('cpmpy solution', sol4,sep='\n')
+
+    dvar, cons = sudoku_model(puzzle)
+
+    sol1 = solve_vizsudoku_baseline(dvar, cons,logprobs, is_given)
+    print('vizsudoku baseline solution', sol1,sep='\n')
+
+    sol2 = solve_vizsudoku_hybrid1(dvar, cons,logprobs, is_given)
+    print('vizsudoku hybrid1 solution', sol2,sep='\n')
+
+    sol3 = solve_vizsudoku_hybrid2(dvar, cons,logprobs, is_given)
+    print('vizsudoku hyrbid2 solution', sol3,sep='\n')
