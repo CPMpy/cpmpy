7b7004b1fd250f39d83d53ddb88c9357822be788
CryoCardiogram <menalewa@live.com>
2021-10-01 18:10:40 +0200

    visual sudoku example

--- examples/advanced/visual_sudoku.py
+++ examples/advanced/visual_sudoku.py
@@ -14,11 +14,10 @@ from cpmpy.solvers.solver_interface import ExitStatus
 
 import matplotlib.pyplot as plt
 
-PRECISION = 1e-3
+PRECISION = 1e-4
 
 
 
-#%%
 def sudoku_model(grid):
     n = len(grid)
     b = np.sqrt(n).astype(int)
@@ -38,7 +37,6 @@ def sudoku_model(grid):
 
 
 def solve_vizsudoku_baseline(puzvar, constraints, logprobs, is_given):
-        #puzvar, constraints = sudoku_model(is_given)
         cons = [*constraints]
         # Baseline: take most likely digit as deterministic input
         givens = np.argmax(logprobs, axis=2)
@@ -49,13 +47,11 @@ def solve_vizsudoku_baseline(puzvar, constraints, logprobs, is_given):
         else:
             return np.zeros_like(puzvar)
 
-
 def solve_vizsudoku_hybrid1(puzvar, constraints, logprobs, is_given):
     #puzvar, constraints = sudoku_model(is_given)
     # objective function: max log likelihood of all digits
     lprobs = np.array(-logprobs/PRECISION).astype(int)
     obj = sum(Element(lp, v) for lp,v in zip(lprobs[is_given], puzvar[is_given]))
-    #obj = sum(Element(logprobs[i,j], puzvar[i,j]) for i in range(n) for j in range(n) if is_given[i,j])
     model = Model(constraints, minimize=obj)
 
     if model.solve():
@@ -69,7 +65,7 @@ def is_unique(solution, is_given):
     # forbid current solution 
     constraints += [any((puzvar != solution).flatten())] #FIXME auto-flatten 2d dvar arrays?
     model= CPM_ortools(Model(constraints))
-    return model.solve(stop_after_first_solution=True) == ExitStatus.UNSATISFIABLE
+    return not model.solve(stop_after_first_solution=True)
 
 
 def solve_vizsudoku_hybrid2(puzvar, constraints, logprobs, is_given, max_iter=10):
@@ -88,29 +84,13 @@ def solve_vizsudoku_hybrid2(puzvar, constraints, logprobs, is_given, max_iter=10
     return solution
 
 
-def cnn_output_simulation(puzzle):
-    """
-    Poisson distribution probability tensor containing a probability 
-    score for each possible value in each cell.
-    """
-    probs = np.zeros(shape=(puzzle.shape + (len(puzzle)+ 1,) ))
-    values = np.arange(len(puzzle)+ 1) 
-    for r in range(len(puzzle)):
-        for c in range(len(puzzle[0])):
-            pss = poisson.pmf(values,puzzle[r,c]+1)
-            pss[puzzle[r,c]] += 0.01 # break equal probs
-            probs[r,c] = pss/np.sum(pss)
-    return probs
-
 # %%
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5,), (0.5,))])
 
 # Download and load the MNIST data
 testset = datasets.MNIST('.', download=True, train=False, transform=transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True)
-
-# %%
+#testloader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True)
 digit_indices = {k:torch.LongTensor(*np.where(testset.targets == k)) for k in range(1,10)}
 
 # sample a dataset index for each non-zero number
@@ -123,68 +103,12 @@ def sample_visual_sudoku(sudoku_p):
         vizsudoku[val_idx] = torch.stack([testset[i][0] for i in idx])
     return vizsudoku
 
-puzzle = np.array(
-        [[0,0,0, 2,0,5, 0,0,0],
-        [0,9,0, 0,0,0, 7,3,0],
-        [0,0,2, 0,0,9, 0,6,0],
-        [2,0,0, 0,0,0, 4,0,9],
-        [0,0,0, 0,7,0, 0,0,0],
-        [6,0,9, 0,0,0, 0,0,1],
-        [0,8,0, 4,0,0, 1,0,0],
-        [0,6,3, 0,0,0, 0,8,0],
-        [0,0,0, 6,0,8, 0,0,0]]
-    )
-
-# %%
-def show_grid_img(images):
-    dim = 9
-    #figure = plt.figure()
-    num_of_images = dim*dim
-    for index in range(num_of_images):
-        plt.subplot(dim, dim, index+1)
-        plt.axis('off')
-        plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
-vs = sample_visual_sudoku(puzzle)
-
-show_grid_img(vs.reshape(81, 1, 28,28))
-
-# %%
-imgs_supply = {k:testset.data[digit_indices[k]] for k in range(1,10)}
-##helper function to plot and compare solution found with hybrid approach
-def plot_vs(visualsudoku, output, is_given, ml_digits, solution):
-    n = 9
-    fig, axes = plt.subplots(n, n, figsize=(1.5*n,2*n))
-
-    for i in range(n*n):
-        ax = axes[i//n, i%n]
-        # sample image wrt solver output
-        img = torch.zeros(28,28).float()
-        c = 'gray'
-        if not is_given.reshape(-1)[i]:
-            # cell filled by the solver in gray
-            img = imgs_supply[output.reshape(-1)[i]][0]
-        else:
-            img = visualsudoku.view(-1, 28,28)[i].squeeze()
-            # wrong given -> red
-            # given fixed by cp -> green
-            c = 'gray' if output.reshape(-1)[i] == ml_digits.reshape(-1)[i] else 'summer'
-
-        c = 'autumn' if is_given.reshape(-1)[i] and output.reshape(-1)[i] != solution.reshape(-1)[i] else c
-
-        if c == 'summer':
-            ax.set_title('ML label: {}\nsolver label: {}'.format(ml_digits.reshape(-1)[i], output.reshape(-1)[i]))
-        elif c == 'autumn':
-            ax.set_title('solver label: {}\nTrue label: {}'.format(output.reshape(-1)[i], solution.reshape(-1)[i]))
-            
-        ax.imshow(img, cmap=c)
-        ax.set_axis_off()
 
 
-# %%
 from torch import nn 
 import torch.nn.functional as F
 class LeNet(nn.Module):
-    def __init__(self, calibrated=False):
+    def __init__(self):
         super(LeNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5, 1, padding=2)
         self.conv2 = nn.Conv2d(6, 16, 5, 1)
@@ -216,24 +140,7 @@ def predict_proba_sudoku(model, vizsudoku):
     # our NN return 81 probabilistic vector: an 81x10 matrix
     return pred.reshape(9,9,10).detach() # reshape as 9x9x10 tensor for easier visualisation
 
-model = load_clf(LeNet,'lenet_mnist_e15.pt' )
-# (log)probabilities for each cell
-logprobs = predict_proba_sudoku(model, vs)
-is_given = puzzle > 0
-# maximum likelihood class 
-ml_digits = np.argmax(logprobs, axis=-1)
-dvar, cons = sudoku_model(puzzle)
-sol3 = solve_vizsudoku_hybrid2(dvar, cons,logprobs, is_given)
-
-plot_vs(vs, sol3, is_given, ml_digits, puzzle)
-
-# %%
-
-
-# %%
-
-if __name__ == '__main__':
-    puzzle = np.array(
+puzzle = np.array(
         [[0,0,0, 2,0,5, 0,0,0],
         [0,9,0, 0,0,0, 7,3,0],
         [0,0,2, 0,0,9, 0,6,0],
@@ -244,24 +151,25 @@ if __name__ == '__main__':
         [0,6,3, 0,0,0, 0,8,0],
         [0,0,0, 6,0,8, 0,0,0]]
     )
-    is_given = puzzle > 0
-    probs = cnn_output_simulation(puzzle) 
-    # add some noise 
-    print('truth',np.argmax(probs, axis=-1))
-    probs[0,3], probs[8,5] = probs[8,5], probs[0,3]
-    probs[puzzle==0][0] = 0.8
-    logprobs = np.log(np.maximum(probs, PRECISION))
-    print(np.argmax(logprobs, axis=-1))
 
 
+vs = sample_visual_sudoku(puzzle)
 
-    dvar, cons = sudoku_model(puzzle)
+model = load_clf(LeNet,'lenet_mnist_e15.pt' )
+# (log)probabilities for each cell
+logprobs = predict_proba_sudoku(model, vs)
+is_given = puzzle > 0
+# maximum likelihood class 
+ml_digits = np.argmax(logprobs, axis=-1)
+dvar, cons = sudoku_model(puzzle)
+sol3 = solve_vizsudoku_hybrid2(dvar, cons,logprobs, is_given)
+cons += [dvar[is_given] == puzzle[is_given]]
+model = Model(cons) 
+model.solve()
+solution = dvar.value()
 
-    sol1 = solve_vizsudoku_baseline(dvar, cons,logprobs, is_given)
-    print('vizsudoku baseline solution', sol1,sep='\n')
+print('True solution', *solution)
+print('hybrid model output', *sol3)
 
-    sol2 = solve_vizsudoku_hybrid1(dvar, cons,logprobs, is_given)
-    print('vizsudoku hybrid1 solution', sol2,sep='\n')
+# %%
 
-    sol3 = solve_vizsudoku_hybrid2(dvar, cons,logprobs, is_given)
-    print('vizsudoku hyrbid2 solution', sol3,sep='\n')
