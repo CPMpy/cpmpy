cc68653c10b54db1763cefa9c4bbbbb2f3da9437
Tias Guns <tias.guns@kuleuven.be>
2021-10-03 20:41:03 +0200

    npuzzle: simplify considerably with state-space ideas from lpcp21_p1_frog

--- examples/npuzzle.py
+++ examples/npuzzle.py
@@ -2,14 +2,15 @@
 """
 N-puzzle problem in CPMpy
 
-Reworked based on Hakan Kjellerstrand's
+Based on the state-space approach of 'lpcp21_p1_frog',
+originally motivated by Hakan Kjellerstrand's
 https://github.com/hakank/hakank/blob/master/minizinc/n_puzzle.mzn
 
 A typical children toy, where a picture is divided in n+1 blocks and mangled,
 and the goal is to trace the steps to the original picture.
 
 It is a typical planning problem, here solved with CSP over a finite
-planning horizon of `num_steps` steps.
+planning horizon of `N` steps.
 """
 
 # load the libraries
@@ -22,103 +23,90 @@ from cpmpy import *
 puzzle_start = np.array([
     [0,3,6],
     [2,4,8],
-    [1,7,5]]).reshape(-1) # 12 steps
+    [1,7,5]]) # 13 steps
 """
 puzzle_start = np.array([
     [3,7,5],
     [1,6,4],
-    [8,2,0]]).reshape(-1) # 18 steps
+    [8,2,0]]) # 19 steps
 puzzle_start = np.array([
     [8,3,4],
     [1,7,6],
-    [2,5,0]]).reshape(-1) # 24 steps
+    [2,5,0]]) # 25 steps
 puzzle_start = np.array([
     [8,1,0],
     [3,4,7],
-    [2,5,6]]).reshape(-1) # 28 steps
+    [2,5,6]]) # 29 steps
 """
 puzzle_end = np.array([
     [1,2,3],
     [4,5,6],
-    [7,8,0]]).reshape(-1)
-n = puzzle_start.shape[0] # flat length
-dim = int(math.sqrt(n))
-assert (dim*dim == n), "non-square matrix??"
+    [7,8,0]])
 
-# Generate the allowed moves (including to same position)
-# such that (move_from, move_to) in valid_table
-valid_table = []
-def pos(r,c):
-    return r*dim+c
-for r in range(dim):
-    for c in range(dim):
-        # same, up, down, left, right
-        for (rr, cc) in [(0,0),(-1,0),(1,0),(0,-1),(0,1)]: 
-            if 0 <= r+rr and r+rr < dim and 0 <= c+cc and c+cc < dim:
-                valid_table.append([pos(r,c), pos(r+rr,c+cc)])
+def n_puzzle(puzzle_start, puzzle_end, N):
+    # max nr steps to the solution
+    print("Max steps:", N)
+    m = Model()
 
+    (dim,dim2) = puzzle_start.shape
+    assert (dim == dim2), "puzzle needs square shape"
+    n = dim*dim2 - 1 # e.g. an 8-puzzle
 
-# max nr steps to the solution
-num_steps = 30
+    # State of puzzle at every step
+    x = intvar(0,n, shape=(N,dim,dim), name="x")
 
-# Variables
-x = intvar(0,n-1, shape=(num_steps,n), name="x")
+    # Start state constraint
+    m += (x[0] == puzzle_start)
 
-# the moves, index in puzzle
-move_from = intvar(0,n-1, shape=(num_steps), name="move_from") # is move_to[i-1]
-move_to = intvar(0,n-1, shape=(num_steps), name="move_to")
+    # End state constraint
+    m += (x[-1] == puzzle_end)
 
-# is this row the solution?
-check = boolvar(shape=num_steps, name="check")
+    # define neighbors = allowed moves for the '0'
+    def neigh(i,j):
+        # same, left,right, down,up, if within bounds
+        for (rr, cc) in [(0,0),(-1,0),(1,0),(0,-1),(0,1)]:
+            if 0 <= i+rr and i+rr < dim and 0 <= j+cc and j+cc < dim:
+                yield (i+rr,j+cc)
 
-# index of first solution
-check_ix = intvar(0,num_steps, name="check_ix")
+    # Transition: define next based on prev + invariants
+    def transition(m, prev_x, next_x):
+        # for each position, determine its reachability
+        for i in range(dim):
+            for j in range(dim):
+                m += (next_x[i,j] == 0).implies(any(prev_x[r,c] == 0 for r,c in neigh(i,j)))
 
+        # Invariant: in each step, all cells are different
+        m += AllDifferent(next_x)
 
-m = Model(minimize=check_ix)
+        # Invariant: only the '0' position can move
+        m += ((prev_x == 0) | (next_x == 0) | (prev_x == next_x))
 
-def same(x, y):
-    return all(x == y)
+    # apply transitions (0,1) (1,2) (2,3) ...
+    for i in range(1, N):
+        transition(m, x[i-1], x[i])
 
-# start and end puzzle
-m += [
-    same(x[0, :], puzzle_start),
-    same(x[num_steps-1, :], puzzle_end),
-]
+    return (m,x)
 
-# in each puzzle, all cells different
-m += [ AllDifferent(x[i,:]) for i in range(num_steps) ]
+N = 20 # max nr steps
+(m,x) = n_puzzle(puzzle_start, puzzle_end, N)
+# Lets minimize the number of steps used...
+is_sol = [all((x[i] == puzzle_end).flat) for i in range(N)]
+# which means, maximize nr of late steps that are full sol
+m.maximize( sum(i*is_sol[i] for i in range(N)) )
 
+if m.solve() is False:
+    print("UNSAT, try increasing nr of steps? or wrong input...")
+else:
+    for i in range(N):
+        print("Step", i+1)
+        print(x[i].value())
+        if (x[i].value() != puzzle_end).sum() == 0:
+            # all puzzle_end
+            break
+print(m.status())
 
-# move_to is location of the empty piece
-m += [ x[i, move_to[i]] == 0 for i in range(num_steps) ]
-
-# move_from is previous move_to, except first
-m += [ move_from[0] == move_to[0] ]
-m += [ move_from[i] == move_to[i-1] for i in range(1,num_steps) ]
-
-# only move_from/move_to can have a change in x
-m += [
-    ((move_from[i] != j) & (move_to[i] != j)).implies(x[i,j] == x[i-1,j])
-    for i in range(1,num_steps) for j in range(n)
-]
-
-
-# require valid moves
-m += [ Table( (move_from[i],move_to[i]), valid_table) for i in range(num_steps)]
-
-
-# check whether this is the end solution
-m += [ check[i] == same(x[i,:], puzzle_end) for i in range(num_steps)]
-
-# once the solution is found, keep it
-m += [ check[i] >= check[i-1] for i in range(1,num_steps) ]
-
-# index of a solution (when minimized, index of first solution)
-m += [ check[check_ix] == 1 ]
-
-
-# visualisation helper function
+"""
+# previous visualisation helper function
 def visu_npuzzle(xval, pos_from, pos_to):
     out = ""
     for r in range(dim):
@@ -130,11 +118,4 @@ def visu_npuzzle(xval, pos_from, pos_to):
                 out += f"{xval[xpos]}  "
         out += '\n'
     print(out)
-
-if m.solve() is False:
-    print("UNSAT, try increasing nr of steps? or wrong input...")
-else:
-    for i in range(check_ix.value()+1):
-        visu_npuzzle(x[i,:].value(), move_from[i].value(), move_to[i].value())
-    print(f"Found in {check_ix.value()} steps")
-print(m.status())
+"""
