be0c1a7d5b556756563ea742e7518bc077a7947f
Nicholas <nicholas.decleyre@live.be>
2021-10-09 12:19:48 +0200

    Reworked rectangular packing problem based on comments

--- examples/visual_examples/packing/run_rect_packing.py
+++ examples/visual_examples/packing/run_rect_packing.py
@@ -8,15 +8,24 @@ https://github.com/Alexander-Schiendorfer/cp-examples/tree/main/packing
 Given some rectangular 2D items, the program finds the (minimum) 2D area to pack these items.
 """
 
+import builtins
 from cpmpy import *
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 def run():
     # 2D dimensions of the items to be packed
-    widths  = [5, 6, 4, 3, 2, 4, 3, 1, 2, 1, 7, 3]
-    heights = [1, 2, 3, 2, 1, 2, 4, 6, 5, 1, 1, 2]
+    widths  = cpm_array([5, 6, 4, 3, 2, 4, 3, 1, 2, 1, 7, 3])
+    heights = cpm_array([1, 2, 3, 2, 1, 2, 4, 6, 5, 1, 1, 2])
 
+    (model, vars) = model_rect_packing(widths, heights)
+
+    if model.solve():
+        for (name, var) in vars.items():
+            print(f"{name}:\n{var.value()}")
+        visualize_rect_packing(vars, widths, heights)
+
+def model_rect_packing(widths, heights):
     # Number of different items
     n = len(widths)
 
@@ -25,51 +34,51 @@ def run():
     max_height_rect = sum(heights)
 
     # Min dimensions of the overall needed space
-    min_width_rect = max(widths)
-    min_height_rect = max(heights)
-
-    # Bounds on needed space (best vs. worst)
-    min_area = sum([widths[i]*heights[i] for i in range(0,n)])
-    max_area = max_width_rect*max_height_rect
-
-    (model, vars) = model_rect_packing(widths, heights, n, max_width_rect, 
-                                        max_height_rect, min_width_rect, min_height_rect,
-                                        min_area, max_area)
-
-    if model.solve():
-        for v in vars:
-            print(v + ":\t" + str(vars[v].value()))
-        visualize_rect_packing(vars, widths, heights, n)
-
-def model_rect_packing(widths, heights, n, max_width_rect, 
-                        max_height_rect, min_width_rect, min_height_rect,
-                        min_area, max_area):
+    min_width_rect = builtins.max(widths)
+    min_height_rect = builtins.max(heights)
+    
     # Decision variables
+    print(min_height_rect, max_height_rect)
+    print(min_width_rect, max_width_rect)
     rect_height = intvar(min_height_rect, max_height_rect)
     rect_width = intvar(min_width_rect, max_width_rect)
-    rect_area = intvar(min_area, max_area)
+
     x = intvar(0, max_width_rect, shape = n)
     y = intvar(0, max_height_rect, shape = n)
 
-    model = Model(
-        ### Necessary constraints
-        (rect_area == rect_height * rect_width), # Definition of the area of the needed space
-        [np.add(x, widths) <= rect_width], # Every item has to be within the width of the overall area
-        [np.add(y, heights) <= rect_height], # Every item has to be withing the height of the overall area
-        [(x[i] + widths[i] <= x[j]) | (x[j] + widths[j] <= x[i]) | (y[i] + heights[i] <= y[j]) | (y[j] + heights[j] <= y[i]) for i in range(n) for j in range(n) if i != j], # Every item has to be fully above, below or next to every other item
+    m = Model()
 
-        ### Optional constraints
-        # (rect_width > rect_height),
-        # (rect_width >= 10),
-        # (rect_height >= 10)
-    )
+    ### Necessary constraints
+    # Every item has to be within the width of the overall area
+    m += (x + widths <= rect_width)
+
+    # Every item has to be within the height of the overall area
+    m += (y + heights <= rect_height)
+    
+    # Every item has to be fully above, below or next to every other item
+    for i in range(n):
+        for j in range(n):
+            if i != j:
+                m += ((x[i] + widths[i] <= x[j]) | (x[j] + widths[j] <= x[i]) | (y[i] + heights[i] <= y[j]) | (y[j] + heights[j] <= y[i]))
+
+    ### Optional constraints
+    # The needed space needs to be wider than taller
+    # (rect_width > rect_height),
+
+    # The needed space has to have a width larger than 10
+    # (rect_width >= 10),
+
+    # The needed space has to have a height larger than 10
+    # (rect_height >= 10)
 
     # Minimize wrt the overall area
-    model.minimize(rect_area)
+    m.minimize((rect_height*rect_width))
 
-    return (model, {"rect_height": rect_height, "rect_width": rect_width, "rect_area": rect_area, "x": x, "y": y})
+    return (m, {"rect_height": rect_height, "rect_width": rect_width, "rect_area": rect_height*rect_width, "x": x, "y": y})
 
-def visualize_rect_packing(vars, widths, heights, n):
+def visualize_rect_packing(vars, widths, heights):
+    n = len(widths) # Amount of items
+
     rect_width = vars["rect_width"]
     rect_height = vars["rect_height"]
     rect_area = vars["rect_area"]
