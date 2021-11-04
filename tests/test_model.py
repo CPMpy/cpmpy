011e67b56a77d8a35dac9b0d9ce2419d2aec9f76
Tias Guns <tias.guns@kuleuven.be>
2021-11-04 16:33:20 +0100

    model: make sure cons is a python list\nfixes #55

---
+++ tests/test_model.py
@@ -0,0 +1,11 @@
+import unittest
+import cpmpy as cp
+from cpmpy.expressions.variables import NullShapeError, _IntVarImpl, _BoolVarImpl, NegBoolView, NDVarArray
+
+
+class TestModel(unittest.TestCase):
+    def test_ndarray(self):
+        iv = cp.intvar(1,9, shape=3)
+        m = cp.Model( iv > 3 )
+        m += (iv[0] == 5)
+        self.assertTrue(m.solve())
