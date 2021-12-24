c63d6cecfc56a26439d2e737157274840a40cb2f
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2021-12-24 11:59:27 +0100

    Expanded linearization

--- cpmpy/transformations/linearize.py
+++ cpmpy/transformations/linearize.py
@@ -5,7 +5,7 @@ Linearization of constraints to use in MIP-like solvers
 from ..expressions.core import Comparison
 from ..expressions.globalconstraints import GlobalConstraint
 from ..expressions.utils import is_any_list
-from ..expressions.variables import _BoolVarImpl
+from ..expressions.variables import _BoolVarImpl, boolvar
 from ..transformations.flatten_model import flatten_constraint
 
 def linearize_constraint(cpm_expr):
@@ -41,13 +41,31 @@ def linearize_constraint(cpm_expr):
 
     if cpm_expr.name == "!=":
         lhs, rhs = cpm_expr.args
-        return linearize_constraint(flatten_constraint((lhs >= rhs + 1) | (lhs <= rhs - 1)))
+        llb, lub, rlb, rub = lhs.lb, lhs.ub, rhs.lb, rhs.ub
+
+        M1, M2 = lub + 1 - rlb , rub + 1 - llb
+        bvar = boolvar()
+        c1 = lhs <= rhs - 1 + M1 * bvar
+        c2 = lhs >= rhs + 1 - M1 * (1 - bvar)
+
+        return flatten_constraint([c1, c2])
 
     if cpm_expr.name == "->":
         lhs, rhs = cpm_expr.args
         if lhs.is_bool() and rhs.is_bool():
             return [lhs <= rhs]
         raise Exception(f"Numeric constants or numeric variables not allowed as base constraint, cannot linearize {cpm_expr}")
+
+    if cpm_expr.name == "alldifferent":
+        # Cfr http://yetanothermathprogrammingconsultant.blogspot.com/2016/05/all-different-and-mixed-integer.html
+        cnstrs = []
+        for i, lhs in enumerate(cpm_expr.args):
+            for j, rhs in enumerate(cpm_expr.args):
+                if i >= j :
+                    continue
+                cnstrs += linearize_constraint(lhs != rhs)
+        return cnstrs
+
 
     return [cpm_expr]
 
