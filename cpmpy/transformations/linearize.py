64d1e27ccc1ed7f8acba33bf369807c138d6a7f2
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-01-03 10:23:30 +0100

    Alldiff linearized

--- cpmpy/transformations/linearize.py
+++ cpmpy/transformations/linearize.py
@@ -2,8 +2,10 @@
 Linearization of constraints to use in MIP-like solvers
 
 """
+import numpy as np
+
 from ..expressions.core import Comparison
-from ..expressions.globalconstraints import GlobalConstraint
+from ..expressions.globalconstraints import GlobalConstraint, AllDifferent
 from ..expressions.utils import is_any_list
 from ..expressions.variables import _BoolVarImpl, boolvar
 from ..transformations.flatten_model import flatten_constraint
@@ -40,15 +42,8 @@ def linearize_constraint(cpm_expr):
         return [rhs + 1 <= lhs]
 
     if cpm_expr.name == "!=":
-        lhs, rhs = cpm_expr.args
-        llb, lub, rlb, rub = lhs.lb, lhs.ub, rhs.lb, rhs.ub
-
-        M1, M2 = lub + 1 - rlb , rub + 1 - llb
-        bvar = boolvar()
-        c1 = lhs <= rhs - 1 + M1 * bvar
-        c2 = lhs >= rhs + 1 - M1 * (1 - bvar)
-
-        return flatten_constraint([c1, c2])
+        # TODO implement this --> Ignace
+        raise NotImplementedError("!= is not implemented yet: TODO")
 
     if cpm_expr.name == "->":
         lhs, rhs = cpm_expr.args
@@ -57,27 +52,35 @@ def linearize_constraint(cpm_expr):
         raise Exception(f"Numeric constants or numeric variables not allowed as base constraint, cannot linearize {cpm_expr}")
 
     if cpm_expr.name == "alldifferent":
-        # Cfr http://yetanothermathprogrammingconsultant.blogspot.com/2016/05/all-different-and-mixed-integer.html
-        cnstrs = []
-        for i, lhs in enumerate(cpm_expr.args):
-            for j, rhs in enumerate(cpm_expr.args):
-                if i >= j :
-                    continue
-                cnstrs += linearize_constraint(lhs != rhs)
-        return cnstrs
+        """
+            More efficient implementations possible
+            http://yetanothermathprogrammingconsultant.blogspot.com/2016/05/all-different-and-mixed-integer.html
+            This method avoids bounds computation
+            Introduces n^2 new boolean variables
+        """
 
+        # Boolean variables
+        if all(isinstance(arg, _BoolVarImpl) for arg in cpm_expr.args):
+            lb, ub = 0, 1
+        elif all(not isinstance(arg, _BoolVarImpl) for arg in cpm_expr.args):
+            # All intvars, check lower and upper bounds
+            lb, ub = cpm_expr.args[0].lb, cpm_expr.args[0].ub
+            if not all(arg.lb == lb and arg.ub == ub for arg in cpm_expr.args):
+                return linearize_constraint(cpm_expr.decompose())
+        else:
+            # Mix of ints and bools, check lower and upper bounds of ints
+            if not all(arg.is_bool() or (arg.lb == 0 and arg.ub == 1) for arg in cpm_expr.args):
+                return linearize_constraint(cpm_expr.decompose())
+            lb, ub = 0, 1
 
-    return [cpm_expr]
+        sigma = boolvar(shape=(len(cpm_expr.args), 1 + ub - lb))
 
+        constraints = [sum(row) == 1 for row in sigma] # Exactly one value
+        constraints += [sum(col) <= 1 for col in sigma.T] # All diff values
 
-def no_global_constraints(cpm_expr):
-    # TODO: maybe not an explicit transformation? Can also be done in the solver itself.
-    # OR maybe we put it somewhere else?
-    if is_any_list(cpm_expr):
-        ng_cons = [no_global_constraints(expr) for expr in cpm_expr]
-        return [c for l in ng_cons for c in l]
+        for arg, row in zip(cpm_expr.args, sigma):
+            constraints += [arg == sum(np.arange(lb, ub+1) * row)]
 
-    if isinstance(cpm_expr, GlobalConstraint):
-        return cpm_expr.decompose()
+        return constraints
 
     return [cpm_expr]
