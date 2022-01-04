5f1341b127da34730c0bfe65627c3b7a370198b3
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-01-04 21:53:22 +0100

    Fixed boolvar linearize and added != linearization

--- cpmpy/transformations/linearize.py
+++ cpmpy/transformations/linearize.py
@@ -4,7 +4,7 @@ Linearization of constraints to use in MIP-like solvers
 """
 import numpy as np
 
-from ..expressions.core import Comparison
+from ..expressions.core import Comparison, Operator
 from ..expressions.globalconstraints import GlobalConstraint, AllDifferent
 from ..expressions.utils import is_any_list
 from ..expressions.variables import _BoolVarImpl, boolvar
@@ -16,6 +16,9 @@ def linearize_constraint(cpm_expr):
     if is_any_list(cpm_expr):
         lin_cons = [linearize_constraint(expr) for expr in cpm_expr]
         return [c for l in lin_cons for c in l]
+
+    if isinstance(cpm_expr, _BoolVarImpl):
+        return [cpm_expr]
 
     if cpm_expr.name == "and":
         if all(arg.is_bool() for arg in cpm_expr.args):
@@ -32,6 +35,17 @@ def linearize_constraint(cpm_expr):
             return [sum(cpm_expr.args) == 1]
         raise Exception("Numeric constants or numeric variables not allowed as base constraint")
 
+    if cpm_expr.name == "->":
+        lhs, rhs = cpm_expr.args
+        if not lhs.is_bool() or not rhs.is_bool():
+            raise Exception(
+                f"Numeric constants or numeric variables not allowed as base constraint, cannot linearize {cpm_expr}")
+        if isinstance(rhs, _BoolVarImpl):
+            return [lhs <= rhs]
+        if isinstance(lhs, _BoolVarImpl):
+            return [rhs >= lhs]
+        raise Exception(f"{cpm_expr} should be of the form Var -> BoolExpr or BoolExpr -> Var")
+
     #Binary operators
     if cpm_expr.name == "<":
         lhs, rhs = cpm_expr.args
@@ -47,24 +61,32 @@ def linearize_constraint(cpm_expr):
         lhs, rhs = cpm_expr.args
         if isinstance(lhs, _BoolVarImpl) and isinstance(rhs, _BoolVarImpl):
             return [lhs + rhs == 1]
-        M = 1e20 # Arbitrary VERY large number
+        M = int(10e10) # Arbitrary VERY large number
         z = boolvar()
 
         rhs_plus_1, cons_plus = get_or_make_var(rhs + 1)
         rhs_minus_1, cons_minus = get_or_make_var(rhs - 1)
 
-        c1 = M * z + lhs >= rhs_plus_1
-        c2 = -M(1-z) + lhs <= rhs_minus_1
-        return [M * z + lhs >= y]
+        Mz, cons_Mz = get_or_make_var(M * z)
+        Mmz, cons_Mmz = get_or_make_var(-M * (1-z))
 
-        # TODO fix this, very ugly and inefficient
-        return flatten_constraint(linearize_constraint(lhs + 1 <= rhs or lhs >= 1 + rhs))
+        c1 = Mz + lhs >= rhs_plus_1
+        c2 = Mmz + lhs <= rhs_minus_1
 
-    if cpm_expr.name == "->":
+        return cons_plus + cons_minus + cons_Mz + cons_Mmz + [c1, c2]
+
+    if cpm_expr.name in [">=", "<=", "=="] and cpm_expr.args[0].name == "mul":
+        if all(isinstance(arg, _BoolVarImpl) for arg in cpm_expr.args[0].args):
+            return [Comparison(cpm_expr.name, sum(cpm_expr.args[0].args), cpm_expr.args[1])]
+
         lhs, rhs = cpm_expr.args
-        if lhs.is_bool() and rhs.is_bool():
-            return [lhs <= rhs]
-        raise Exception(f"Numeric constants or numeric variables not allowed as base constraint, cannot linearize {cpm_expr}")
+        var, constraints = get_or_make_var(lhs.args[0] * lhs.args[1])
+        for arg in lhs.args[2:]:
+            var, cons = get_or_make_var(var * arg)
+            constraints += cons
+
+        return constraints + [Comparison(cpm_expr.name, var, rhs)]
+
 
     if cpm_expr.name == "alldifferent":
         """
@@ -73,7 +95,7 @@ def linearize_constraint(cpm_expr):
             This method avoids bounds computation
             Introduces n^2 new boolean variables
         """
-
+        # TODO check performance of implementation
         # Boolean variables
         if all(isinstance(arg, _BoolVarImpl) for arg in cpm_expr.args):
             lb, ub = 0, 1
