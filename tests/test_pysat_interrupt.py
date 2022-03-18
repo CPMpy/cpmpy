b4c59305e9acd370cc67d18d37157e997c686e6d
Emilio <emilio.gamba@outlook.be>
2022-03-18 09:17:46 +0100

    cleaning imports and functions

--- tests/test_pysat_interrupt.py
+++ tests/test_pysat_interrupt.py
@@ -1,8 +1,6 @@
-from email.policy import default
 import unittest
 from cpmpy import *
 from cpmpy.solvers import CPM_pysat
-from cpmpy.transformations.get_variables import get_variables_model
 
 def frietkot():
     # Construct the model.
@@ -25,30 +23,6 @@ def frietkot():
     model = Model(allwishes)
     return model, [mayo, ketchup, curry, andalouse, samurai]
 
-def frietkot_n(n):
-    allvars = []
-    allwishes = []
-
-    # Construct the model.
-    for i in range(n):
-        (mayo, ketchup, curry, andalouse, samurai) = boolvar(5)
-        allvars += [mayo, ketchup, curry, andalouse, samurai]
-        # Pure CNF
-        Nora = mayo | ketchup
-        Leander = ~samurai | mayo
-        Benjamin = ~andalouse | ~curry | ~samurai
-        Behrouz = ketchup | curry | andalouse
-        Guy = ~ketchup | curry | andalouse
-        Daan = ~ketchup | ~curry | andalouse
-        Celine = ~samurai
-        Anton = mayo | ~curry | ~andalouse
-        Danny = ~mayo | ketchup | andalouse | samurai
-        Luc = ~mayo | samurai
-
-        allwishes += [Nora, Leander, Benjamin, Behrouz, Guy, Daan, Celine, Anton, Danny, Luc]
-
-    model = Model(allwishes)
-    return model, allvars
 
 class TestPySATInterrupt(unittest.TestCase):
     def test_small_isntance_no_interrupt(self):
@@ -67,21 +41,28 @@ class TestPySATInterrupt(unittest.TestCase):
         from collections import defaultdict
         import time
         lit_cpmvar = defaultdict(lambda: boolvar())
-        cpm_clauses = []
         m  = Model()
+
+        # Implementing pysat example for interrupt in cpmpy
+        # https://pysathq.github.io/docs/html/api/solvers.html#pysat.solvers.Solver.interrupt
         for clause in PHP(nof_holes=10).clauses:
             new_clause = []
             for lit in clause:
                 lit_var = lit_cpmvar[abs(lit)]
                 new_clause.append(~lit_var if lit < 0 else lit_var)
             m +=any(c for c in new_clause)
+
         s = CPM_pysat(m)
+
         assumption = [lit_cpmvar[1]]
+
         # offset for additional stuff done by cpmpy after solving
         time_limit, time_offset = 1, 0.5
+
         tstart_solving = time.time()
         s.solve(assumptions=assumption, time_limit=time_limit)
         tend_solving = time.time()
+
         self.assertLessEqual(tend_solving - tstart_solving, time_limit + time_offset)
 
 if __name__ == '__main__':
