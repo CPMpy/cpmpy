51b09646b94e4ea588648fff3cdb727428a011b3
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-03-25 09:22:29 +0100

    Updated docstring Renamed and move file

--- examples/advanced/ortools_presolve_propagate.py
+++ examples/advanced/ortools_presolve_propagate.py
@@ -1,3 +1,12 @@
+"""
+Example showing direct solver access to the OR-tools solver object.
+
+Uses the presolve feature of ortools and reads the protobuffer response directly.
+
+Constructs new constraints for bounds found by the OR-tools presolve.
+Consult https://developers.google.com/optimization/reference/sat/cp_model_presolve/CpModelPresolver for details about OR-tools presolver.
+"""
+
 from cpmpy import *
 
 
@@ -28,7 +37,8 @@ for bound in bounds:
             """
             lbs = [val for i,val in enumerate(bound.domain) if i % 2 == 0]
             ubs = [val for i,val in enumerate(bound.domain) if i % 2 == 1]
-
+            # More elaborate constraints can be found here.
+            # I.e. cpm_var != val if there is only one hole in the domain of the variable
             args = [(cpm_var >= lb) & (cpm_var <= ub) for lb, ub in zip(lbs, ubs)]
 
             print(f"Derived new constraints for {cpm_var}:", any(args))
