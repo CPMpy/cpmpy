e8eb8cb8f466d41932116031cfb184b2f052371d
Emilio <emilio.gamba@outlook.be>
2022-04-04 10:51:29 +0200

    cleaning up

--- tests/test_inequality_chaining.py
+++ tests/test_inequality_chaining.py
@@ -5,7 +5,6 @@ class TestInequalityChaining(unittest.TestCase):
     def test_chaining_lt(self):
 
         bv1, bv2, bv3 = boolvar(shape=3)
-        # pdb.set_trace()
         c1  = (bv1 < bv2) and (bv2 < bv3)
         c2  = (bv1 < bv2 < bv3)
         self.assertEqual(str(c1), str(c2))
