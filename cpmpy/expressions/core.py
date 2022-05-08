a66acf60bed247348d8ae81356f4f537f0276e3d
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-05-08 22:31:59 +0200

    Bool multiplication (#116)
    * Added 0 as zero element for multiplication
    * Bools are now treated as numeric when needed (e.g. in multiplication)

--- cpmpy/expressions/core.py
+++ cpmpy/expressions/core.py
@@ -265,10 +265,14 @@ class Expression(object):
     def __mul__(self, other):
         if is_num(other) and other == 1:
             return self
+        if is_num(other) and other == 0:
+            return 0
         return Operator("mul", [self, other])
     def __rmul__(self, other):
         if is_num(other) and other == 1:
             return self
+        if is_num(other) and other == 0:
+            return 0
         return Operator("mul", [other, self])
 
     # matrix multipliciation TODO?
