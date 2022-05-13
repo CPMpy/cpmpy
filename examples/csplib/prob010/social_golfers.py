544947aa0260314498520c7189daab3978c95bf5
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-05-13 13:16:11 +0200

    prob010

---
+++ examples/csplib/prob010/social_golfers.py
@@ -0,0 +1,109 @@
+"""
+Social golfer problem in cpmpy.
+
+CSPLib problem 10:
+http://www.csplib.org/prob/prob010/index.html
+'''
+The coordinator of a local golf club has come to you with the following
+problem. In her club, there are 32 social golfers, each of whom play golf
+once a week, and always in groups of 4. She would like you to come up
+with a schedule of play for these golfers, to last as many weeks as
+possible, such that no golfer plays in the same group as any other golfer
+on more than one occasion.
+
+Possible variants of the above problem include: finding a 10-week schedule
+with ``maximum socialisation''; that is, as few repeated pairs as possible
+(this has the same solutions as the original problem if it is possible
+to have no repeated pairs), and finding a schedule of minimum length
+such that each golfer plays with every other golfer at least once
+(``full socialisation'').
+
+The problem can easily be generalized to that of scheduling m groups of
+n golfers over p weeks, such that no golfer plays in the same group as any
+other golfer twice (i.e. maximum socialisation is achieved).
+'''
+
+
+This model is a translation of the OPL code from
+http://www.dis.uniroma1.it/~tmancini/index.php?currItem=research.publications.webappendices.csplib2x.problemDetails&problemid=010
+
+
+
+This cpmpy model was written by Hakan Kjellerstrand (hakank@gmail.com)
+See also my cpmpy page: http://hakank.org/cpmpy/
+
+Modified by Ignace Bleukx
+"""
+from cpmpy import *
+from cpmpy.expressions.utils import all_pairs
+import numpy as np
+
+def print_sol(assign, golfers):
+    print("assign:")
+    print(assign, end="\n\n")
+
+    # print schedule
+    print("Schedule:")
+    for w in range(weeks):
+        print("week:", w + 1)
+        for gr in range(groups):
+            gs = np.where(assign[:,w] == gr)[0] + 1
+            print("group ", gr + 1, end=": ")
+            print("golfers:", "".join([f"{g:3d}" for g in gs]))
+    print()
+
+    # check which golfers meet each other
+    meets = {g: [] for g in range(golfers)}
+    for g1, g2 in all_pairs(range(golfers)):
+        if sum(assign[g1] == assign[g2]) >= 1:
+            meets[g1] += [g2+1]
+            meets[g2] += [g1+1]
+
+    for g in range(golfers):
+        print(f"Golfer {g+1} meets:", sorted(meets[g]))
+    print(flush=True)
+
+
+if __name__ == "__main__":
+
+    weeks, groups, groupSize, num_sols = 4, 3, 3, 1
+
+    n_golfers = groups * groupSize
+    print("golfers:", n_golfers, "weeks:", weeks, "groupSize:", groupSize, "groups:", groups)
+
+    Golfer = np.arange(n_golfers)
+    Week = np.arange(weeks)
+    Group = np.arange(groups)
+
+    #Possible configurations
+    assign = intvar(0, groups - 1, shape=(n_golfers, weeks), name="assign")
+
+    model = Model()
+
+    # C1: Each group has exactly groupSize players
+    for gr in Group:
+        # can be written cleaner, see issue #117
+        # model += sum(assign == gr, axis=1) == groupSize
+        for w in Week:
+            model += sum(assign[:,w] == gr) == groupSize
+
+    # C2: Each pair of players only meets at most once
+    for g1, g2 in all_pairs(Golfer):
+        model += sum(assign[g1] == assign[g2]) <= 1
+
+
+    # SBSA: Symmetry-breaking by selective assignment
+    # On the first week, the first groupSize golfers play in group 1, the
+    # second groupSize golfers play in group 2, etc. On the second week,
+    # golfer 1 plays in group 1, golfer 2 plays in group 2, etc.
+    model += [assign[:,0] == (Golfer // groupSize)]
+
+    for g in Golfer:
+        if g < groupSize:
+            model += [assign[g, 1] == g]
+
+    # First golfer always in group 0
+    model += [assign[0, :] == 0]
+
+    # print all solutions
+    model.solveAll(solution_limit=num_sols, display=lambda : print_sol(assign.value(), n_golfers))
---
+++ examples/csplib/prob010/specification.md
@@ -0,0 +1,18 @@
+---
+Title:    Social Golfers Problem
+Proposer: Warwick Harvey
+Category: Scheduling and related problems
+---
+
+
+The coordinator of a local golf club has come to you with the following problem. In their club, there are 32 social golfers, each of whom play golf once a week, and always in groups of 4. They would like you to come up with a schedule of play for these golfers, to last as many weeks as possible, such that no golfer plays in the same group as any other golfer on more than one occasion.
+Possible variants of the above problem include: finding a 10-week schedule with "maximum socialisation"; that is, as few repeated pairs as possible (this has the same solutions as the original problem if it is possible to have no repeated pairs), and finding a schedule of minimum length such that each golfer plays with every other golfer at least once ("full socialisation").
+
+The problem can easily be generalized to that of scheduling $m$ groups of $n$ golfers over $p$ weeks, such that no golfer plays in the same group as any other golfer twice (i.e. maximum socialisation is achieved).
+
+This problem is derived from a question posted to sci.op-research by
+bigwind777@aol.com (Bigwind777) in May 1998. It is a generalisation of the
+problem of constructing a
+round-robin tournament schedule, where the number
+of players in a "game" is
+more than two. The optimal solution for 32 golfers is not yet known.
