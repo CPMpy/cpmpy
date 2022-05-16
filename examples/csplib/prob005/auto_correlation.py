e31ebb7d51a8a9629fa87af08f357d467ebd7d8c
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-05-16 11:53:43 +0200

    prob005

---
+++ examples/csplib/prob005/auto_correlation.py
@@ -0,0 +1,42 @@
+"""
+    Minimizing autocorrelation of bitarray in CPMpy
+
+    Problem 005 on CSPlib
+
+    Model created by Ignace Bleukx
+"""
+
+
+from cpmpy import *
+import numpy as np
+
+
+# periodic auto correlation
+def PAF(arr, s):
+    return sum(arr * np.roll(arr,-s))
+
+
+if __name__ == "__main__":
+    n = 16
+
+    # bitarray of length n
+    arr = intvar(-1,1,shape=n, name="arr")
+
+    model = Model()
+
+    # exclude 0
+    model += arr != 0
+
+    # minimize sum of squares
+    model.minimize(
+        sum([PAF(arr,s) ** 2 for s in range(1,n)])
+    )
+
+    model.solve()
+
+    # print using runlength notation
+    arr = arr.value()
+    pieces = np.split(arr, np.where(arr == -1)[0])
+
+    print("Run length encoding of solution:")
+    print("".join([str(len(p)) for p in pieces if len(p) != 0]))
