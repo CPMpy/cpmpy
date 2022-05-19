1671f2881cb204be7b17c79b4582976347665fa0
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-05-19 15:49:08 +0200

    Restructured 008

--- examples/csplib/prob008_vessel_loading.py
+++ examples/csplib/prob008_vessel_loading.py
@@ -14,47 +14,7 @@ from cpmpy import *
 import numpy as np
 from cpmpy.expressions.utils import all_pairs
 
-def get_data(name):
-
-    if name == "easy":
-        deck_width = 5
-        deck_length = 5
-        n_containers = 3
-        n_classes = 2
-
-        width = [5,2,3]
-        length=[1,4,4]
-
-        classes = [1,1,1]
-        separation = [[0,0],[0,0]]
-
-    elif name == "hard":
-        deck_width = 16
-        deck_length = 16
-        n_containers = 10
-        n_classes = 3
-
-        width = [6, 4, 4, 4, 4, 4, 4, 4, 4, 4]
-        length = [8, 6, 4, 4, 4, 6, 8, 8, 6, 6]
-
-        classes = [1, 1, 1, 2, 2, 2, 3, 3, 3, 3]
-
-        separation = [[0, 0, 0],
-                      [0, 0, 2],
-                      [0, 2, 0]]
-
-    return deck_width, deck_length, n_containers, n_classes, np.array(width), np.array(length), np.array(classes), np.array(separation)
-
 def vessel_loading(deck_width, deck_length, n_containers, n_classes, width, length, classes, separation):
-
-    print(f"{deck_width=}")
-    print(f"{deck_length=}")
-    print(f"{n_containers=}")
-    print(f"{n_classes=}")
-    print(f"{width=}")
-    print(f"{length=}")
-    print(f"{classes=}")
-    print(f"{separation=}")
 
     containers = list(range(n_containers))
 
@@ -66,12 +26,10 @@ def vessel_loading(deck_width, deck_length, n_containers, n_classes, width, leng
     top = intvar(0, deck_length, shape=n_containers, name="top")
     bottom = intvar(0, deck_length, shape=n_containers, name="bottom")
 
-    all_points = np.stack([left, right, top, bottom], axis=0)
-
     # set shape of containers
     model += (
             (((right - left) == width) & ((top - bottom) == length)) # along shipdeck
-                 |
+                                        |
             (((right - left) == length) & ((top - bottom) == width)) # accross shipdeck
     )
 
@@ -87,28 +45,57 @@ def vessel_loading(deck_width, deck_length, n_containers, n_classes, width, leng
                 (bottom[x] >= top[y] + sep)   # x at least sep above y
         )
 
+    return model, (left, right, top, bottom)
+
+def get_data(name):
+
+    if name == "easy":
+        return {
+            "deck_width": 5,
+            "deck_length" : 5,
+            "n_containers" : 3,
+            "n_classes" : 2,
+
+            "width" : np.array([5,2,3]),
+            "length" :np.array([1,4,4]),
+
+            "classes" : np.array([1,1,1]),
+            "separation" : np.array([[0,0],[0,0]])
+        }
+
+    if name == "hard":
+        return {
+            "deck_width" : 16,
+            "deck_length" : 16,
+            "n_containers" : 10,
+            "n_classes" : 3,
+
+            "width" : np.array([6, 4, 4, 4, 4, 4, 4, 4, 4, 4]),
+            "length" : np.array([8, 6, 4, 4, 4, 6, 8, 8, 6, 6]),
+
+            "classes" : np.array([1, 1, 1, 2, 2, 2, 3, 3, 3, 3]),
+            "separation" : np.array([[0, 0, 0],
+                                     [0, 0, 2],
+                                     [0, 2, 0]])
+        }
+
+if __name__ == "__main__":
+    name = "hard"
+    if len(sys.argv) > 1:
+        name = sys.argv[1]
+
+    data = get_data(name)
+    model, (left, right, top, bottom) = vessel_loading(**data)
 
     # solve the model
-    if model.solve(solver="ortools"):
-        container_map = np.zeros(shape=(deck_length, deck_width),dtype=int)
+    if model.solve():
+        container_map = np.zeros(shape=(data["deck_length"], data["deck_width"]), dtype=int)
         l, r, t, b = left.value(), right.value(), top.value(), bottom.value()
-        for c in containers:
-            container_map[b[c]:t[c],l[c]:r[c]] = c+1
+        for c in range(data["n_containers"]):
+            container_map[b[c]:t[c], l[c]:r[c]] = c + 1
 
         print("Shipdeck layout (0 means no container in that spot):")
         print(np.flip(container_map, axis=0))
 
     else:
         print("Model is unsatisfiable")
-
-
-
-if __name__ == "__main__":
-    name = "hard"
-
-    if len(sys.argv) > 1:
-        name = sys.argv[1]
-
-    data = get_data(name)
-
-    vessel_loading(*data)
