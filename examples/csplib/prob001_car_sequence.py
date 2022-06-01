8494d2e4b08b32e49ec5c0417429cc02bb14e607
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-06-01 11:38:34 +0200

    Converted car_sequence to work with json data

--- examples/csplib/prob001_car_sequence.py
+++ examples/csplib/prob001_car_sequence.py
@@ -6,14 +6,18 @@ Based on the Minizinc model car.mzn.
 Data format compatible with both variations of model (with and without block constraints)
 Model was created by Ignace Bleukx.
 """
+import sys
+import json
+import requests
 
-from cpmpy import *
 import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 
-import sys
+from cpmpy import *
 
-def car_sequence(n_cars, n_options, n_classes, capacity, block_size, n_cars_p_class, options):
+
+
+def car_sequence(n_cars, n_options, n_classes, n_cars_p_class, options, capacity=None, block_size=None, **kwargs):
 
   # build model
   model = Model()
@@ -21,7 +25,10 @@ def car_sequence(n_cars, n_options, n_classes, capacity, block_size, n_cars_p_cl
   # decision variables
   slots = intvar(0, n_classes-1, shape=n_cars, name="slots")
   setup = boolvar(shape=(n_cars, n_options), name="setup")
-  
+
+  # convert options to cpm_array
+  options = cpm_array(options)
+
   # satisfy demand
   model += [sum(slots == c) == n_cars_p_class[c] for c in range(n_classes)]
 
@@ -43,48 +50,33 @@ def car_sequence(n_cars, n_options, n_classes, capacity, block_size, n_cars_p_cl
   return model, slots, setup
 
 
-def get_data(fname):
-  # read data
-  with open(fname, "r") as data:
-    n_cars, n_options, n_classes = map(int, data.readline().split(" "))
+def get_data(data, pname):
+  for entry in data:
+    if pname in entry["name"]:
+      return entry
 
-    n_cars_p_class = np.zeros(shape=n_classes, dtype=int)
-    options = np.zeros(shape=(n_classes, n_options), dtype=bool)
-
-    line = list(map(int, data.readline().split(" ")))
-    if line[0] == 0:
-      # no block constraints
-      capacity, block_size = None, None
-
-      c_idx, n, *opt = line
-      n_cars_p_class[c_idx] = n
-      options[c_idx] = opt
-
-    else:
-      # block constraints
-      capacity = np.array(line)
-      block_size = np.array(list(map(int, data.readline().split(" "))))
-
-    while 1:
-      line = data.readline()
-      if len(line) == 0:
-        break
-
-      c_idx, n, *opt = map(int, line.split(" "))
-
-      n_cars_p_class[c_idx] = n
-      options[c_idx] = opt
-
-    options = cpm_array(options)
-
-    return n_cars, n_options, n_classes, \
-           capacity, block_size,\
-           n_cars_p_class, options
 
 if __name__ == "__main__":
   # get data
-  data = get_data(sys.argv[1])
-  model, slots, setup = car_sequence(*data)
+  fname = "https://raw.githubusercontent.com/CPMpy/cpmpy/csplib/examples/csplib/prob001_car_sequence.json"
+  problem_name = "Problem 4/72"
+
+  data = None
+
+  if len(sys.argv) > 1:
+    fname = sys.argv[1]
+    with open(fname,"r") as f:
+      data = json.load(f)
+
+  if len(sys.argv) > 2:
+    problem_name = sys.argv[2]
+
+  if data is None:
+    data = requests.get(fname).json()
+
+  params = get_data(data, problem_name)
+
+  model, slots, setup = car_sequence(**params)
 
   # solve the model
   if model.solve():
