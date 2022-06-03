cf1a7d451b18bb5d5bbe423b54ed81e2a057b627
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-06-03 13:40:24 +0200

    updated description prob013

--- examples/csplib/prob013_progressive_party.py
+++ examples/csplib/prob013_progressive_party.py
@@ -4,7 +4,14 @@
     Problem 013 on CSPlib:
     https://www.csplib.org/Problems/prob013/
 
-    The problem is to timetable a party at a yacht club. Certain boats are to be designated hosts, and the crews of the remaining boats in turn visit the host boats for several successive half-hour periods. The crew of a host boat remains on board to act as hosts while the crew of a guest boat together visits several hosts. Every boat can only hold a limited number of people at a time (its capacity) and crew sizes are different. The total number of people aboard a boat, including the host crew and guest crews, must not exceed the capacity. A table with boat capacities and crew sizes can be found below; there were six time periods. A guest boat cannot not revisit a host and guest crews cannot meet more than once. The problem facing the rally organizer is that of minimizing the number of host boats.
+    The problem is to timetable a party at a yacht club.
+    Certain boats are to be designated hosts, and the crews of the remaining boats in turn visit the host boats for several successive half-hour periods.
+    The crew of a host boat remains on board to act as hosts while the crew of a guest boat together visits several hosts.
+    Every boat can only hold a limited number of people at a time (its capacity) and crew sizes are different.
+    The total number of people aboard a boat, including the host crew and guest crews, must not exceed the capacity.
+    A table with boat capacities and crew sizes can be found below; there were six time periods.
+    A guest boat cannot not revisit a host and guest crews cannot meet more than once.
+    The problem facing the rally organizer is that of minimizing the number of host boats.
 
     Model by Ignace Bleukx, ignace.bleukx@kuleuven.be
 """
