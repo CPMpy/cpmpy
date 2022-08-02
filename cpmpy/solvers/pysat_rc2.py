d1ddd6e5bdb8ebf8c85f3ef5e1260a21c30c9699
Emilio Gamba <emilio.gamba@outlook.be>
2022-08-02 11:38:05 +0300

    Keep track of trasnformed clauses of maxsat model

--- cpmpy/solvers/pysat_rc2.py
+++ cpmpy/solvers/pysat_rc2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
 ##
-## pysat.py
+## pysat_rc2.py
 ##
 """
     Interface to PySAT's API
@@ -27,7 +27,7 @@
     .. autosummary::
         :nosignatures:
 
-        CPM_rc2
+        CPM_RC2
 """
 from ..expressions.core import Operator
 from ..expressions.variables import _BoolVarImpl
@@ -79,7 +79,8 @@ class CPM_RC2(CPM_pysat):
 
     def __init__(self, cpm_model=None, subsolver="glucose3"):
         """
-        Constructor of the native RC2 solver object
+        Constructor of the native RC2 solver object.
+        We keep track of the built cnf clauses to reinitliase the solver when assumptions are added to the maxsat solver as hard clauses.
 
         Requires a CPMpy model as input, and will create the corresponding
         PySAT clauses and RC2 solver object.
@@ -100,11 +101,66 @@ class CPM_RC2(CPM_pysat):
         self.pysat_solver = RC2(formula=WCNF(), solver=subsolver)
         self.pysat_vpool = self.pysat_solver.pool
 
+        # RESTARTS: keep track of subsovler
+        self.subsolver = subsolver
+        # RESTARTS: keep track of transformed clauses
+        self.wcnf = WCNF()
+        # RESTART only if assumptions are used !
+        self._solved_assumption = False
+
         SolverInterface.__init__(self, name="rc2", cpm_model=cpm_model)
+
+    def __add__(self, cpm_con):
+        """
+        Post a (list of) CPMpy constraints(=expressions) to the solver
+
+        Note that we store the constraints in a WCNF formula,
+        we first transform the constraints into primitive constraints,
+        then post those primitive constraints directly to the native solver
+
+        :param cpm_con CPMpy constraint, or list thereof
+        :type cpm_con (list of) Expression(s)
+        """
+        # add new user vars to the set
+        self.user_vars.update(get_variables(cpm_con))
+
+        # apply transformations, then post internally
+        cpm_cons = to_cnf(cpm_con)
+        for con in cpm_cons:
+            clauses = self._encode_constraint(con)
+            self._post_clauses(clauses)
+            ## keep track of the transformed clauses
+            self.wcnf.extend(clauses)
+
+        return self
+
+    def _restart(self, wcnf):
+        if self.pysat_solver is not None:
+            del self.pysat_solver
+        self.pysat_solver = RC2(formula=wcnf, solver=self.subsolver)
+
+    def _add_assumptions(self, assumptions=None):
+        ## restart the solver in case it's solved with assumptions
+        if self._solved_assumption:
+            self._restart(self.wcnf)
+            self._solved_assumption = False
+
+        pysat_assum_vars = self.solver_vars(assumptions)
+        ## only set to true if there are assumptions
+        if len(pysat_assum_vars) > 0:
+            self._solved_assumption = True
+
+        for pysat_assum_var in pysat_assum_vars:
+            self.pysat_solver.add_clause([pysat_assum_var])
 
     def solve(self, assumptions=None):
         """
-            Call the MaxSAT RC2 solver
+            Call the MaxSAT RC2 solver.
+            1. In case, a new solve call is executed without changing the assumptions
+               then the rc2 is not restarted and computes a solution
+
+            2. In case new assumptions are added, then the solver is restarted with the
+               base constraints of the input model.
 
             Arguments:
             - assumptions: list of CPMpy Boolean variables that are assumed to be true.
@@ -112,10 +168,7 @@ class CPM_RC2(CPM_pysat):
                            CPM_rc2 adds the assumptions as hard clauses to the MaxSAT solver.
         """
         if assumptions is not None:
-            print("[Warning] Assumption variables interpreted as hard clauses!")
-            pysat_assum_vars = self.solver_vars(assumptions)
-            for pysat_assum_var in pysat_assum_vars:
-                self.pysat_solver.add_clause([pysat_assum_var])
+            self._add_assumptions(assumptions)
 
         sol = self.pysat_solver.compute()
 
@@ -166,11 +219,13 @@ class CPM_RC2(CPM_pysat):
             pysat_assum_vars = self.solver_vars(expr.args)
             for pysat_assum_var in pysat_assum_vars:
                 self.pysat_solver.add_clause([pysat_assum_var], weight=1)
+                self.wcnf.append([pysat_assum_var], weight=1)
         elif isinstance(expr, Operator) and expr.name == "wsum":
             weights, vars = expr.args
             pysat_assum_vars = self.solver_vars(vars)
             for weight, pysat_assum_var in zip(weights, pysat_assum_vars):
                 self.pysat_solver.add_clause([pysat_assum_var], weight=weight)
+                self.wcnf.append([pysat_assum_var], weight=1)
         else:
             raise NotImplementedError(f"Expression {expr} not handled")
 
