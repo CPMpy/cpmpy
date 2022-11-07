b5b00ed79c05e9c57b878d8fb56828bd8b4554f6
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2022-11-07 14:39:20 +0100

    alternative algorithm based on intersection

--- examples/advanced/maximal_propagate.py
+++ examples/advanced/maximal_propagate.py
@@ -27,6 +27,31 @@ def maximal_propagate(vars=None, constraints=[], solvername="ortools"):
     # exhausted all possible domain values
     return visisted_domain
 
+def maximal_propagate_alt(vars=None, constraints=[], solvername="ortools"):
+    # For faster propagation, use incremental solver such as pysat, z3 or gurobi.
+    if vars is None:
+        vars = get_variables(constraints)
+
+    domain_to_visit = {var : set(range(var.lb, var.ub+1)) for var in vars}
+
+    solver = SolverLookup.get(solvername)
+    solver += constraints
+
+    while solver.solve():
+        cons = False
+        for var, values in domain_to_visit.items():
+            values.discard(var.value())
+            # assign at least one variable to an unvisited value
+            cons |= any(var ==  val for val in values)
+
+        solver += cons
+
+    # return values of variables reachable given the constraints
+    return {
+        var : {val for val in range(var.lb, var.ub) if val not in domain_to_visit[var]}
+            for var in vars
+    }
+
 
 if __name__ == "__main__":
 
@@ -35,7 +60,7 @@ if __name__ == "__main__":
     model = Model([x < y, y != 3])
     print("Propagating constraints in model:", model, sep="\n", end="\n\n")
 
-    possible_vals = maximal_propagate(model.constraints)
+    possible_vals = maximal_propagate_alt(constraints = model.constraints)
 
     for var, values in possible_vals.items():
         print(f"{var}: {sorted(values)}")
