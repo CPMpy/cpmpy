59884fa08e3193013222bfa4658e53c6109017e4
Emilio Gamba <emilio.gamba@outlook.be>
2022-11-28 13:55:49 +0100

    Added 168 bug test case

--- tests/test_trans_linearize.py
+++ tests/test_trans_linearize.py
@@ -1,8 +1,8 @@
 import unittest
 
-from cpmpy.expressions import boolvar
+import cpmpy as cp
+from cpmpy.expressions import boolvar, intvar
 from cpmpy.transformations.linearize import linearize_constraint
-
 
 class TestTransLineariez(unittest.TestCase):
 
@@ -26,3 +26,12 @@ class TestTransLineariez(unittest.TestCase):
         # implies
         cons = linearize_constraint(a.implies(b))[0]
         self.assertEqual("(a) -> (b >= 1)", str(cons))
+    
+    def test_bug_168(self):
+        bv = boolvar(shape=2)
+        iv = intvar(1, 9)
+        e1 = (bv[0] * bv[1] == iv)
+        s1 = cp.Model(e1).solve("gurobi")
+        s1 = cp.Model(e1).solve("ortools")
+        self.assertTrue(s1)
+        self.assertEqual([bv[0].value(), bv[1].value(), iv.value()],[True, True, 1])
