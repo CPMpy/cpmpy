b60310d7962bc7631bcf0b9024140e47c1fb302e
Tias Guns <tias.guns@kuleuven.be>
2022-11-29 10:30:05 +0100

    186 testcase requires gurobi, check for it

--- tests/test_trans_linearize.py
+++ tests/test_trans_linearize.py
@@ -28,10 +28,12 @@ class TestTransLineariez(unittest.TestCase):
         self.assertEqual("(a) -> (b >= 1)", str(cons))
     
     def test_bug_168(self):
-        bv = boolvar(shape=2)
-        iv = intvar(1, 9)
-        e1 = (bv[0] * bv[1] == iv)
-        s1 = cp.Model(e1).solve("gurobi")
-        s1 = cp.Model(e1).solve("ortools")
-        self.assertTrue(s1)
-        self.assertEqual([bv[0].value(), bv[1].value(), iv.value()],[True, True, 1])
+        from cpmpy.solvers import CPM_gurobi
+        if CPM_gurobi.supported():
+            bv = boolvar(shape=2)
+            iv = intvar(1, 9)
+            e1 = (bv[0] * bv[1] == iv)
+            s1 = cp.Model(e1).solve("gurobi")
+            s1 = cp.Model(e1).solve("ortools")
+            self.assertTrue(s1)
+            self.assertEqual([bv[0].value(), bv[1].value(), iv.value()],[True, True, 1])
