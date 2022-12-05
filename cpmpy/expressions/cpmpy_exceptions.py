125dbfde1411962b335c6d8b95ebaa39ebc6e761
wout4 <wout.vanroose@gmail.com>
2022-12-05 16:25:29 +0100

    Added new file for custom cpmpy exceptions

---
+++ cpmpy/expressions/cpmpy_exceptions.py
@@ -0,0 +1,12 @@
+'''
+Custom exception classes, for finer grained error handling
+'''
+
+
+class CPMpyException(Exception):
+    '''Parent class for all our exceptions'''
+    pass
+
+
+class MinizincPathException(CPMpyException):
+    pass
