548da0c91d17bcda2230ceea585c07e039d37a2e
Emilio Gamba <emilio.gamba@outlook.be>
2022-12-06 18:16:28 +0100

    attempted solution printer

--- cpmpy/solvers/pysat_rc2.py
+++ cpmpy/solvers/pysat_rc2.py
@@ -21,7 +21,8 @@
 
         CPM_RC2
 """
-from ..expressions.core import Operator
+from cpmpy.transformations import get_variables
+from ..expressions.core import Operator, Expression
 from .pysat import CPM_pysat
 from .solver_interface import SolverInterface, SolverStatus, ExitStatus
 
@@ -68,10 +69,10 @@ class CPM_RC2(CPM_pysat):
                 names.append(name)
         return names
 
-    def __init__(self, cpm_model=None, subsolver="glucose3"):
+    def __init__(self, cpm_model=None, subsolver=None):
         """
         Constructor of the native RC2 solver object.
-        We keep track of the built cnf clauses to reinitliase the solver when assumptions are added to the maxsat solver as hard clauses.
+        We keep track of the built cnf clauses to reinitialize the solver when assumptions are added to the MaxSAT solver as hard clauses.
 
         Requires a CPMpy model as input, and will create the corresponding
         PySAT clauses and RC2 solver object.
@@ -86,6 +87,9 @@ class CPM_RC2(CPM_pysat):
 
         from pysat.formula import WCNF
         from pysat.examples.rc2 import RC2
+
+        if subsolver is None:
+            subsolver = "glucose3"
 
         assert subsolver in CPM_RC2.solvernames(), f"Wrong solver ({subsolver}) selected from available: ({CPM_RC2.solvernames()})"
 
@@ -140,30 +144,33 @@ class CPM_RC2(CPM_pysat):
         RC2 is called incrementally if `_prev_pysat_assum_vars' is a subset
         of the given assumptions. There is no need to restart RC2 solver.
 
-        Otherwhise, restart RC2.
+        Otherwise, restart RC2.
         """
-        pysat_assum_vars = set(self.solver_vars(assumptions))
+        pysat_vars = set(self.solver_vars(assumptions))
 
         # No need to restart if solved incrementally
-        if not set(self._prev_pysat_assum_vars).issubset(pysat_assum_var):
+        if not set(self._prev_pysat_assum_vars).issubset(pysat_vars):
             self._restart(self.wcnf)
 
-        for pysat_assum_var in pysat_assum_vars:
-            self.rc2_solver.add_clause([pysat_assum_var])
+        for pysat_var in pysat_vars:
+            self.rc2_solver.add_clause([pysat_var])
 
         self._solved_assumption = True
 
     def solve(self, assumptions=None):
         """
             Call the MaxSAT RC2 solver.
+            Assumption are treated as hard constraints.
+
             1. In case, a new solve call is executed without changing the assumptions
                then the rc2 is not restarted and computes a solution
 
-            2. In case new assumptions are added, then the solver is restarted with the
-               base constraints of the input model.
+            2. In case new assumptions are added and they are not a superset of
+               previous assumption variables, then the solver is restarted with the
+               base constraints of the input model and solved with the current assumptions.
 
             Arguments:
-            - assumptions: list of CPMpy Boolean variables that are assumed to be true.
+            - assumptions: list of (negated) CPMpy Boolean variables that are assumed to be true.
                            MaxSAT solver does not support incremental solving and assumptions.
                            CPM_rc2 adds the assumptions as hard clauses to the MaxSAT solver.
         """
@@ -206,24 +213,18 @@ class CPM_RC2(CPM_pysat):
 
     def solveAll(self, assumptions=None, block=0, solution_limit=None, display=None):
         """
-            A shorthand to (efficiently) compute all solutions, map them to CPMpy and optionally display the solutions.
-
-            It is just a wrapper around the use of `OrtSolutionPrinter()` in fact.
+            A short-hand to (efficiently) compute all solutions, map them to CPMpy and optionally display the solutions.
 
             Arguments:
                 - display: either a list of CPMpy expressions, called with the variables before value-mapping
                         default/None: nothing displayed
                 - solution_limit: stop after this many solutions (default: None)
+                - block: 
 
             Returns: number of solutions found
         """
-        if display:
-            pysat_rc2_vars = set()
-            for cpm_var in display:
-                pysat_var = self.solver_var(cpm_var)
-                pysat_rc2_vars.add(pysat_var)
-                pysat_rc2_vars.add(-pysat_var)
-
+        cb = RC2SolutionPrinter(self, display=display, solution_limit=solution_limit)
+        
         ## Previous call was made with assumptions or not
         if assumptions is not None:
             self._solved_assumption = True
@@ -233,7 +234,9 @@ class CPM_RC2(CPM_pysat):
 
         max_cost = None
         nbsols = 0
+
         for model in self.rc2_solver.enumerate(block=block):
+            cb.on_solution_callback(model)
             cost = self.rc2_solver.cost
             if max_cost is None:
                 max_cost = cost
@@ -242,10 +245,9 @@ class CPM_RC2(CPM_pysat):
             elif solution_limit is not None and nbsols >= solution_limit:
                 return nbsols
 
-            if display:
-                sol = set(model) & pysat_rc2_vars
-                print(f'Solution {sol} with cost={cost}')
             nbsols +=1
+
+        return nbsols
 
     def objective(self, expr, minimize=False):
         """
@@ -260,18 +262,73 @@ class CPM_RC2(CPM_pysat):
             raise NotImplementedError("RC2 does not support minimizing the objective.")
 
         if isinstance(expr, Operator) and expr.name == "sum":
-            pysat_assum_vars = self.solver_vars(expr.args)
-            for pysat_assum_var in pysat_assum_vars:
-                self.rc2_solver.add_clause([pysat_assum_var], weight=1)
-                self.wcnf.append([pysat_assum_var], weight=1)
+            pysat_vars = self.solver_vars(expr.args)
+            for pysat_var in pysat_vars:
+                self.rc2_solver.add_clause([pysat_var], weight=1)
+                self.wcnf.append([pysat_var], weight=1)
         elif isinstance(expr, Operator) and expr.name == "wsum":
             weights, vars = expr.args
-            pysat_assum_vars = self.solver_vars(vars)
-            for weight, pysat_assum_var in zip(weights, pysat_assum_vars):
-                self.rc2_solver.add_clause([pysat_assum_var], weight=weight)
-                self.wcnf.append([pysat_assum_var], weight=weight)
+            pysat_vars = self.solver_vars(vars)
+            for weight, pysat_var in zip(weights, pysat_vars):
+                self.rc2_solver.add_clause([pysat_var], weight=weight)
+                self.wcnf.append([pysat_var], weight=weight)
         else:
             raise NotImplementedError(f"Expression {expr} not handled")
 
     def get_core(self):
         raise NotImplementedError("RC2 does not support unsat core extraction, check out the PySat solver for this functionality.")
+
+
+class RC2SolutionPrinter:
+    def __init__(self, solver, display=None, solution_limit=None, verbose=False):
+        self._solution_limit = solution_limit
+        # we only need the cpmpy->solver varmap from the solver
+        self._varmap = solver._varmap
+        # identify which variables to populate with their values
+        self._cpm_vars = []
+        self._display = display
+
+        if isinstance(display, (list,Expression)):
+            self._cpm_vars = get_variables(display)
+        elif callable(display):
+            # might use any, so populate all (user) variables with their values
+            self._cpm_vars = solver.user_vars
+
+    def on_solution_callback(self, sol):
+        """Called on each new solution."""
+        if len(self._cpm_vars):
+            # populate values before printing
+            for cpm_var in self._cpm_vars:
+                # it might be an NDVarArray
+                if hasattr(cpm_var, "flat"):
+                    for cpm_subvar in cpm_var.flat:
+                        lit = self.solver_var(cpm_subvar)
+                        if lit in sol:
+                            cpm_var._value = True
+                        elif -lit in sol:
+                            cpm_var._value = False
+                        else:
+                            # not specified...
+                            cpm_var._value = None
+
+                else:
+                    lit = self.solver_var(cpm_subvar)
+                    if lit in sol:
+                        cpm_var._value = True
+                    elif -lit in sol:
+                        cpm_var._value = False
+                    else:
+                        # not specified...
+                        cpm_var._value = None
+
+            if isinstance(self._display, Expression):
+                print(self._display.value())
+            elif isinstance(self._display, list):
+                # explicit list of expressions to display
+                print([v.value() for v in self._display])
+            else: # callable
+                self._display()
+
+        # check for count limit
+        if self.solution_count() == self._solution_limit:
+            self.StopSearch()
