23b88e92e88f28c39353d2dbbd5b5783977f8a31
Emilio Gamba <54995032+sourdough-bread@users.noreply.github.com>
2022-12-20 15:51:34 +0100

    191 mus tool crashes with only 1 soft constraint (#196)
    * 1 soft -> assum var transformed to NDVarArray
    
    * transforming assumvars to cpmarray
    
    * Updating tests
    
    * Another MUS case with more vars
    
    * Another MUS case with more vars
    
    * Another MUS case with more vars
    
    * Test case for naive mus
    
    * Bug request reference

--- tests/test_tools_mus.py
+++ tests/test_tools_mus.py
@@ -1,4 +1,4 @@
-import time
+import unittest
 from unittest import TestCase
 
 from cpmpy import *
@@ -21,6 +21,38 @@ class MusTests(TestCase):
         self.assertEqual(mus(cons), cons[:3])
         self.assertEqual(mus_naive(cons), cons[:3])
 
+    def test_bug_191(self):
+        """
+        Original Bug request: https://github.com/CPMpy/cpmpy/issues/191
+        When assum is a single boolvar and candidates is a list (of length 1), it fails.
+        """
+        bv = boolvar(name="x")
+        hard = [~bv]
+        soft = [bv]
+
+        mus_cons = mus(soft=soft, hard=hard) # crashes
+        self.assertEqual(set(mus_cons), set(soft))
+        mus_naive_cons = mus_naive(soft=soft, hard=hard) # crashes
+        self.assertEqual(set(mus_naive_cons), set(soft))
+
+    def test_bug_191_many_soft(self):
+        """
+        Checking whether bugfix 191  doesn't break anything in the MUS tool chain,
+        when the number of soft constraints > 1.
+        """
+        x = intvar(-9, 9, name="x")
+        y = intvar(-9, 9, name="y")
+        hard = [x > 2]
+        soft = [
+            x + y < 6,
+            y == 4
+        ]
+
+        mus_cons = mus(soft=soft, hard=hard) # crashes
+        self.assertEqual(set(mus_cons), set(soft))
+        mus_naive_cons = mus_naive(soft=soft, hard=hard) # crashes
+        self.assertEqual(set(mus_naive_cons), set(soft))
+
     def test_wglobal(self):
         x = intvar(-9, 9, name="x")
         y = intvar(-9, 9, name="y")
@@ -40,3 +72,6 @@ class MusTests(TestCase):
 
         self.assertEqual(set(mus(cons)), set(cons[1:3]))
         self.assertEqual(set(mus_naive(cons)), set(cons[1:3]))
+
+if __name__ == '__main__':
+    unittest.main()
