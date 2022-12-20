7c4a4a716953c72dd68f3bcf0141a359fb0ec0d4
Emilio Gamba <emilio.gamba@outlook.be>
2022-12-20 10:17:54 +0100

    Another MUS case with more vars

--- tests/test_tools_mus.py
+++ tests/test_tools_mus.py
@@ -29,12 +29,37 @@ class MusTests(TestCase):
         or a list of length 1, and extract this value I think. Or change to all(candidates)
         in the mus tool, but this might have some side effect by itself... to check.
         """
-        bv = boolvar()
+        bv = boolvar(name="x")
         hard = [~bv]
         soft = [bv]
 
         mus_cons = mus(soft=soft, hard=hard) # crashes
         self.assertEqual(set(mus_cons), set(soft))
+
+    def test_bug_191_many_soft(self):
+        """Source of the error is assum.implies(candidates). 
+        When assum is a single boolvar and candidates is a list (of length 1), it fails.
+
+        We should check when invoking bv.implies(rhs) rhs is either a single expression
+        or a list of length 1, and extract this value I think. Or change to all(candidates)
+        in the mus tool, but this might have some side effect by itself... to check.
+        """
+        x = intvar(-9, 9, name="x")
+        y = intvar(-9, 9, name="y")
+        hard = [x > 2]
+        soft = [
+            x < 0,
+            x == 4,
+            y == 4,
+            (x + y > 0) | (y < 0),
+            (y >= 0) | (x >= 0),
+            (y < 0) | (x < 0),
+            (y > 0) | (x < 0),
+            AllDifferent(x,y) # invalid for musx_assum
+        ]
+
+        mus_cons = mus(soft=soft, hard=hard) # crashes
+        self.assertEqual(set(mus_cons), set(soft[:1]))
 
     def test_wglobal(self):
         x = intvar(-9, 9, name="x")
