b43d5c43dcbf07e5b734b360b9170204dd8bb8cb
wout4 <wout.vanroose@gmail.com>
2023-01-13 11:45:21 +0100

    removed unused transformation

--- cpmpy/transformations/comparison.py
+++ cpmpy/transformations/comparison.py
@@ -1,7 +1,5 @@
 import copy
 
-from cpmpy.expressions.utils import *
-
 from .flatten_model import get_or_make_var
 from ..expressions.core import Comparison, Operator
 from ..expressions.variables import _NumVarImpl, _IntVarImpl
@@ -44,59 +42,3 @@ def only_numexpr_equality(constraints, supported=frozenset(["sum","wsum"])):
                 newcons.insert(i, lhscons[0])
 
     return newcons
-
-
-def nbc_main(constraints):
-    '''
-    Input must be a list of 2 elements, where the first one keeps track of assignment of helper IV's that should not be
-    affected by nested constraints. The second element keeps the regular constraints.
-
-    Do not call this function directly, instead call 'no_boolean_comparisons'
-    '''
-    aux, con = constraints
-    if isinstance(con, Comparison):
-        lhs, rhs = con.args
-        if con.name == '==' or con.name == '!=':
-            if not is_boolexpr(rhs) and lhs.is_bool():
-                #introduce aux var
-                iv = _IntVarImpl(0, 1)  # 0,1 is the valid domain, as this represents true or false
-                #deal with nested constraints
-                nbc_lhs = nbc_main([[], lhs])
-                nbc_rhs = nbc_main([[], rhs])
-                return [aux + nbc_lhs[0] + nbc_rhs[0] + [nbc_lhs[1] == iv], Comparison(con.name, iv, nbc_rhs[1])]
-        elif lhs.is_bool():
-            iv = _IntVarImpl(0, 1)
-            nbc_lhs = nbc_main([[], lhs])
-            nbc_rhs = nbc_main([[], rhs])
-            #other comparisons:
-            assert con.name == '<=' or con.name == '<' or con.name == '>=' or con.name == '>'
-            return [aux + nbc_lhs[0] + nbc_rhs[0] + [nbc_lhs[1] == iv], Comparison(con.name, iv, nbc_rhs[1])]
-
-        else:
-            #allowed comparison, just do the nested call
-            nbc_lhs = nbc_main([[], lhs])
-            nbc_rhs = nbc_main([[], rhs])
-            return[aux + nbc_lhs[0] + nbc_rhs[0], Comparison(con.name, nbc_lhs[1], nbc_rhs[1])]
-    elif isinstance(con, Operator):
-        nbc_args = []
-        for arg in con.args:
-            nbc_arg = nbc_main([[], arg])
-            aux += nbc_arg[0]
-            nbc_args += [nbc_arg[1]]
-        return [aux, Operator(con.name, nbc_args)]
-    else:
-        return [aux, con] #base case
-
-
-def no_boolean_comparisons(constraints):
-    '''
-    Transforms a set of constraints such that a boolexpr is never compared with a numerical expression
-    furthermore 2 boolean expressions are never compared with <,>,<= or >=
-    (!= and == are allowed between 2 boolean expressions)
-    Boolexpr == Numexpr will be transformed to Boolexpr == IV and IV == Numexpr
-    where IV is an additional intvar with domain 0,1
-    '''
-    if is_any_list(constraints):
-        return [no_boolean_comparisons(con) for con in constraints]
-    return flatlist(nbc_main([[], constraints]))
-    #main function expects a list of 2 elements, where the first element keeps track of helper variables.
