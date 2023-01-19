cd3677f872897aca747f600191d6983e23060ebd
Tias Guns <tias@ulyssis.org>
2023-01-19 19:22:10 +0100

    examples: rcpsp cosmetic changes

--- examples/rcpsp.py
+++ examples/rcpsp.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
 from cpmpy import *
 
-
 # toy model taken from :
 # https://python-mip.readthedocs.io/en/latest/examples.html#resource-constrained-project-scheduling
-#
+# ported to CPMpy by Guillaume Poveda
 
+# Data
 durations = cpm_array([0, 3, 2, 5, 4, 2, 3, 4, 2, 4, 6, 0])
 
-resource_needs = cpm_array([[0, 0], [5, 1], [0, 4], [1, 4], [1, 3], [3, 2], [3, 1], [2, 4],
-                            [4, 0], [5, 2], [2, 5], [0, 0]])
+resource_needs = cpm_array([[0, 0], [5, 1], [0, 4], [1, 4], [1, 3], [3, 2], [3, 1], [2, 4], [4, 0], [5, 2], [2, 5], [0, 0]])
 
 resource_capacities = cpm_array([6, 8])
 
-successors_link = cpm_array([[0, 1], [0, 2], [0, 3], [1, 4], [1, 5],
-                             [2, 9], [2, 10], [3, 8], [4, 6],
-                             [4, 7], [5, 9], [5, 10], [6, 8], [6, 9],
-                             [7, 8], [8, 11], [9, 11], [10, 11]])
+successors_link = cpm_array([[0, 1], [0, 2], [0, 3], [1, 4], [1, 5], [2, 9], [2, 10], [3, 8], [4, 6], [4, 7], [5, 9], [5, 10], [6, 8], [6, 9], [7, 8], [8, 11], [9, 11], [10, 11]])
+
 nb_resource = len(resource_capacities)
 nb_jobs = len(durations)
 max_duration = sum(durations)  # dummy upper bound, can be improved of course
+
+# Variables
 start_time = intvar(0, max_duration, shape=nb_jobs)
+
+
 model = Model()
-# precedence constraints.
+# Precedence constraints
 for j in range(successors_link.shape[0]):
     model += start_time[successors_link[j, 1]] >= start_time[successors_link[j, 0]]+durations[successors_link[j, 0]]
+
+# Cumulative resource constraint
 for r in range(nb_resource):
     model += Cumulative(start=start_time, duration=durations, end=start_time+durations,
                         demand=resource_needs[:, r], capacity=resource_capacities[r])
+
 makespan = max(start_time)
 model.minimize(makespan)
+
 model.solve(solver="ortools")
-print(start_time.value())
+print("Start times:", start_time.value())
 
 
 def check_solution(start_time_values):
@@ -49,3 +54,4 @@ def check_solution(start_time_values):
 
 
 check_solution(start_time.value())
+print("Solution passed all checks.")
