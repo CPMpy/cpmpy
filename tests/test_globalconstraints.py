64a9506ce9a7555394a1280a5ca84362483380aa
wout4 <wout.vanroose@gmail.com>
2023-02-23 09:48:06 +0100

    added missing tests for globals

--- tests/test_globalconstraints.py
+++ tests/test_globalconstraints.py
@@ -144,6 +144,21 @@ class TestGlobal(unittest.TestCase):
         self.assertTrue(model.solve())
         self.assertNotEqual(str(max(iv.value())), '4')
 
+    def test_element(self):
+        iv = cp.intvar(-8, 8, 3)
+        idx = cp.intvar(-8, 8)
+        constraints = [cp.Element(iv,idx) == 8]
+        model = cp.Model(constraints)
+        self.assertTrue(model.solve())
+        self.assertTrue(iv.value()[idx.value()] == 8)
+        self.assertTrue(cp.Element(iv,idx).value() == 8)
+
+
+    def test_Xor(self):
+        bv = cp.boolvar(5)
+        self.assertTrue(cp.Model(cp.Xor(bv)).solve())
+        self.assertTrue(cp.Xor(bv).value())
+
     def test_minimax_python(self):
         from cpmpy import min,max
         iv = cp.intvar(1,9, 10)
