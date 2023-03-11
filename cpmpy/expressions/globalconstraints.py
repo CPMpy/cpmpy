a0048544dce3954840524b129a423407a458c323
Brad Ediger <brad@bradediger.com>
2023-03-11 15:39:05 -0600

    remove redundant constraint

--- cpmpy/expressions/globalconstraints.py
+++ cpmpy/expressions/globalconstraints.py
@@ -267,7 +267,7 @@ class Inverse(GlobalConstraint):
     def value(self):
         fwd = argval(self.args[0])
         rev = argval(self.args[1])
-        return len(fwd) == len(rev) and all(rev[x] == i for i, x in enumerate(fwd))
+        return all(rev[x] == i for i, x in enumerate(fwd))
 
 class Table(GlobalConstraint):
     """The values of the variables in 'array' correspond to a row in 'table'
