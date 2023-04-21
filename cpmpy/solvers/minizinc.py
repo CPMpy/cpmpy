3b7a23a5b1d2c9582511bab0aba9f837ae2ccdc0
Wout <wout.vanroose@gmail.com>
2023-04-21 11:49:37 +0200

    minizinc exitstatus fix when time-out (#277)
    * minizinc post_solve() fix when time out
    * code cleanup with helper function

--- cpmpy/solvers/minizinc.py
+++ cpmpy/solvers/minizinc.py
@@ -223,13 +223,17 @@ class CPM_minizinc(SolverInterface):
 
         # new status, translate runtime
         self.cpm_status = SolverStatus(self.name)
+        runtime = 0
         if 'time' in mzn_result.statistics:
-            time = mzn_result.statistics['time']
-            if isinstance(time, int):
-                self.cpm_status.runtime = time / 1000
-            elif isinstance(time, timedelta):
-                self.cpm_status.runtime = time.total_seconds()  # --output-time
-
+            self.cpm_status.runtime = self.mzn_time_to_seconds(mzn_result.statistics.get("time"))
+        else:
+            runtime += self.mzn_time_to_seconds(mzn_result.statistics.get("flatTime", 0))
+            runtime += self.mzn_time_to_seconds(mzn_result.statistics.get("initTime", 0))
+            runtime += self.mzn_time_to_seconds(mzn_result.statistics.get("solveTime", 0))
+            if runtime != 0:
+                self.cpm_status.runtime = runtime
+            else:
+                raise NotImplementedError #Please report on github, minizinc probably changed their time names/types
 
         # translate exit status
         mzn_status = mzn_result.status
@@ -246,11 +250,19 @@ class CPM_minizinc(SolverInterface):
             raise Exception("MiniZinc solver returned with status 'Error'")
         elif mzn_status == minizinc.result.Status.UNKNOWN:
             # means, no solution was found (e.g. within timeout?)...
-            self.cpm_status.exitstatus = ExitStatus.ERROR
+            self.cpm_status.exitstatus = ExitStatus.UNKNOWN
         else:
             raise NotImplementedError  # a new status type was introduced, please report on github
 
         return self.cpm_status
+
+    def mzn_time_to_seconds(self, time):
+        if isinstance(time, int):
+            return time / 1000
+        elif isinstance(time, timedelta):
+            return time.total_seconds()  # --output-time
+        else:
+            raise NotImplementedError #unexpected type for time
 
     async def _solveAll(self, display=None, time_limit=None, solution_limit=None, **kwargs):
         """ Special 'async' function because mzn.solutions() is async """
