3eb23d72604385e3f09729fc6e74504c2bf44c32
Verhaeghe Hélène <helene.verhaeghe27@gmail.com>
2023-05-31 10:55:07 +0200

    Update utils.py
    make sure the bound is a numerical value (tight bound of boolean value)

--- cpmpy/expressions/utils.py
+++ cpmpy/expressions/utils.py
@@ -139,4 +139,4 @@ def get_bounds(expr):
         return expr.get_bounds()
     else:
         assert is_num(expr), f"All Expressions should have a get_bounds function, `{expr}`"
-        return expr, expr
+        return int(expr), int(expr)
