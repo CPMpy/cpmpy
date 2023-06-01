62f827931cdcca849e4bf8737459fac94122e54f
hverhaeghe <helene.verhaeghe27@gmail.com>
2023-06-01 12:07:08 +0200

    removal of redundant code (first evaluation is never used)

--- cpmpy/expressions/globalconstraints.py
+++ cpmpy/expressions/globalconstraints.py
@@ -344,7 +344,6 @@ class IfThenElse(GlobalConstraint):
 
     def value(self):
         condition, if_true, if_false = self.args
-        condition_val = argval(condition)
         if argval(condition):
             return argval(if_true)
         else:
