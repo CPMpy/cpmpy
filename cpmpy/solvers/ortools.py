52dd4f4e6b15a7a104219a10ffd7d0530656a44e
ignace <ignace.bleukx@kuleuven.be>
2023-06-05 17:51:23 +0200

    cleaner interface

--- cpmpy/solvers/ortools.py
+++ cpmpy/solvers/ortools.py
@@ -326,7 +326,9 @@ class CPM_ortools(SolverInterface):
         :return: list of Expression
         """
         cpm_cons = toplevel_list(cpm_expr)
-        cpm_cons = decompose_in_tree(cpm_cons, supported={"min","max","element","alldifferent","xor","table", "cumulative","circuit"})
+        supported = {"min", "max", "element", "alldifferent", "xor", "table", "cumulative", "circuit"}
+        supported_nested = {"min", "max", "element"} # will be rewritten as non-nested in reify_rewrite
+        cpm_cons = decompose_in_tree(cpm_cons, supported, supported_nested)
         cpm_cons = flatten_constraint(cpm_cons)  # flat normal form
         cpm_cons = reify_rewrite(cpm_cons, supported=frozenset(['sum', 'wsum']))  # constraints that support reification
         cpm_cons = only_numexpr_equality(cpm_cons, supported=frozenset(["sum", "wsum", "sub"]))  # supports >, <, !=
