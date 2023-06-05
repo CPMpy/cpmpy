2f5b442a8ea0dfe3e1b4312f6c26b419c1ea2ae1
ignace <ignace.bleukx@kuleuven.be>
2023-06-05 17:51:20 +0200

    cleaner interface

--- cpmpy/solvers/gurobi.py
+++ cpmpy/solvers/gurobi.py
@@ -268,7 +268,9 @@ class CPM_gurobi(SolverInterface):
         # apply transformations, then post internally
         # expressions have to be linearized to fit in MIP model. See /transformations/linearize
         cpm_cons = toplevel_list(cpm_expr)
-        cpm_cons = decompose_in_tree(cpm_cons, supported={"min","max","alldifferent"}) # alldiff has specialized MIP decomp in linearize
+        supported = {"min", "max", "alldifferent"} # alldiff has a specialized MIP decomp in linearize
+        supported_nested = {"min", "max", "alldifferent"} # min and max will be rewritten as non-nested in reify_rewrite
+        cpm_cons = decompose_in_tree(cpm_cons, supported, supported_nested) # alldiff has specialized MIP decomp in linearize
         cpm_cons = flatten_constraint(cpm_cons)  # flat normal form
         cpm_cons = reify_rewrite(cpm_cons, supported=frozenset(['sum', 'wsum']))  # constraints that support reification
         cpm_cons = only_numexpr_equality(cpm_cons, supported=frozenset(["sum", "wsum", "sub"]))  # supports >, <, !=
