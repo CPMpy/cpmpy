1a3cc553673136b17cf2521609a2fd57001a4dc4
ignace <ignace.bleukx@kuleuven.be>
2023-06-21 13:48:31 +0200

    update tests

--- tests/test_trans_simplify.py
+++ tests/test_trans_simplify.py
@@ -47,16 +47,16 @@ class TransSimplify(unittest.TestCase):
         self.assertEqual(str(self.transform(expr)), '[1 + (iv[0]) >= 0]')
 
     def test_boolvar_comps(self):
-        num_args = {"<0": -1, "0": 0, "1": 1, ">0": 2}
+        num_args = {"<0": -1, "0": 0, "]0..1[": 0.5, "1": 1, ">0": 2}
         # test table from github (#add url)
         bv = self.bvs[0]
         test_dict = {
-            "==": {"<0": False,"0": ~bv,  "1": bv,   ">0": False},
-            "!=": {"<0": True, "0": bv,   "1": ~bv,  ">0": True},
-            ">":  {"<0": True, "0": bv,   "1": False,">0": False},
-            "<":  {"<0": False,"0": False,"1": ~bv,  ">0": True},
-            ">=": {"<0": True, "0": True, "1": bv,   ">0": False},
-            "<=": {"<0": False,"0": ~bv,  "1": True, ">0": True}
+            "==": {"<0": False,"0": ~bv,  "]0..1[":False,"1": bv,   ">0": False},
+            "!=": {"<0": True, "0": bv,   "]0..1[":True, "1": ~bv,  ">0": True},
+            ">":  {"<0": True, "0": bv,   "]0..1[":bv,   "1": False,">0": False},
+            "<":  {"<0": False,"0": False,"]0..1[":~bv,  "1": ~bv,  ">0": True},
+            ">=": {"<0": True, "0": True, "]0..1[":bv,   "1": bv,   ">0": False},
+            "<=": {"<0": False,"0": ~bv,  "]0..1[":~bv,  "1": True, ">0": True}
         }
 
         for op in test_dict:
@@ -83,4 +83,20 @@ class TransSimplify(unittest.TestCase):
         # very nested one
         expr = Operator("and", self.bvs[:1].tolist() + [BoolVal(False)]) == Operator("or", self.bvs)
         self.assertEqual(str(self.transform(expr)), '[not([or([bv[0], bv[1], bv[2]])])]')
+
+    # issue #322
+    def test_with_floats(self):
+        expr = self.bvs[0] == 1.0
+        self.assertEqual(str(self.transform(expr)), "[bv[0]]")
+        expr = cp.AllDifferent(self.ivs) == 4.2
+        self.assertEqual(str(self.transform(expr)), '[boolval(False)]')
+        expr = 0.0 == cp.AllDifferent(self.ivs)
+        self.assertEqual(str(self.transform(expr)), '[not([alldifferent(iv[0],iv[1],iv[2])])]')
+
+        expr = (self.ivs[0] <= self.ivs[1]) < 0.8
+        self.assertEqual(str(self.transform(expr)), '[not([(iv[0]) <= (iv[1])])]')
+
+        expr = (self.ivs[0] == self.ivs[1]) == 1.0
+        self.assertEqual(str(self.transform(expr)), '[(iv[0]) == (iv[1])]')
+
 
