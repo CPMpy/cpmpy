8b2ae06e17e805a30fcf1b4ad0bc7ba4363c0f3e
wout4 <wout.vanroose@gmail.com>
2023-07-12 11:42:02 +0200

    enforce element bounds at toplevel

--- cpmpy/expressions/globalfunctions.py
+++ cpmpy/expressions/globalfunctions.py
@@ -273,8 +273,8 @@ class Element(GlobalFunction):
         from .python_builtins import any
 
         arr, idx = self.args
-        return [(idx == i).implies(eval_comparison(cpm_op, arr[i], cpm_rhs)) for i in range(len(arr))] + \
-               [idx >= 0, idx < len(arr)], []
+        return [(idx == i).implies(eval_comparison(cpm_op, arr[i], cpm_rhs)) for i in range(len(arr))], \
+               [idx >= 0, idx < len(arr)]
 
     def __repr__(self):
         return "{}[{}]".format(self.args[0], self.args[1])
