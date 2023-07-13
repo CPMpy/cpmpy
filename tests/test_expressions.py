21569a61c91d8af6ab117f76246fd6266c0d67a9
wout4 <wout.vanroose@gmail.com>
2023-07-13 12:24:14 +0200

    add counting element test

--- tests/test_expressions.py
+++ tests/test_expressions.py
@@ -445,6 +445,26 @@ class TestBounds(unittest.TestCase):
             self.assertTrue(m.solve(solver="z3"))
             self.assertTrue(cons.value())
 
+    def test_reified_normalized_element(self):
+        iv = intvar(-5,5,2)
+        lis = intvar(-2,3,3)
+        bv = boolvar(2)
+        const = [(lis[iv[0]] == iv[1]).implies(bv[0])]
+        const2 = [(bv[1]).implies(bv[0]), (lis[iv[0]] == iv[1]) == bv[1]]
+        const3 = [(lis[iv[0]] == iv[1]).implies(bv[0]),
+                  (lis[iv[0]] == iv[1]).implies(bv[0]) | (~((lis[iv[0]] == iv[1]).implies(bv[0])))]
+        const4 = [(bv[1]).implies(bv[0]), (bv[1]).implies(bv[0]) | (~((bv[1]).implies(bv[0]))),
+                  (lis[iv[0]] == iv[1]) == bv[1]]
+
+        count1 = cp.Model(const).solveAll()
+        count2 = cp.Model(const2).solveAll()
+        count3 = cp.Model(const3).solveAll()
+        count4 = cp.Model(const4).solveAll()
+
+        self.assertEqual(count1,count2)
+        self.assertEqual(count3,count2)
+        self.assertEqual(count3,count4)
+
     def test_not_operator(self):
         p = boolvar()
         q = boolvar()
