e646b930e67d041926d76a2db64df9d255e54412
ignace <ignace.bleukx@kuleuven.be>
2023-10-03 09:27:56 +0200

    remove order argument, rely on ordering of list

--- examples/advanced/quickxplain.py
+++ examples/advanced/quickxplain.py
@@ -9,15 +9,14 @@ from cpmpy.transformations.get_variables import get_variables
 from cpmpy.transformations.normalize import toplevel_list
 
 
-def quickXplain(soft, hard=[], order=lambda x : 0, solver="ortools"):
+def quickXplain(soft, hard=[], solver="ortools"):
     """
         Find a preferred minimal unsatisfiable subset of constraints
-        A partial order is imposed on the constraints using the `order` argument
-        Constraints with lower order are preferred to ones with higher order
+        A partial order is imposed on the constraints using the order of `soft`.
+        Constraints with lower index are preferred over ones with higher index
 
         :param: soft: soft constraints, list of expressions
         :param: hard: hard constraints which cannot be relaxed, optional, list of expressions
-        :param: order: optional function to compute order of constraints
         :param: solver: name of a solver, see SolverLookup.solvernames()
             "z3", "pysat" and "gurobi" are incremental, "ortools" restarts the solver
     """
@@ -30,11 +29,11 @@ def quickXplain(soft, hard=[], order=lambda x : 0, solver="ortools"):
     assert solver.solve(assumptions=assump) is False, "The model should be UNSAT!"
 
     dmap = dict(zip(assump, soft))
-    core = recurse_explain(list(assump),[],[],order, dmap, solver=solver)
+    core = recurse_explain(list(assump),[],[], solver=solver)
     return [dmap[a] for a in core]
 
 
-def recurse_explain(soft, hard, delta, order, dmap, solver):
+def recurse_explain(soft, hard, delta, solver):
 
     if len(delta) != 0 and solver.solve(assumptions=hard) is False:
         # conflict is in hard constraints, no need to recurse
@@ -44,14 +43,13 @@ def recurse_explain(soft, hard, delta, order, dmap, solver):
         # conflict is not in hard constraints, but only 1 soft constraint
         return list(soft) # base case of recursion
 
-    soft = sorted(soft, key=lambda a : order(dmap[a]))
     split = len(soft) // 2 # determine split point
     more_preferred, less_preferred = soft[:split], soft[split:] # split constraints into two sets
 
     # treat more preferred part as hard and find extra constants from less preferred
-    delta2 = recurse_explain(less_preffered, hard+more_preffered, more_preffered, order=order, dmap=dmap, solver=solver)
+    delta2 = recurse_explain(less_preferred, hard+more_preferred, more_preferred, solver=solver)
     # find which preferred constraints exactly
-    delta1 = recurse_explain(more_preffered, hard+delta2, delta2, order=order, dmap=dmap, solver=solver)
+    delta1 = recurse_explain(more_preferred, hard+delta2, delta2, solver=solver)
     return delta1 + delta2
 
 
@@ -73,12 +71,12 @@ if __name__ == "__main__":
     print(m)
     assert (m.solve() is False)
 
-    core1 = quickXplain(soft=m.constraints,
-                       order=lambda c: len(get_variables(c)), # prefer constraints with less variables
-                       solver="ortools")
+    #  prefer constraints with less variables
+    soft = sorted(m.constraints, key = lambda c: len(get_variables(c)))
+    core1 = quickXplain(soft, solver="ortools")
     print("Preferred MUS with constraints containing little variables:\n",core1)
 
-    core2 = quickXplain(soft=m.constraints,
-                       order=lambda c: -len(get_variables(c)), # prefer constraints with more variables
-                       solver="ortools")
+    # prefer constraints with more variables
+    soft = sorted(m.constraints, key = lambda c : -len(get_variables(c)))
+    core2 = quickXplain(soft=m.constraints, solver="ortools")
     print("Preferred MUS with constraints containing many variables:\n",core2)
