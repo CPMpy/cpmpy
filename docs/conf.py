9cd5893ffda45164f1cdfbdcd18eb1b31c2241c0
wout4 <wout.vanroose@gmail.com>
2023-10-23 13:46:14 +0200

    Add documentation

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
     'm2r2',
+    'sphinx_rtd_theme',
     'sphinx_automodapi.automodapi',
     'sphinx_automodapi.smart_resolver'
 ]
@@ -73,7 +74,7 @@ autodoc_default_flags = ['members', 'special-members']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
