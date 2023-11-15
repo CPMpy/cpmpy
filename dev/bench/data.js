1c063e54a296dfa7bbdc77b6935131735c74f153
github-action-benchmark <github@users.noreply.github.com>
2023-11-15 14:13:22 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 956f48b12a21c42e3b313550c62cc3cdddff393d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1700056810287,
+  "lastUpdate": 1700057602346,
   "repoUrl": "https://github.com/CPMpy/cpmpy",
   "entries": {
     "Python Benchmark with pytest-benchmark": [
@@ -94,6 +94,100 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0",
             "extra": "mean: 4.059492194999962 sec\nrounds: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "wout.vanroose@gmail.com",
+            "name": "wout4",
+            "username": "Wout4"
+          },
+          "committer": {
+            "email": "wout.vanroose@gmail.com",
+            "name": "wout4",
+            "username": "Wout4"
+          },
+          "distinct": true,
+          "id": "956f48b12a21c42e3b313550c62cc3cdddff393d",
+          "message": "add permissions to gh action",
+          "timestamp": "2023-11-15T15:09:34+01:00",
+          "tree_id": "e00de8d9fa9894defa971a4db20829b8f3f42a14",
+          "url": "https://github.com/CPMpy/cpmpy/commit/956f48b12a21c42e3b313550c62cc3cdddff393d"
+        },
+        "date": 1700057599814,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/Slant-02_c23.xml]",
+            "value": 1.1845930999315484,
+            "unit": "iter/sec",
+            "range": "stddev: 0.061310875038192096",
+            "extra": "mean: 844.1717244999865 msec\nrounds: 2"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/Dominoes-35-116_c23.xml]",
+            "value": 0.09729641033375404,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 10.277871470999997 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/AntimagicSquare-04_c23.xml]",
+            "value": 23.326346661322937,
+            "unit": "iter/sec",
+            "range": "stddev: 0.002497583804307923",
+            "extra": "mean: 42.86997936363883 msec\nrounds: 22"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/Coloring-csp-lei450-05b_c23.xml]",
+            "value": 0.97010578379795,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 1.0308154189999925 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/Slant-10_c23.xml]",
+            "value": 0.08092910247567081,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 12.356494381000005 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/Slant-02_c23.xml]",
+            "value": 1.6027160986110092,
+            "unit": "iter/sec",
+            "range": "stddev: 0.008032708992638964",
+            "extra": "mean: 623.9408220000087 msec\nrounds: 2"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/Dominoes-35-116_c23.xml]",
+            "value": 0.7527664842020426,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 1.3284332140000004 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/AntimagicSquare-04_c23.xml]",
+            "value": 456.190331484124,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00007784345085924845",
+            "extra": "mean: 2.192067501182456 msec\nrounds: 423"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/Coloring-csp-lei450-05b_c23.xml]",
+            "value": 3.388817114307048,
+            "unit": "iter/sec",
+            "range": "stddev: 0.004543400880049008",
+            "extra": "mean: 295.0882170000142 msec\nrounds: 4"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/Slant-10_c23.xml]",
+            "value": 0.41403042437404797,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 2.415281441000019 sec\nrounds: 1"
           }
         ]
       }
