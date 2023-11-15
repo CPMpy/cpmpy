2d00bbb333d9a4e8e2d4941f348bf0ce78b25b91
github-action-benchmark <github@users.noreply.github.com>
2023-11-15 14:00:10 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for ae413d8bb19a0c7ae63bd0e3814710ea79dae62f

---
+++ dev/bench/data.js
@@ -0,0 +1,102 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1700056810287,
+  "repoUrl": "https://github.com/CPMpy/cpmpy",
+  "entries": {
+    "Python Benchmark with pytest-benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "wout.vanroose@gmail.com",
+            "name": "wout4",
+            "username": "Wout4"
+          },
+          "committer": {
+            "email": "wout.vanroose@gmail.com",
+            "name": "wout4",
+            "username": "Wout4"
+          },
+          "distinct": true,
+          "id": "ae413d8bb19a0c7ae63bd0e3814710ea79dae62f",
+          "message": "test gh_pages with small dataset",
+          "timestamp": "2023-11-15T14:53:17+01:00",
+          "tree_id": "cbdcc82024893770b768a7b7f37421ae2b5fa517",
+          "url": "https://github.com/CPMpy/cpmpy/commit/ae413d8bb19a0c7ae63bd0e3814710ea79dae62f"
+        },
+        "date": 1700056805972,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/Slant-02_c23.xml]",
+            "value": 0.5171662582363407,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 1.9336141599999905 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/Dominoes-35-116_c23.xml]",
+            "value": 0.059013994538063164,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 16.94513323199999 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/AntimagicSquare-04_c23.xml]",
+            "value": 11.424658355484665,
+            "unit": "iter/sec",
+            "range": "stddev: 0.01687445945534439",
+            "extra": "mean: 87.52996972727219 msec\nrounds: 11"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/Coloring-csp-lei450-05b_c23.xml]",
+            "value": 0.41999032291056837,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 2.3810072410000203 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_solve_ortools[benchmarks/instances/sat/Slant-10_c23.xml]",
+            "value": 0.027674582803157606,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 36.134239389000015 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/Slant-02_c23.xml]",
+            "value": 0.9483514998867765,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 1.054461346999915 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/Dominoes-35-116_c23.xml]",
+            "value": 0.4388706167448268,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 2.278575875999991 sec\nrounds: 1"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/AntimagicSquare-04_c23.xml]",
+            "value": 267.60658561897094,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005868092629436828",
+            "extra": "mean: 3.7368288141602024 msec\nrounds: 226"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/Coloring-csp-lei450-05b_c23.xml]",
+            "value": 1.898184192161286,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0014221381080766146",
+            "extra": "mean: 526.819264500034 msec\nrounds: 2"
+          },
+          {
+            "name": "benchmarks/test_benchmarks.py::test_transform_ortools[benchmarks/instances/sat/Slant-10_c23.xml]",
+            "value": 0.246336229253425,
+            "unit": "iter/sec",
+            "range": "stddev: 0",
+            "extra": "mean: 4.059492194999962 sec\nrounds: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
