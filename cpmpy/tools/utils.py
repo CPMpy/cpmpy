0b34341f47a3be25b23dcb1d3fddc721543ef198
ignace <ignace.bleukx@kuleuven.be>
2023-11-24 09:43:51 +0100

    optional nameing

--- cpmpy/tools/utils.py
+++ cpmpy/tools/utils.py
@@ -2,16 +2,17 @@ import cpmpy as cp
 from cpmpy.transformations.normalize import toplevel_list
 
 
-def make_assump_model(soft, hard=[]):
+def make_assump_model(soft, hard=[], name=None):
     """
         Construct implied version of all soft constraints
         Can be used to extract cores (see tools.mus)
+        Provide name for assumption variables with `name` param
     """
     # ensure toplevel list
     soft2 = toplevel_list(soft, merge_and=False)
 
     # make assumption variables
-    assump = cp.boolvar(shape=(len(soft),))
+    assump = cp.boolvar(shape=(len(soft),), name=name)
 
     # hard + implied soft constraints
     model = cp.Model(hard + [assump.implies(soft2)])  # each assumption variable implies a candidate
