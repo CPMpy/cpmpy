937b057d65b1f9697d7aa096db2863972077fd7c
wout4 <wout.vanroose@gmail.com>
2023-12-21 13:17:29 +0100

    add local runnable benchmarks

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -1,13 +1,11 @@
-import random
-import pytest
+import timeit
 import cpmpy as cp
 import os
 from os.path import join
 import glob
 from cpmpy.tools.read_xcsp import XCSPParser
 
-MODELS = []
-SOLVERNAMES = [name for name, solver in cp.SolverLookup.base_solvers() if solver.supported()]
+alltimes = {}
 xmlmodels = []
 cwd = os.getcwd()
 if 'y' in cwd[-2:]:
@@ -16,15 +14,18 @@ if 'y' in cwd[-2:]:
 else:
     xmlmodels.extend(glob.glob(join('instances', 'sat', "*.xml")))
     xmlmodels.extend(glob.glob(join('instances', 'unsat', "*.xml")))
-random.shuffle(xmlmodels)
 
-#for subdividing the models (use 'instances' directory for xmlmodels above)
-"""for xmlmodel in xmlmodels:
+#for subdividing the models (use 'instances' directory for xmlmodels)
+'''if 'y' in cwd[-2:]:
+    xmlmodels.extend(glob.glob(join("benchmarks", 'instances', "*.xml")))
+else:
+    xmlmodels.extend(glob.glob(join('instances', "*.xml")))
+for xmlmodel in xmlmodels:
     try:
         model = XCSPParser(xmlmodel)
         s = cp.SolverLookup.get('ortools', model)
-        sat = s.solve(num_search_workers=1, time_limit=300)
-        if (s.status().runtime > 290):
+        sat = s.solve(num_search_workers=1, time_limit=25)
+        if (s.status().runtime > 22):
             #slow models
             name = os.path.basename(xmlmodel)
             os.rename(xmlmodel, xmlmodel[:len(xmlmodel) - len(name)] + 'slow\\' + name)
@@ -36,25 +37,28 @@ random.shuffle(xmlmodels)
             os.rename(xmlmodel, xmlmodel[:len(xmlmodel) - len(name)] + 'unsat\\' + name)
     except Exception:
         name = os.path.basename(xmlmodel)
-        os.rename(xmlmodel, xmlmodel[:len(xmlmodel) - len(name)] + 'unsupported\\' + name)"""
+        os.rename(xmlmodel, xmlmodel[:len(xmlmodel) - len(name)] + 'unsupported\\' + name)
+'''
 
-
-
-@pytest.mark.parametrize('xmlmodel', xmlmodels, ids=str)
-@pytest.mark.ortools_solve
-def test_solve_ortools(xmlmodel, benchmark):
+for xmlmodel in xmlmodels:
     model = XCSPParser(xmlmodel)
     s = cp.SolverLookup.get('ortools',model)
-    result = benchmark(s.solve,num_search_workers=6,time_limit=1800)
+
+    result = None
+    def solv():
+        global result
+        result = s.solve(num_search_workers=1,time_limit=1800)
+
+    t_solve = timeit.timeit(stmt=solv, number=1)
+    timings = s.timings
+    timings['solve'] = t_solve
+    alltimes[xmlmodel] = timings
     if 'unsat' in xmlmodel:
         assert not result #should be unsat
     else:
         assert result #should be sat
 
+    if result is None:
+        assert False
 
-
-@pytest.mark.parametrize('xmlmodel', xmlmodels, ids=str)
-@pytest.mark.transform_ortools
-def test_transform_ortools(xmlmodel, benchmark):
-    model = XCSPParser(xmlmodel)
-    s = benchmark(cp.SolverLookup.get,'ortools',model)
+print(alltimes)
