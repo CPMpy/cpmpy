56d4e931ffbf4235bbdd6ca4a21852b999be80c3
wout4 <wout.vanroose@gmail.com>
2023-12-22 17:07:26 +0100

    linebreaks are annoying

--- cpmpy/solvers/ortools.py
+++ cpmpy/solvers/ortools.py
@@ -42,16 +42,15 @@ from ..transformations.comparison import only_numexpr_equality
 
 class CPM_ortools(SolverInterface):
     """Interface to the python 'ortools' CP-SAT API
-
     Requires that the 'ortools' python package is installed:
     $ pip install ortools
 
-    See detailed installation instructions at: \n
+    See detailed installation instructions at:
     https://developers.google.com/optimization/install
 
     Creates the following attributes (see parent constructor for more):
-    :ort_model: the ortools.sat.python.cp_model.CpModel() created by _model()
-    :ort_solver: the ortools cp_model.CpSolver() instance used in solve()
+    ..ort_model:: the ortools.sat.python.cp_model.CpModel() created by _model()
+    ..ort_solver:: the ortools cp_model.CpSolver() instance used in solve()
 
     The `DirectConstraint`, when used, calls a function on the `ort_model` object.
     """
