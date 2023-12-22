0cf19be0b28d486f519cc621a728e69030509c06
wout4 <wout.vanroose@gmail.com>
2023-12-22 17:04:46 +0100

    linebreaks are annoying

--- cpmpy/solvers/ortools.py
+++ cpmpy/solvers/ortools.py
@@ -46,12 +46,12 @@ class CPM_ortools(SolverInterface):
     Requires that the 'ortools' python package is installed:
     $ pip install ortools
 
-    See detailed installation instructions at: |
+    See detailed installation instructions at: \n
     https://developers.google.com/optimization/install
 
-    Creates the following attributes (see parent constructor for more): <\br>
-    **ort_model:** the ortools.sat.python.cp_model.CpModel() created by _model()
-    **ort_solver:** the ortools cp_model.CpSolver() instance used in solve()
+    Creates the following attributes (see parent constructor for more):
+    :ort_model: the ortools.sat.python.cp_model.CpModel() created by _model()
+    :ort_solver: the ortools cp_model.CpSolver() instance used in solve()
 
     The `DirectConstraint`, when used, calls a function on the `ort_model` object.
     """
