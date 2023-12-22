632f9fc6593a29617bf3d8aeb7abe65c2ad88699
wout4 <wout.vanroose@gmail.com>
2023-12-22 16:02:02 +0100

    try to force line breaks again

--- cpmpy/solvers/ortools.py
+++ cpmpy/solvers/ortools.py
@@ -52,7 +52,7 @@ class CPM_ortools(SolverInterface):
 
     Creates the following attributes (see parent constructor for more):
     ort_model: the ortools.sat.python.cp_model.CpModel() created by _model()
-    ort_solver: the ortools cp_model.CpSolver() instance used in solve()
+    |  ort_solver: the ortools cp_model.CpSolver() instance used in solve()
 
     The `DirectConstraint`, when used, calls a function on the `ort_model` object.
     """
