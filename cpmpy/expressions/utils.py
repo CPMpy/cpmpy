f6fb9e254b05af665d35daa8483682e65abaac87
wout4 <wout.vanroose@gmail.com>
2024-03-01 16:52:45 +0100

    remove unused import

--- cpmpy/expressions/utils.py
+++ cpmpy/expressions/utils.py
@@ -24,8 +24,8 @@ Internal utilities for expression handling.
 
 import numpy as np
 import math
-from collections.abc import Iterable # for _flatten
-from itertools import chain, combinations
+from collections.abc import Iterable  # for flatten
+from itertools import combinations
 from cpmpy.exceptions import IncompleteFunctionError
 
 
