d134ccd182eaeb976bf698195b883e7542555614
Tias Guns <tias@ulyssis.org>
2024-10-11 11:35:39 +0200

    restructure based on the explanation in comments (and some efficiency things too)

--- cpmpy/transformations/safening.py
+++ cpmpy/transformations/safening.py
@@ -1,7 +1,7 @@
 from copy import copy
 
 from ..expressions.variables import _NumVarImpl, boolvar, intvar, NDVarArray, cpm_array
-from ..expressions.core import Operator, BoolVal
+from ..expressions.core import Expression, Operator, BoolVal
 from ..expressions.utils import get_bounds, is_num
 from ..expressions.globalfunctions import GlobalFunction, Element
 from ..expressions.globalconstraints import DirectConstraint
@@ -52,8 +52,8 @@ def no_partial_functions(lst_of_expr, _toplevel=None, _nbc=None):
         toplevel_call = True
         _toplevel = []
     else:
-        assert isinstance(_toplevel, list), f"_toplevel argument must be of type list but got {type(_toplevel)}"
         toplevel_call = False
+        assert isinstance(_toplevel, list), f"_toplevel argument must be of type list but got {type(_toplevel)}"
 
     new_lst = []
     for cpm_expr in lst_of_expr:
@@ -64,66 +64,65 @@ def no_partial_functions(lst_of_expr, _toplevel=None, _nbc=None):
         elif isinstance(cpm_expr, (list,tuple)):
             new_lst.append(no_partial_functions(cpm_expr, _toplevel, _nbc))
 
-        elif isinstance(cpm_expr, NDVarArray):
-            safened = no_partial_functions(cpm_expr, _toplevel, _nbc)
-            new_lst.append(cpm_array(safened))
+        elif isinstance(cpm_expr, NDVarArray):  # TODO efficiency: and cpm_expr.has_subexpr()
+            # efficiency: flatten into single iterator, then reshape to n-dimensional again
+            new_cpm_expr = cpm_array(no_partial_functions(cpm_expr.flat, _toplevel, _nbc)).reshape(cpm_expr.shape)
+            new_lst.append(new_cpm_expr)
 
-
-        elif isinstance(cpm_expr, Operator) and (cpm_expr.name == "div" or cpm_expr.name == "mod"):
-            safe_args = no_partial_functions(cpm_expr.args, _toplevel, _nbc=_nbc)
-            x, y = safe_args
-            lb, ub = get_bounds(y)
-            expr = Operator(cpm_expr.name, [x,y])
-
-            if lb <= 0 <= ub:
-                assert lb != 0 or ub != 0, "domain of divisor contains only 0" # TODO, I guess we can fix this by making nbc = False?
-
-                if lb == 0:
-                    is_safe, safe_expr, extra_cons = _safen_range(expr, (1, ub), 1)
-                elif ub == 0:
-                    is_safe, safe_expr, extra_cons = _safen_range(expr, (lb, -1), 1)
-                else: # proper hole
-                    is_safe, safe_expr, extra_cons = _safen_hole(expr, 0, 1)
-
-                _nbc.append(is_safe)
-                new_lst.append(safe_expr)
-                _toplevel += extra_cons
-
-            else: # already safe
-                new_lst.append(expr)
-
-
-        elif isinstance(cpm_expr, GlobalFunction) and cpm_expr.name == "element":
-            safe_args = no_partial_functions(cpm_expr.args, _toplevel, _nbc=_nbc)
-            arr, idx = safe_args
-            lb, ub = get_bounds(idx)
-            expr = Element(arr,idx)
-
-            if lb < 0 or ub >= len(arr): # index can be out of bounds
-                is_safe, safe_expr, extra_cons = _safen_range(expr, (0, len(arr)-1), 1)
-                _nbc.append(is_safe)
-                new_lst.append(safe_expr)
-                _toplevel += extra_cons
-            else:
-                new_lst.append(expr)
-
-
-        elif isinstance(cpm_expr, DirectConstraint): # do nothing
+        elif isinstance(cpm_expr, DirectConstraint):  # do not recurse into args
             new_lst.append(cpm_expr)
 
-        elif cpm_expr.is_bool(): # reached Boolean (sub)expression
-            new_exprs = []
-            safe_args = no_partial_functions(cpm_expr.args, _toplevel, _nbc=new_exprs)
-            cpm_expr = copy(cpm_expr)
-            cpm_expr.args = safe_args
-            new_lst.append(cpm_expr & cpm_all(new_exprs))
+        else:
+            assert isinstance(cpm_expr, Expression), f"each `cpm_expr` should be an Expression at this point, not {type(cpm_expr)}"
+            args = cpm_expr.args
 
-        else: # numerical subexpression or toplevel, just recurse
-            safe_args = no_partial_functions(cpm_expr.args, _toplevel, _nbc=_nbc)
-            cpm_expr = copy(cpm_expr)
-            cpm_expr.args = safe_args
+            if cpm_expr.is_bool():  # a Boolean context, create a new _nbc
+                _nbc = []
+
+            # recurse over the arguments of the expression
+            # TODO efficiency: if cpm_expr.has_subexpr()
+            new_args = no_partial_functions(args, _toplevel, _nbc=_nbc)
+            if any((a1 is not a2) for a1,a2 in zip(new_args,args)):  # efficiency (hopefully): only copy if an arg changed
+                cpm_expr = copy(cpm_expr)
+                cpm_expr.args = new_args
+                args = new_args
+
+            if cpm_expr.is_bool() and len(_nbc) != 0:
+                # a Boolean context, conjoin my Boolean expression with _nbc
+                # in `b <-> (Arr[idx] == 5)`, this would trigger for cpm_expr (Arr[idx] == 5)
+                cpm_expr = cpm_all(_nbc) & cpm_expr
+
+            elif isinstance(cpm_expr, GlobalFunction) and cpm_expr.name == "element":
+                arr, idx = args
+                lb, ub = get_bounds(idx)
+
+                if lb < 0 or ub >= len(arr): # index can be out of bounds
+                    guard, output_expr, extra_cons = _safen_range(cpm_expr, safe_range=(0, len(arr)-1), idx_to_safen=1)
+
+                    _nbc.append(guard)  # guard must be added to nearest Boolean context
+                    _toplevel += extra_cons  # any additional constraint that must be true
+                    cpm_expr = output_expr  # replace partial function by this (total) new output expression
+
+            elif cpm_expr.name == "div" or cpm_expr.name == "mod":
+                idx_to_safen = 1
+                lb, ub = get_bounds(args[idx_to_safen])
+
+                if lb <= 0 <= ub:
+                    assert lb != 0 or ub != 0, "domain of divisor contains only 0" # TODO, I guess we can fix this by making nbc = False?
+
+                    if lb == 0:
+                        guard, output_expr, extra_cons = _safen_range(cpm_expr, safe_range=(1, ub), idx_to_safen=idx_to_safen)
+                    elif ub == 0:
+                        guard, output_expr, extra_cons = _safen_range(cpm_expr, safe_range=(lb, -1), idx_to_safen=idx_to_safen)
+                    else:  # proper hole
+                        guard, output_expr, extra_cons = _safen_hole(cpm_expr, exclude=0, idx_to_safen=idx_to_safen)
+
+                    _nbc.append(guard)  # guard must be added to nearest Boolean context
+                    _toplevel += extra_cons  # any additional constraint that must be true
+                    cpm_expr = output_expr  # replace partial function by this (total) new output expression
+
+            # add the (potentially new) cpm_expr to the list
             new_lst.append(cpm_expr)
-
 
     if toplevel_call is True:
         return new_lst + _toplevel
@@ -146,53 +145,57 @@ def _safen_range(cpm_expr, safe_range, idx_to_safen):
 
     """
     lb, ub = safe_range
-    safe_expr = copy(cpm_expr)
-    unsafe_arg = safe_expr.args[idx_to_safen]
     is_safe, safe_arg = boolvar(), intvar(lb, ub)
-    safe_expr.args = [safe_arg if i == idx_to_safen else a for i,a in enumerate(cpm_expr.args)]
+
+    unsafe_arg = cpm_expr.args[idx_to_safen]
+    safe_expr = copy(cpm_expr)
+    safe_expr.args = [safe_arg if i == idx_to_safen else a for i,a in enumerate(safe_expr.args)]
 
     toplevel = [((unsafe_arg >= lb) & (unsafe_arg <= ub)).implies(is_safe),
                  (is_safe == (safe_arg == unsafe_arg)),
-                # avoid new solutions in aux vars (is this dangerous?)
+                 # avoid additional solutions when new var is unconstrained (should always work because newly defined?)
                  (~is_safe).implies(safe_arg == lb)]
 
     return is_safe, safe_expr, toplevel
+
 
 def _safen_hole(cpm_expr, exclude, idx_to_safen):
     """
         Safen expression where a single value of an argument can cause undefinedness.
         Examples include `div` where 0 has to be removed from the denominator
 
-
         Constructs an expression for each interval of safe values, and
-            introduces a new `result` variable (essentially does flattening)
+            introduces a new `output_expr` variable
 
         :param cpm_expr: The numerical expression to safen
         :param exclude: The domain value to exclude
         :param idx_to_safen: The index of unsafe argument in the expression
     """
-    result = intvar(*get_bounds(cpm_expr))
-
     unsafe_arg = cpm_expr.args[idx_to_safen]
-    lb, ub = get_bounds(unsafe_arg)
-    # introduce safe arguments
-    safe_lower, safe_upper = intvar(lb, exclude-1), intvar(exclude+1, ub)
-    lower_expr, upper_expr = copy(cpm_expr), copy(cpm_expr)
-    lower_expr.args = [safe_lower if i == idx_to_safen else a for i,a in enumerate(cpm_expr.args)]
-    upper_expr.args = [safe_upper if i == idx_to_safen else a for i,a in enumerate(cpm_expr.args)]
+    unsafe_lb, unsafe_ub = get_bounds(unsafe_arg)
+
+    # output when arg in [unsafe_lb..exclude-1]
+    safe_arg_lower = intvar(unsafe_lb, exclude-1)
+    output_expr_lower = copy(cpm_expr)
+    output_expr_lower.args = [safe_arg_lower if i == idx_to_safen else a for i,a in enumerate(cpm_expr.args)]
+    # output when arg in [exclude+1..unsafe_ub]
+    safe_arg_upper = intvar(exclude+1, unsafe_ub)
+    output_expr_upper = copy(cpm_expr)
+    output_expr_upper.args = [safe_arg_upper if i == idx_to_safen else a for i, a in enumerate(cpm_expr.args)]
 
     is_safe = boolvar()
+    output_expr = intvar(*get_bounds(cpm_expr))
 
     toplevel  =[
-        (unsafe_arg < exclude).implies(lower_expr == result),
-        (unsafe_arg > exclude).implies(upper_expr == result),
-        is_safe == ((unsafe_arg == safe_lower) | (unsafe_arg == safe_upper)),
-        # avoid new solutions in aux vars (is this dangerous?)
-        (unsafe_arg >= exclude).implies(safe_lower == exclude-1),
-        (unsafe_arg <= exclude).implies(safe_upper == exclude+1),
-        (~is_safe).implies(result == result.lb)
+        (unsafe_arg < exclude).implies(output_expr_lower == output_expr),
+        (unsafe_arg > exclude).implies(output_expr_upper == output_expr),
+        is_safe == ((unsafe_arg == safe_arg_lower) | (unsafe_arg == safe_arg_upper)),
+        # avoid additional solutions when new vars are unconstrained (should always work because newly defined?)
+        (unsafe_arg >= exclude).implies(safe_arg_lower == exclude-1),
+        (unsafe_arg <= exclude).implies(safe_arg_upper == exclude+1),
+        (~is_safe).implies(output_expr == output_expr.lb)
     ]
 
-    return is_safe, result, toplevel
+    return is_safe, output_expr, toplevel
 
 
