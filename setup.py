20c06a891b5aba8cd2516166a0b47d7dcae6c661
wout4 <wout.vanroose@gmail.com>
2025-01-14 11:50:01 +0100

    update minimal python version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'ortools>=5.0',
+        'ortools>=9.9',
         'numpy>=1.5',
     ],
     #extra dependency, only needed if minizinc is to be used.
@@ -41,12 +41,14 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
