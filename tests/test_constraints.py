342009eb0fc3567c121a80435b7a9cbe5db536de
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2025-02-04 08:02:00 +0100

    exclude mod and pow from pumpkin tests

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -45,6 +45,7 @@ EXCLUDE_OPERATORS = {"gurobi": {"mod"},
                      "pysat": {"sum", "wsum", "sub", "mod", "div", "pow", "abs", "mul","-"},
                      "pysdd": {"sum", "wsum", "sub", "mod", "div", "pow", "abs", "mul","-"},
                      "exact": {"mod","div"},
+                     "pumpkin": {"pow", "mod"},
                      }
 
 # Variables to use in the rest of the test script
