7e0ae8d61553889cea984e86521729b477b68ebe
wout4 <wout.vanroose@gmail.com>
2025-02-17 14:16:38 +0100

    remove silly name

--- cpmpy/transformations/flatten_model.py
+++ cpmpy/transformations/flatten_model.py
@@ -317,11 +317,11 @@ def get_or_make_var(expr, expr_dict=None):
         return (expr, [])
 
     if expr_dict is None:
-        expr_dict_getrmake_var = cpm_dict()
+        expr_dict = cpm_dict()
     else:
-        expr_dict_getrmake_var = expr_dict
-    if expr in expr_dict_getrmake_var:
-        return expr_dict_getrmake_var[expr], []
+        expr_dict = expr_dict
+    if expr in expr_dict:
+        return expr_dict[expr], []
 
     if is_any_list(expr):
         raise Exception(f"Expected single variable, not a list for: {expr}")
@@ -335,10 +335,10 @@ def get_or_make_var(expr, expr_dict=None):
             return flatexpr,flatcons
 
         bvar = _BoolVarImpl()
-        if flatexpr in expr_dict_getrmake_var:
-            return expr_dict_getrmake_var[flatexpr], []
+        if flatexpr in expr_dict:
+            return expr_dict[flatexpr], []
         else:
-            expr_dict_getrmake_var[flatexpr] = bvar
+            expr_dict[flatexpr] = bvar
         return bvar, [flatexpr == bvar] + flatcons
 
     else:
@@ -353,10 +353,10 @@ def get_or_make_var(expr, expr_dict=None):
                           UserWarning)
             lb, ub = math.floor(lb), math.ceil(ub)
         ivar = _IntVarImpl(lb, ub)
-        if flatexpr in expr_dict_getrmake_var:
-            return expr_dict_getrmake_var[flatexpr], []
+        if flatexpr in expr_dict:
+            return expr_dict[flatexpr], []
         else:
-            expr_dict_getrmake_var[flatexpr] = ivar
+            expr_dict[flatexpr] = ivar
         return ivar, [flatexpr == ivar] + flatcons
 
 def get_or_make_var_or_list(expr, expr_dict=None):
