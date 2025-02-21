2f9d93bc10b762e6bdacb97da9e7a77ba3e38f74
wout4 <wout.vanroose@gmail.com>
2025-02-21 14:31:01 +0100

    optimize has_subexpr for tables

--- cpmpy/expressions/globalconstraints.py
+++ cpmpy/expressions/globalconstraints.py
@@ -424,6 +424,13 @@ class ShortTable(GlobalConstraint):
                 return True
         return False
 
+    # specialisation to avoid recursing over big tables
+    def has_subexpr(self):
+        if not hasattr(self, '_has_subexpr'): # if _has_subexpr has not been computed before or has been reset
+            arr, tab = self.args # the table 'tab' can only hold constants, never a nested expression
+            self._has_subexpr = any(a.has_subexpr() for a in arr)
+        return self._has_subexpr
+
 class NegativeTable(GlobalConstraint):
     """The values of the variables in 'array' do not correspond to any row in 'table'
     """
@@ -443,6 +450,13 @@ class NegativeTable(GlobalConstraint):
         arrval = argvals(arr)
         tabval = argvals(tab)
         return arrval not in tabval
+
+    # specialisation to avoid recursing over big tables
+    def has_subexpr(self):
+        if not hasattr(self, '_has_subexpr'): # if _has_subexpr has not been computed before or has been reset
+            arr, tab = self.args # the table 'tab' can only hold constants, never a nested expression
+            self._has_subexpr = any(a.has_subexpr() for a in arr)
+        return self._has_subexpr
 
 
 # syntax of the form 'if b then x == 9 else x == 0' is not supported (no override possible)
