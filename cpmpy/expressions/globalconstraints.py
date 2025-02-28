f5f28f581af444f11071583cdb61a55b2d135927
wout4 <wout.vanroose@gmail.com>
2025-02-28 14:55:51 +0100

    remove has_subexpr from this pr

--- cpmpy/expressions/globalconstraints.py
+++ cpmpy/expressions/globalconstraints.py
@@ -388,12 +388,6 @@ class Table(GlobalConstraint):
         arrval = argvals(arr)
         return arrval in tab
 
-    # specialisation to avoid recursing over big tables
-    def has_subexpr(self):
-        if not hasattr(self, '_has_subexpr'): # if _has_subexpr has not been computed before or has been reset
-            arr, tab = self.args # the table 'tab' can only hold constants, never a nested expression
-            self._has_subexpr = any(a.has_subexpr() for a in arr)
-        return self._has_subexpr
 class ShortTable(GlobalConstraint):
     """
         Extension of the `Table` constraint where the `table` matrix may contain wildcards (STAR), meaning there are
@@ -424,12 +418,6 @@ class ShortTable(GlobalConstraint):
                 return True
         return False
 
-    # specialisation to avoid recursing over big tables
-    def has_subexpr(self):
-        if not hasattr(self, '_has_subexpr'): # if _has_subexpr has not been computed before or has been reset
-            arr, tab = self.args # the table 'tab' can only hold constants, never a nested expression
-            self._has_subexpr = any(a.has_subexpr() for a in arr)
-        return self._has_subexpr
 
 class NegativeTable(GlobalConstraint):
     """The values of the variables in 'array' do not correspond to any row in 'table'
@@ -450,13 +438,6 @@ class NegativeTable(GlobalConstraint):
         arrval = argvals(arr)
         tabval = argvals(tab)
         return arrval not in tabval
-
-    # specialisation to avoid recursing over big tables
-    def has_subexpr(self):
-        if not hasattr(self, '_has_subexpr'): # if _has_subexpr has not been computed before or has been reset
-            arr, tab = self.args # the table 'tab' can only hold constants, never a nested expression
-            self._has_subexpr = any(a.has_subexpr() for a in arr)
-        return self._has_subexpr
 
 
 # syntax of the form 'if b then x == 9 else x == 0' is not supported (no override possible)
