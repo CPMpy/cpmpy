28d37337be984dee6a7895bc43c46341cb954a99
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-04-25 11:39:15 +0000

    Add `int2bool` tests
    Bit similar to test_constraints, but I want to test the transformation
    directly. Might be mergeable.

---
+++ tests/test_int2bool.py
@@ -0,0 +1,65 @@
+import pytest
+
+import cpmpy as cp
+from cpmpy.transformations.flatten_model import flatten_constraint
+from cpmpy.expressions.core import Comparison, Operator, BoolVal
+
+from cpmpy.transformations.int2bool import int2bool
+from cpmpy.expressions.variables import _IntVarImpl, _BoolVarImpl  # to reset counters
+
+
+x = cp.intvar(0, 2, name="x")
+y = cp.intvar(0, 2, name="y")
+z = cp.intvar(0, 2, name="z")
+
+p = cp.boolvar(name="p")
+q = cp.boolvar(name="p")
+
+c = cp.intvar(2, 2, name="c")
+
+CONSTRAINTS = [
+    BoolVal(True),
+    BoolVal(False),
+    p.implies(q),
+] + [
+    con if antecedent is True else antecedent.implies(con)
+    for cmp in ("==", "!=", ">=", "<=", ">", "<")
+    for con in (
+        Comparison(cmp, c, 1),
+        Comparison(cmp, c, 2),
+        Comparison(cmp, x, 1),
+        Comparison(cmp, x, 5),
+        Comparison(cmp, Operator("sum", [[x, y, z]]), 3),
+        Comparison(cmp, Operator("wsum", [[2, 3, 5], [x, y, z]]), 12),
+        Comparison(cmp, Operator("wsum", [[2, 3, 5], [x, y, z]]), 100),
+        Comparison(cmp, Operator("wsum", [[2, 3, 5, 4], [x, y, z, c]]), 16),
+    )
+    for antecedent in (True, p, ~p)
+]
+
+
+@pytest.fixture()
+def setup():
+    _IntVarImpl.counter = 0
+    _BoolVarImpl.counter = 0
+    yield
+
+
+class TestTransInt2Bool:
+
+    @pytest.mark.parametrize("constraint", CONSTRAINTS, ids=str)
+    def test_transforms(self, constraint, setup):
+        flat = int2bool(flatten_constraint(constraint))
+        orig = constraint
+
+        flat_m = cp.Model(flat)
+        orig_m = cp.Model(orig)
+
+        assert (
+            flat_m.solve() == orig_m.solve()
+        ), f"{constraint} is not equisatisfiable with {flat}"
+        num_sols_orig = orig_m.solveAll()
+        num_sols_flat = flat_m.solveAll()
+        assert (
+            num_sols_flat == num_sols_orig
+        ), f"{constraint} has different number of sols with {flat}"
