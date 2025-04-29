91feeaea4672772a2182b1a027df41112ebeeacd
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2025-04-29 22:08:56 +0200

    check if __bool__ is called on expression

--- cpmpy/expressions/core.py
+++ cpmpy/expressions/core.py
@@ -430,6 +430,10 @@ class Expression(object):
             raise TypeError("Not operator is only allowed on boolean expressions: {0}".format(self))
         return Operator("not", [self])
 
+    def __bool__(self):
+        raise ValueError(f"__bool__ should not be called on a CPMPy expression {self} as it will always return True\n"
+                         "Do not use an expression as argument in an `if` statement and use cpmpy.any, cpmpy.max instead of python builtins\n"
+                         "To check whether an expression is part of a collection, make sure to convert the collection to a set first.   ")
 
 class BoolVal(Expression):
     """
