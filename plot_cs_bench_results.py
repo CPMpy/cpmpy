8886905512f99e4b52b52bf0e680b1fcea685276
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2025-05-16 17:22:10 +0200

    update plotting script

--- plot_cs_bench_results.py
+++ plot_cs_bench_results.py
@@ -3,25 +3,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("xcsp3_stats_cse.csv")
+df = pd.read_csv("xcsp3_stats.csv")
 df.set_index("instance", inplace=True)
 df.sort_values(by="nb_cons_trans", inplace=True)
-df['variant'] = 'cse'
+df['variant'] = 'master'
 df['instance'] = df.index
+df = df.sort_index()
 
-print(df)
+df_cse = pd.read_csv("xcsp3_stats_cse.csv")
+df_cse.set_index("instance", inplace=True)
+df_cse.sort_values(by="nb_cons_trans", inplace=True)
+df_cse['variant'] = 'cse'
+df_cse['instance'] = df_cse.index
+df_cse = df_cse.sort_index()
 
-df2 = pd.read_csv("xcsp3_stats.csv")
-df2.set_index("instance", inplace=True)
-df2.sort_values(by="nb_cons_trans", inplace=True)
-df2['variant'] = 'master'
-df2['instance'] = df2.index
+df_all = pd.concat([df, df_cse], axis=0)
 
-print(df2)
-
-df = pd.concat([df, df2], axis=1)
-
-print(df[df['nb_vars_trans'] < df['nb_vars_orig']])
 
 
 import seaborn as sns
@@ -30,27 +27,33 @@ import seaborn as sns
 
 fig, ax = plt.subplots(figsize=(5, 5))
 
-sns.scatterplot(data=df, x="nb_vars_orig", y="nb_vars_trans", hue="variant", ax=ax)
-ax.set_title("Number of variables")
-ax.set_xlabel("Number of variables before transformation")
-ax.set_ylabel("Number of variables after transformation")
+ax.scatter(x = df['nb_vars_trans'], y= df_cse['nb_vars_trans'])
+
+ax.set_title("Number of variables after transformation (ortools)")
+ax.set_xlabel("master")
+ax.set_ylabel("cse")
+
+ax.set_xscale("log")
+ax.set_yscale("log")
 
 lim = max(*ax.get_xlim(), *ax.get_ylim())
 ax.plot([0, lim], [0, lim], ls="--", c=".3")
+
 plt.show()
 
 
 fig, ax = plt.subplots(figsize=(5, 5))
 
-sns.scatterplot(data=df, x="nb_cons_orig", y="nb_cons_trans", hue="variant", ax=ax)
-ax.set_title("Number of constraints")
-ax.set_xlabel("Number of constraints before transformation")
-ax.set_ylabel("Number of constraints after transformation")
+ax.scatter(x = df['nb_cons_trans'], y= df_cse['nb_cons_trans'])
+
+ax.set_title("Number of constraints after transformation (ortools)")
+ax.set_xlabel("master")
+ax.set_ylabel("cse")
+
+ax.set_xscale("log")
+ax.set_yscale("log")
 
 lim = max(*ax.get_xlim(), *ax.get_ylim())
 ax.plot([0, lim], [0, lim], ls="--", c=".3")
+
 plt.show()
-
-# now compar cse vs master
-fig, ax = plt.subplots(figsize=(5, 5))
-
