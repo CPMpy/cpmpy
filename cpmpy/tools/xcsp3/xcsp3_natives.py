a7b7e0b81d8bd98274aa4d1fad26749a9e820947
ThomSerg <thomas.s2000@hotmail.com>
2025-05-21 14:34:45 +0200

    Fix natives init

--- cpmpy/tools/xcsp3/xcsp3_natives.py
+++ cpmpy/tools/xcsp3/xcsp3_natives.py
@@ -11,23 +11,23 @@ from cpmpy.expressions.globalconstraints import DirectConstraint
 
 class OrtNoOverlap2D(DirectConstraint):
     def __init__(self, arguments):
-        self._args = arguments
+        super().__init__("ortnooverlap2d", arguments)
 
     def callSolver(self, CPMpy_solver, Native_solver):
-        start_x, dur_x, end_x, start_y, dur_y, end_y = CPMpy_solver.solver_vars(self.args)
+        start_x, dur_x, end_x, start_y, dur_y, end_y = CPMpy_solver.solver_vars(self.args[0])
         intervals_x = [Native_solver.NewIntervalVar(s,d,e, f"xinterval_{s}-{d}-{d}") for s,d,e in zip(start_x,dur_x,end_x)]
         intervals_y = [Native_solver.NewIntervalVar(s,d,e, f"yinterval_{s}-{d}-{d}") for s,d,e in zip(start_y,dur_y,end_y)]
         return Native_solver.add_no_overlap_2d(intervals_x, intervals_y)
 
 class OrtSubcircuit(DirectConstraint):
     def __init__(self, arguments):
-        self._args = arguments
+        super().__init__("ortsubcircuit", arguments)
 
     def callSolver(self, CPMpy_solver, Native_solver):
-        N = len(self.args)
+        N = len(self.args[0])
         arcvars = cp.boolvar(shape=(N,N))
         # post channeling constraints from int to bool
-        CPMpy_solver += [b == (self.args[i] == j) for (i,j),b in np.ndenumerate(arcvars)]
+        CPMpy_solver += [b == (self.args[0][i] == j) for (i,j),b in np.ndenumerate(arcvars)]
         # post the global constraint
         #   posting arcs on diagonal (i==j) allows for subcircuits
         ort_arcs = [(i,j, CPMpy_solver.solver_var(b)) for (i,j),b in np.ndenumerate(arcvars)] # Allows for empty subcircuits
@@ -36,17 +36,16 @@ class OrtSubcircuit(DirectConstraint):
 
 class OrtSubcircuitWithStart(DirectConstraint):
     def __init__(self, arguments, start_index:int=0):
-        self._args = arguments
-        self.start_index = start_index
+        super().__init__("ortsubcircuitwithstart", (arguments, start_index))
 
     def callSolver(self, CPMpy_solver, Native_solver):
-        N = len(self.args)
+        N = len(self.args[0])
         arcvars = cp.boolvar(shape=(N,N))
         # post channeling constraints from int to bool
-        CPMpy_solver += [b == (self.args[i] == j) for (i,j),b in np.ndenumerate(arcvars)]
+        CPMpy_solver += [b == (self.args[0][i] == j) for (i,j),b in np.ndenumerate(arcvars)]
         # post the global constraint
         # posting arcs on diagonal (i==j) allows for subcircuits
-        ort_arcs = [(i,j,CPMpy_solver.solver_var(b)) for (i,j),b in np.ndenumerate(arcvars) if not ((i == j) and (i == self.start_index))] # The start index cannot self loop and thus must be part of the subcircuit.
+        ort_arcs = [(i,j,CPMpy_solver.solver_var(b)) for (i,j),b in np.ndenumerate(arcvars) if not ((i == j) and (i == self.args[1]))] # The start index cannot self loop and thus must be part of the subcircuit.
 
         return Native_solver.AddCircuit(ort_arcs)
         
@@ -55,11 +54,11 @@ class OrtSubcircuitWithStart(DirectConstraint):
 
 class ChocoSubcircuit(DirectConstraint):
     def __init__(self, arguments):
-        self._args = arguments
+        super().__init__("chocosubcircuit", arguments)
 
     def callSolver(self, CPMpy_solver, Native_solver):
         # Successor variables
-        succ = CPMpy_solver.solver_vars(self.args)
+        succ = CPMpy_solver.solver_vars(self.args[0])
         # Add an unused variable for the subcircuit length.
         subcircuit_length = CPMpy_solver.solver_var(cp.intvar(0, len(succ)))
         return Native_solver.sub_circuit(succ, 0, subcircuit_length)
@@ -68,10 +67,10 @@ class ChocoSubcircuit(DirectConstraint):
 
 class MinizincSubcircuit(DirectConstraint):
     def __init__(self, arguments):
-        self._args = arguments
+        super().__init__("minizincsubcircuit", arguments)
 
     def callSolver(self, CPMpy_solver, Native_solver):
         # minizinc is offset 1, which can be problematic here...
-        args_str = ["{}+1".format(CPMpy_solver._convert_expression(e)) for e in self.args]
+        args_str = ["{}+1".format(CPMpy_solver._convert_expression(e)) for e in self.args[0]]
         return "{}([{}])".format("subcircuit", ",".join(args_str))
         
