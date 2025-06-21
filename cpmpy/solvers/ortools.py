1b88842adaa98422f7abea43ee9e588ffa3c6d6d
Tias Guns <tias@ulyssis.org>
2025-06-20 22:38:00 -0400

    reset comment

--- cpmpy/solvers/ortools.py
+++ cpmpy/solvers/ortools.py
@@ -43,7 +43,7 @@
     Module details
     ==============
 """
-import sys
+import sys  # for stdout checking
 import numpy as np
 
 from .solver_interface import SolverInterface, SolverStatus, ExitStatus
