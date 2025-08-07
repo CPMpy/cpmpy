21a781938b9652391122b9d2258624f6d5bd14f6
Thomas Sergeys <thomas.s2000@hotmail.com>
2025-08-07 11:24:05 +0200

    Option to print solver versions

--- cpmpy/cli.py
+++ cpmpy/cli.py
@@ -7,6 +7,8 @@ def command_status(args):
 
 def command_version(args):
     print(f"CPMpy version: {__version__}")
+    if args.solvers:
+        cp.SolverLookup().print_version()
 
 def main():
     parser = argparse.ArgumentParser(description="CPMpy command line interface")
@@ -19,6 +21,7 @@ def main():
     # cpmpy version
     version_parser = subparsers.add_parser("version", help="Show version")
     version_parser.set_defaults(func=command_version)
+    version_parser.add_argument("--solvers", action='store_true')
 
     args = parser.parse_args()
     args.func(args)
