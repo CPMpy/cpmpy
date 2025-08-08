8e65be821d9c08370a31ec258eda2130e329e216
Senne Berden <senne_berden@hotmail.com>
2025-08-08 11:57:28 +0200

    Update decision_focused_learning.py
    Using more of PyEPO's code + using a generic PyEPO wrapper around CPMpy models

--- examples/advanced/decision_focused_learning.py
+++ examples/advanced/decision_focused_learning.py
@@ -1,70 +1,35 @@
-import numpy as np
 import pyepo
-from cpmpy import *
+import cpmpy as cp
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
 
-# CPMPy implementation of the knapsack problem
-class KnapsackProblem():
-    def __init__(self, weights, capacity):
-        self.weights = np.array(weights)
-        self.capacity = capacity
-        self.num_item = len(weights)
-        self.x = boolvar(shape=self.num_item, name="x")
-        self.model = Model(sum(self.x * self.weights) <= self.capacity)
 
-    def solve(self, y):
-        self.model.maximize(sum(self.x * y))
-        self.model.solve(solver="gurobi")
-        return self.x.value().astype(int)
+# Generic PyEPO wrapper for CPMpy models
+class optCPMpyModel(pyepo.model.opt.optModel):
+    def __init__(self, model, dvars, sense, solver=None, prec=None):
+        super().__init__()
+        self._model = model
+        self.x = dvars
+        self.s = cp.SolverLookup.get(solver, model)
+        self.modelSense = sense
+        self.prec = prec
 
+    def _getModel(self):
+        return None, None  # created by constructor
 
-# SPO+ surrogate loss
-class SPOPlus(torch.autograd.Function):
-    """
-    Implementation of the SPO+ surrogate loss from
-    Elmachtoub, A. N., & Grigas, P. (2021). Smart "predict, then optimize". Management Science.
-    """
-    @staticmethod
-    def forward(ctx, y_pred, y_true, sol_true, optimization_problem):
-        """
-        The forward pass computes and stores the solution for the SPO-perturbed cost
-        vector (for the backward pass).
+    def setObj(self, c):
+        if isinstance(c, torch.Tensor):
+            c = c.detach().cpu().numpy()
 
-        :param ctx: the context object
-        :param y_pred: the predicted cost vector
-        :param y_true: The true cost vector
-        :param sol_true: The true solution
-        :param optimization_problem: The parametric optimization problem
-        """
-        cost_vector = 2 * y_pred - y_true
-        sol_spo = torch.tensor(optimization_problem.solve(cost_vector.detach().cpu().numpy()))
-        ctx.save_for_backward(sol_spo, sol_true)
+        if self.modelSense == pyepo.EPO.MAXIMIZE:
+            self.s.maximize(cp.sum(self.x*c))
+        else:
+            self.s.minimize(cp.sum(self.x*c))
 
-        # We can just return a dummy value, rather than the actual training SPO+ loss, without affecting backprop
-        return torch.tensor(1.0)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        sol_spo, sol_true = ctx.saved_tensors
-        return -2 * (sol_true - sol_spo), None, None, None, None, None
-
-
-# Custom datawrapper
-class DataWrapper(Dataset):
-    def __init__(self, x, y, sol):
-        self.x = x if isinstance(x, torch.Tensor) else torch.from_numpy(x).float()
-        self.y = y if isinstance(x, torch.Tensor) else torch.from_numpy(y).float()
-        self.sol = sol if isinstance(x, torch.Tensor) else torch.from_numpy(sol).float()
-
-    def __len__(self):
-        return len(self.y)
-
-    def __getitem__(self, index):
-        return self.x[index], self.y[index], self.sol[index]
+    def solve(self):
+        self.s.solve()
+        return self.x.value().astype(int), self.s.objective_value()
 
 
 if __name__ == "__main__":
@@ -85,8 +50,11 @@ if __name__ == "__main__":
     weights = weights[0]
     capacity = 0.5 * sum(weights)
 
-    # Initialize knapsack problem
-    knapsack_problem = KnapsackProblem(weights, capacity)
+    # Initialize PyEPO-wrapped optimisation model (just the constraints, works for any CPMpy model)
+    m = cp.Model()
+    dv = cp.boolvar(shape=num_item, name="x")
+    m += weights * dv <= 0.2 * weights.sum()
+    optmodel = optCPMpyModel(m, dv, sense=pyepo.EPO.MAXIMIZE, solver="gurobi")
 
     # Initialize linear regressor
     pred_model = nn.Linear(num_feat, num_item)
@@ -94,55 +62,40 @@ if __name__ == "__main__":
     # Use ADAM optimizer
     optimizer = torch.optim.Adam(pred_model.parameters(), lr=lr)
 
-    # Compute ground-truth solutions
-    sols = np.array(
-        [knapsack_problem.solve(y[i]) for i in range(len(x))]
-    )
+    # Init Torch loss: PyEPO's SPO+ loss over the PyEPO optimisation model
+    spo_plus = pyepo.func.SPOPlus(optmodel, processes=1)
 
     # Split dataset into training and test sets
     n_training = int(num_data * 0.8)
-    x_train, y_train, sol_train = x[:n_training], y[:n_training], sols[:n_training]
-    x_test, y_test, sol_test = x[n_training:], y[n_training:], sols[n_training:]
+    x_train, y_train = x[:n_training], y[:n_training]
+    x_test, y_test = x[n_training:], y[n_training:]
 
     # Data loaders
-    train_dl = DataLoader(DataWrapper(x_train, y_train, sol_train), batch_size=32, shuffle=True)
-    test_dl = DataLoader(DataWrapper(x_test, y_test, sol_test), batch_size=len(x_test))
+    train_dataset = pyepo.data.dataset.optDataset(optmodel, x_train, y_train)
+    train_dl = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+    test_dataset = pyepo.data.dataset.optDataset(optmodel, x_test, y_test)
+    test_dl = torch.utils.data.DataLoader(test_dataset, batch_size=32)
 
     # Training
     pred_model.train()
     training_regrets = []
     for epoch in range(num_epochs):
         for data in train_dl:
-            x, y, sol = data
+            x, y, sol, obj = data
 
             # Forward pass
             y_pred = pred_model(x)
-            loss = 0
-            for i in range(len(x)):
-                loss += SPOPlus.apply(y_pred[i], y[i], sol[i], knapsack_problem)
+            loss = spo_plus(y_pred, y, sol, obj)
 
             # Backward pass
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        
-        # Calculate training regret after each epoch
-        total_regret = 0
-        pred_model.eval()  # Switch to eval mode for regret calculation
-        with torch.no_grad():
-            for data in train_dl:
-                x, y, sol = data
-                for i in range(len(x)):
-                    y_pred = pred_model(x[i])
-                    sol_pred = knapsack_problem.solve(y_pred.numpy())
 
-                    sol_i = sol[i].detach().cpu().numpy()
-                    y_i = y[i].detach().cpu().numpy()
-                    total_regret += (np.dot(sol_i, y_i) - np.dot(sol_pred, y_i)) / np.dot(sol_i, y_i)
-        
-        avg_train_regret = total_regret / len(x_train)
-        training_regrets.append(avg_train_regret)
-        print(f"Epoch {epoch + 1}, Training relative regret: {avg_train_regret:.4f}")
+        # Calculate training regret after each epoch
+        normalized_regret = pyepo.metric.regret(pred_model, optmodel, train_dl)
+        training_regrets.append(normalized_regret)
+        print(f"Epoch {epoch + 1}, Training normalized regret: {normalized_regret:.4f}")
         pred_model.train()  # Switch back to training mode
 
     # Plot training regrets
@@ -165,19 +118,3 @@ if __name__ == "__main__":
     plt.tight_layout()
     plt.grid(True, which='major', linestyle='--', linewidth=0.6, alpha=0.6)
     plt.show()
-
-    # Evaluate on the test set
-    pred_model.eval()
-    average_regret = 0
-    for data in test_dl:
-        x, y, sol = data
-        for i in range(len(x)):
-            y_pred = pred_model(x[i])
-            sol_pred = knapsack_problem.solve(y_pred.detach().numpy())
-
-            sol_i = sol[i].detach().cpu().numpy()
-            y_i = y[i].detach().cpu().numpy()
-            average_regret += (np.dot(sol_i, y_i) - np.dot(sol_pred, y_i)) / np.dot(sol_i, y_i)
-
-        average_regret /= len(x)
-    print(f"Average test regret: {average_regret}")
