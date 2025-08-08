853eb2787837c3fa86155c09e6701d2ea4016e84
Henk Bierlee <bierlee.henk@gmail.com>
2025-08-08 11:38:06 +0200

    Add `pindakaas` solver (#600)
    * Add initial pindakaas interface and submodule
    
    * Trigger action
    
    * Attempt to pull and test pindakaas from submodule
    
    * Update submodule to fork (maybe fixing permission issue)
    
    * Use HTTPS i/o SSH for cloning submodule
    
    * Use submodule checkout action + upgrade checkout action
    
    * Checkout submodules recursively
    
    * Move (required/optional) dependencies to `setup.py`
    
    This smoothes out the developer set-up to install with just e.g. `pip install .[dev]`
    
    * Trigger action
    
    * Add solve/add_linear capabilities
    
    * Fix missing comparator
    
    * Fix user var handling
    
    * Handle Unsatisfiable exception
    
    * Fix unit tests
    
    * Update pindakaas interface
    
    * Rename `pkl` -> `pkd`
    
    * Format
    
    * Lint
    
    * Clean up irrelevant changes
    
    * Update pindakaas
    
    * Remove one more irrelevant change
    
    * Small fix
    
    * Use time_limit
    
    * Enable `time_limit`
    
    * Fix failing test if `pysat` is not installed
    
    * Fix supported
    
    * Fix various issues
    
    - clean up unsupported v. unimplemented
    - fix time limit
    - skip solveall test
    - refactor
    
    * Parametrize `test_examples` for solvers
    
    * Make transformation unsupported exception more consistent
    
    * Update pindakaas
    
    * Fix CI submodule path
    
    * Update
    
    * Update test_examples
    
    * Update pkd
    
    * Remove gurobi from examples
    
    Seems to take very long (but not on master?)
    
    * Fix submodule
    
    * Add subsolver, small fixes
    
    * Minor refactor
    
    * Improve skip behaviour in test examples
    
    * Run examples as `__main__`
    
    * Add pytest-timeout
    
    Unlike pytest-xdist, the user *needs* timeouts to run the test-suite
    completely
    
    * Skip example tests
    
    * Refactor, use recursion to avoid repeating cases
    
    * Enable assumptions
    
    * Rename pkd to pdk
    
    * Update pdk according to new solver interface template
    
    * Revert unrelated changes
    
    * Fix name
    
    * Remove comment
    
    * Fix unsupported var types
    
    * Update major parts of interface
    
    Missing from upstream:
    
    - Conditions
    - Some PanicException which needs to be Unsatisfiable
    
    * Improve BoolLinExp overloading
    
    * Replace submodule for optional dependency
    
    Once released on pypi, we can change the VCS ref for simply `pindakaas`
    
    * Remove submodule step from CI
    
    * Revert version
    
    * Remove .gitmodules
    
    * Small clarification
    
    * Add conditions
    
    * Remove subsolvers
    
    * Polish/docs
    
    * Typo
    
    * Add missing arg
    
    * Pin commit
    
    * Follow remaining guide steps
    
    * Change dependency to released version
    
    * Use regular `supported` implementation
    
    * Fix some comments
    
    * Replace `match` statements
    
    * Implement `get_core`
    
    * Transform partials
    
    * Unsupport get_core for pindakaas awaiting upstream fix
    
    * Update docs
    
    * Fixes in tests
    
    * Avoid ternary
    
    * Improve value setting loop
    
    * Fix test
    
    * Remove duplicate function
    
    * Reformat with longer lines
    
    * Enable CSE
    
    * Enable `int2bool` for pindakaas
    
    * Make int2bool changes consistent in pysat
    
    * Enable extra tests
    
    * Remove comment
    
    * Process review
    
    * Revert pysat changes
    
    * Add and test `version`
    
    * Documentation
    
    * Skip `version` test for pumpkin
    
    * Just remove `version` test for now

--- cpmpy/solvers/utils.py
+++ cpmpy/solvers/utils.py
@@ -29,6 +29,7 @@ from .exact import CPM_exact
 from .choco import CPM_choco
 from .pumpkin import CPM_pumpkin
 from .cpo   import CPM_cpo
+from .pindakaas import CPM_pindakaas
 
 def param_combinations(all_params, remaining_keys=None, cur_params=None):
     """
@@ -81,6 +82,7 @@ class SolverLookup():
                 ("choco", CPM_choco),
                 ("pumpkin", CPM_pumpkin),
                 ("cpo", CPM_cpo),
+                ("pindakaas", CPM_pindakaas),
                ]
 
     @classmethod
--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -34,6 +34,7 @@ SAT_SOLVERS = {"pysdd"}
 
 EXCLUDE_GLOBAL = {"pysat": {},  # with int2bool,
                   "pysdd": NUM_GLOBAL | {"Xor"},
+                  "pindakaas": {},
                   "z3": {},
                   "choco": {},
                   "ortools":{},
@@ -47,6 +48,7 @@ EXCLUDE_GLOBAL = {"pysat": {},  # with int2bool,
 EXCLUDE_OPERATORS = {"gurobi": {},
                      "pysat": {"mul", "div", "pow", "mod"},  # int2bool but mul, and friends, not linearized
                      "pysdd": {"sum", "wsum", "sub", "mod", "div", "pow", "abs", "mul","-"},
+                     "pindakaas": {"mul", "div", "pow", "mod"},
                      "exact": {},
                      "pumpkin": {"pow", "mod"},
                      }
