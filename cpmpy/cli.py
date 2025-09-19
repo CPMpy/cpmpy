0e68af022fe185c3a98edeba71d6a4d490c1cbbf
Thomas Sergeys <thomas.s2000@hotmail.com>
2025-09-19 10:02:32 +0200

    Always print versions of solver backends

--- cpmpy/cli.py
+++ cpmpy/cli.py
@@ -5,17 +5,15 @@ import cpmpy as cp
 
 def command_version(args):
     print(f"CPMpy version: {__version__}")
-    if args.solvers:
-        cp.SolverLookup().print_version()
+    cp.SolverLookup().print_version()
 
 def main():
     parser = argparse.ArgumentParser(description="CPMpy command line interface")
     subparsers = parser.add_subparsers(dest="command", required=True)
 
     # cpmpy version
-    version_parser = subparsers.add_parser("version", help="Show version")
+    version_parser = subparsers.add_parser("version", help="Show version information on CPMpy and its solver backends")
     version_parser.set_defaults(func=command_version)
-    version_parser.add_argument("--solvers", action='store_true')
 
     args = parser.parse_args()
     args.func(args)
