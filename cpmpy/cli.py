abd53cc6e6b6744e734de6b67c8301d4e12c6dfd
Thomas Sergeys <thomas.s2000@hotmail.com>
2025-09-19 09:59:59 +0200

    Remove status cli command

--- cpmpy/cli.py
+++ cpmpy/cli.py
@@ -2,8 +2,6 @@ import argparse
 from cpmpy import __version__
 import cpmpy as cp
 
-def command_status(args):
-    cp.SolverLookup().print_status()
 
 def command_version(args):
     print(f"CPMpy version: {__version__}")
@@ -13,10 +11,6 @@ def command_version(args):
 def main():
     parser = argparse.ArgumentParser(description="CPMpy command line interface")
     subparsers = parser.add_subparsers(dest="command", required=True)
-
-    # cpmpy status
-    status_parser = subparsers.add_parser("status", help="List available solvers")
-    status_parser.set_defaults(func=command_status)
 
     # cpmpy version
     version_parser = subparsers.add_parser("version", help="Show version")
