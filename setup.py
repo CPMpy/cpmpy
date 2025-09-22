e86716840d2848fb6a2be835e26675e07bd081e7
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2025-09-22 18:24:53 +0200

    add pumpkin to extras in setup.py

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ solver_dependencies = {
     "gcs": ["gcspy"],
     "cpo": ["docplex"],
     "pindakaas": ["pindakaas"],
+    "pumpkin": ["pumpkin-solver>=0.2.1"]
 }
 solver_dependencies["all"] = list({pkg for group in solver_dependencies.values() for pkg in group}) 
 
