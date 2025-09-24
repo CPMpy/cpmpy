216cb0214dd4e45c468af44bdb8d8d1debe59fe0
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-09-24 12:23:05 +0000

    Clean up, start on shrink

--- examples/tables.py
+++ examples/tables.py
@@ -4,15 +4,10 @@ import cpmpy as cp
 
 import random
 
-VERBOSITY = 1
-DEBUG = True
-LOOP_LIMIT = None
-random.seed(42)
 
-
-def log(*mess, verbosity=1):
+def log(*mess, verbosity=1, end="\n"):
     if verbosity <= VERBOSITY:
-        print(*mess)
+        print(*mess, end=end)
 
 
 # x = cp.intvar(1, 4, name="x")
@@ -35,16 +30,6 @@ def generate_table(n, m, d):
     return X, T
 
 
-X, T = generate_table_from_data([[1, 1], [2, 2]], 3)
-# X, T = generate_table(2, 2, 3)
-# X, T = generate_table(3, 10, 5)
-# X, T = generate_table(10, 100, 10)
-log(X, verbosity=2)
-log(T, verbosity=2)
-
-# TODO how to determine it is a failure and not a solution?
-
-
 def encode_x(a, lb, ub):
     X = (ub - lb + 1) * [0]
     X[a - lb] = 1
@@ -62,10 +47,6 @@ def encode(X, T):
     return np.array([xij for x, t in zip(X, T) for row in t for xij in encode_x(row, x.lb, x.ub)])
 
 
-T_ = encode(X, T)
-log(T_, verbosity=2)
-
-
 def rows(T, i, j=1):
     """Row indices where T[i]==j"""
     return set(int(i) for i in np.where(T[:, i] == j)[0].flatten())
@@ -76,25 +57,40 @@ def cols(T, i, j=1):
     return set(int(i) for i in np.where(T[i, :] == j)[0].flatten())
 
 
-def explain(A, T, X):
-    log(f"Explain {A}")
+def shrink(C, T):
+    for i in C:
+        print(f"shrinking {i}")
+        S = set.intersection({rows(T, l) for l in C if l != i})
+        print(f"intersection: ", S)
+        if len(S) == 0:
+            C = C - {i}
+    return C
+
+
+def explain(A_enc, T_enc, X_enc):
+    log(f"Explain {A_enc} {[X_enc[i] for i in A_enc]}")
     C = set()  # vars added to cut
-    R = set(range(len(T)))
+    R = set(range(len(T_enc)))
     dbg = 0
     while len(R):
-        i = min((i for i in (A - C)), default=None)  # choose some col which is 1
+        i = min((i for i in (A_enc - C)), default=None)  # choose some col which is 1
         if i is None:  # TODO [?] slightly different stopping condition
             break
-        log("A", A, C, verbosity=3)
+        log("A", A_enc, C, verbosity=3)
         log(f"adding {i + 1}", verbosity=3)
-        T_i = rows(T, i)
+        T_i = rows(T_enc, i)
         R = R.intersection(T_i)
         C.add(i)
         dbg += 1
         assert LOOP_LIMIT is None or dbg < LOOP_LIMIT
 
-    constraint = cp.any(~X[i] for i in C)
-    log(f"  by explanation ({len(C)}): {C} == {constraint}")
+    log(f"  by explanation ({len(C)}): {C}")
+    if SHRINK:
+        C = shrink(C, T_enc)
+        log(f"  shrunk to ({len(C)}): {C}")
+    # sum X[i] < len(X)
+    constraint = cp.any(~X_enc[i] for i in C)
+    log(f"  cut == {constraint}")
     return constraint
 
 
@@ -106,9 +102,15 @@ def show_sols(sols, T):
     return ", ".join(f"*{sol}" if list(sol) in T.tolist() else f"{sol}" for sol in sorted(sols))
 
 
-REAL = True
-if REAL:
-    X_ = [x == d for x in X for d in range(x.lb, x.ub + 1)]
+def solve(X, T):
+    log("X =", ", ".join(f"{x} in {x.lb}..{x.ub}" for x in X), verbosity=2)
+    log("T =", verbosity=2)
+    log(T, verbosity=2)
+    T_enc = encode(X, T)
+    log("T_enc =", verbosity=2)
+    log(T_enc, verbosity=2)
+
+    X_enc = [x == d for x in X for d in range(x.lb, x.ub + 1)]
     m = cp.Model([cp.sum(x == d for d in range(x.lb, x.ub + 1)) == 1 for x in X])
     constraint = cp.Table(X, T)
 
@@ -130,30 +132,36 @@ if REAL:
 
         m.solve()
         A = [x.value() for x in X]
-        log("A", A, constraint.value())
-        # A = [x.value() for x in X]
 
         if constraint.value():
-            break
+            log("Satisfying sol:", A)
+            return A
 
-        A = [encode_x(a, x.lb, x.ub) for a, x in zip(A, X)]
-        A = [ai for a in A for ai in a]
+        A_enc = [encode_x(a, x.lb, x.ub) for a, x in zip(A, X)]
+        A_enc = [ai for a in A_enc for ai in a]
         # A = set(np.where(np.array(A) == 1)[0])
-        A = cols(np.array([A]), 0)
+        A_enc = cols(np.array([A_enc]), 0)
 
-        explanation = explain(A, T_, X_)
+        explanation = explain(A_enc, T_enc, X_enc)
         m += [explanation]
-        log("M", m, verbosity=2)
+        log("New model\n", m, verbosity=3)
 
         dbg += 1
         assert LOOP_LIMIT is None or dbg < LOOP_LIMIT
-    print("Satisfying sol:", A)
 
-    A = [x.value() for x in X]
-else:
-    A = [2, 2, 2]
 
-# TODO take care of encoding to integer
-# TODO use native model to resolve and
+# TODO use gurobi lazy constraints interface
 
-# m.solve(solver="gurobi")
+if __name__ == "__main__":
+    VERBOSITY = 2
+    DEBUG = True
+    LOOP_LIMIT = None
+    SHRINK = False
+    random.seed(42)
+
+    X, T = generate_table_from_data([[1, 1], [2, 2]], 3)
+    # X, T = generate_table(2, 2, 3)
+    # X, T = generate_table(3, 10, 5)
+    # X, T = generate_table(10, 100, 10)
+
+    solve(X, T)
