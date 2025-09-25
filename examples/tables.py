8f42efd525d1e1553a7e4edc25c200b5a74d0a2e
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-09-25 08:18:57 +0000

    Finish heuristics

--- examples/tables.py
+++ examples/tables.py
@@ -17,7 +17,8 @@ def generate_table_from_example():
     X = (x, y, z)
     T = [(2, 1, 1), (3, 2, 2), (4, 3, 3), (1, 2, 3), (2, 1, 2)]
     T = np.array(T)
-    return X,T
+    return X, T
+
 
 def generate_table_from_data(rows, d):
     """Generate a table constraint with the given `rows` and with var domains of size `d`"""
@@ -62,7 +63,7 @@ def shrink(C, T):
     for i in C:
         log(f"shrinking {i} in {C}", verbosity=3)
 
-        if len(C) <= 1: # slightly different from
+        if len(C) <= 1:  # slightly different from
             return C
 
         S = set.intersection(*[rows(T, l) for l in (C - {i})])
@@ -70,15 +71,42 @@ def shrink(C, T):
             C = C - {i}
     return C
 
-def explain(A_enc, T_enc, X_enc):
+
+from enum import Enum
+
+
+class Heuristic(Enum):
+    INPUT = 1
+    GREEDY = 2
+    REDUCE = 3
+
+
+def choose(A, T_enc, R, heuristic=Heuristic.INPUT):
+    if len(A) == 0:
+        return None
+    match heuristic:
+        case Heuristic.INPUT:
+            return min(i for i in A)
+        case Heuristic.GREEDY:
+            return min(A, key=lambda i: len(R.intersection(rows(T_enc, i))))
+        case Heuristic.REDUCE:
+            return min(
+                (i for i in A if R.intersection(rows(T_enc, i)) != R), key=lambda i: len(rows(T_enc, i))
+            )
+
+
+def explain(A_enc, T_enc, X_enc, env):
     log(f"Explain {A_enc} {[X_enc[i] for i in A_enc]}")
     C = set()  # vars added to cut
     R = set(range(len(T_enc)))
     dbg = 0
     while len(R):
-        i = min((i for i in (A_enc - C)), default=None)  # choose some col which is 1
+        # choose some col which is 1
+        i = choose(A_enc - C, T_enc, R, heuristic=env["heuristic"])
+
         if i is None:  # TODO [?] slightly different stopping condition
             break
+
         log("A", A_enc, C, verbosity=3)
         log(f"adding {i + 1}", verbosity=3)
         T_i = rows(T_enc, i)
@@ -88,16 +116,16 @@ def explain(A_enc, T_enc, X_enc):
         assert LOOP_LIMIT is None or dbg < LOOP_LIMIT
 
     log(f"  by explanation of size ({len(C)}): {C}")
-    if SHRINK:
+    if env["shrink"]:
         size = len(C)
         C = shrink(C, T_enc)
         if len(C) < size:
             log(f"  shrunk to size ({len(C)}): {C}")
             # assert False, "TODO; shrinking is not triggering"
 
-    if DEBUG_CUTS is not None:
-        assert C not in DEBUG_CUTS, f"Already found cut {C} previously in {DEBUG_CUTS}"
-        DEBUG_CUTS.append(C)
+    if env["cuts"] is not None:
+        assert C not in env["cuts"], f"Already found cut {C} previously in {env['cuts']}"
+        env["cuts"].append({"cut": C, "shrunk": 0})
 
     constraint = cp.any(~X_enc[i] for i in C)
     log(f"  cut == {constraint}")
@@ -112,13 +140,13 @@ def show_sols(sols, T):
     return ", ".join(f"*{sol}" if list(sol) in T.tolist() else f"{sol}" for sol in sorted(sols))
 
 
-def solve(X, T):
-    log("X =", ", ".join(f"{x} in {x.lb}..{x.ub}" for x in X), verbosity=2)
-    log("T =", verbosity=2)
-    log(T, verbosity=2)
+def solve(X, T, env):
+    log("X =", ", ".join(f"{x} in {x.lb}..{x.ub}" for x in X), verbosity=0)
+    log("T =", verbosity=1)
+    log(T, verbosity=1)
     T_enc = encode(X, T)
-    log("T_enc =", verbosity=2)
-    log(T_enc, verbosity=2)
+    log("T_enc =", verbosity=1)
+    log(T_enc, verbosity=1)
 
     X_enc = [x == d for x in X for d in range(x.lb, x.ub + 1)]
     m = cp.Model([cp.sum(x == d for d in range(x.lb, x.ub + 1)) == 1 for x in X])
@@ -132,6 +160,8 @@ def solve(X, T):
             m.solveAll(display=lambda: sols.append([x.value() for x in X]))
             log(f"Search space remaining: ({len(sols)})")
             log(show_sols(sols, T), verbosity=2)
+            if len(env["cuts"]) > 0:
+                env["cuts"][-1]["space"] = len(sols)
             # TODO check whether all are still in table
 
             for row in T:
@@ -164,7 +194,7 @@ def solve(X, T):
         A_enc = [ai for a in A_enc for ai in a]
         A_enc = cols(np.array([A_enc]), 0)
 
-        explanation = explain(A_enc, T_enc, X_enc)
+        explanation = explain(A_enc, T_enc, X_enc, env)
         m += [explanation]
         log("New model", verbosity=3)
         log(m, verbosity=3)
@@ -176,19 +206,56 @@ def solve(X, T):
 # TODO use gurobi lazy constraints interface
 
 if __name__ == "__main__":
-    VERBOSITY = 1
-    DEBUG = False
-    DEBUG_UNLUCKY = True
+    VERBOSITY = 0
+    DEBUG = True
+    DEBUG_UNLUCKY = False
     LOOP_LIMIT = None
-    SHRINK = True
     random.seed(42)
-    DEBUG_CUTS = []
+
+    envs = [
+        {
+            "shrink": shrink,
+            "heuristic": heuristic,
+            "cuts": [],
+        }
+        for heuristic in [
+            # heuristics
+            Heuristic.INPUT,
+            Heuristic.GREEDY,
+            Heuristic.REDUCE,
+        ]
+        for shrink in [
+            # shrink
+            # False,
+            True,
+        ]
+    ]
+
+    # STATS = {
+    #         "explanations":
+    #         }
 
     # X, T = generate_table_from_data([[1, 1], [2, 2]], 3)
     # X, T = generate_table_from_example()
     # X, T = generate_table(2, 2, 3)
     # X, T = generate_table(3, 10, 5)
+
+    DEBUG = False
     # X, T = generate_table(5, 25, 10)
     X, T = generate_table(10, 100, 10)
 
-    solve(X, T)
+    # envs = envs[0:1]
+    for env in envs:
+        A = solve(X, T, env)
+        assert A in T.tolist(), "Check failed."
+        X.clear()
+
+    log("STATS")
+    for env in envs:
+        log(", ".join(f"{k}={env[k]}" for k in ["shrink", "heuristic"]), verbosity=0)
+        log(f"n_cuts = {len(env['cuts'])}", verbosity=0)
+        log(
+            "cut cardinalities/strengths:",
+            ", ".join(f"{len(c['cut'])} / {c.get('space', '?')}" for c in env["cuts"]),
+            verbosity=2
+        )
