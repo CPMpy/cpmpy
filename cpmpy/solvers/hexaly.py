39ac8aeb58ab60f33cb6096788e038d1cff48599
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2025-09-26 18:52:21 +0200

    update error message

--- cpmpy/solvers/hexaly.py
+++ cpmpy/solvers/hexaly.py
@@ -397,8 +397,8 @@ class CPM_hexaly(SolverInterface):
     
     def solveAll(self, display=None, time_limit=None, solution_limit=None, call_from_model=False, **kwargs):
         if time_limit is None:
-            raise ValueError("Hexaly does not support exhausitve search to find all solutions. "
-                             "Set time limit or solution limit to do a limited search")
+            raise ValueError("Hexaly does not support exhaustive search to find all solutions. "
+                             "Set time limit to do a limited search")
 
         return super(CPM_hexaly, self).solveAll(display, time_limit, solution_limit, call_from_model, **kwargs)
 
