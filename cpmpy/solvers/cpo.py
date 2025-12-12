5263871b21cd5af69a5665d98f8028ca61c45cd4
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2025-12-12 10:25:33 +0100

    add typed arguments in cpo

--- cpmpy/solvers/cpo.py
+++ cpmpy/solvers/cpo.py
@@ -253,7 +253,7 @@ class CPM_cpo(SolverInterface):
 
         return has_sol
 
-    def solveAll(self, display=None, time_limit:Optional[float]=None, solution_limit=None, call_from_model=False, **kwargs):
+    def solveAll(self, display=None, time_limit:Optional[float]=None, solution_limit:Optional[int]=None, call_from_model=False, **kwargs):
         """
             A shorthand to (efficiently) compute all (optimal) solutions, map them to CPMpy and optionally display the solutions.
 
