0d2af6c5668b2641065c2d76666d96d129aa48fe
ThomSerg <thomas.s2000@hotmail.com>
2025-12-18 12:33:27 +0100

    skip example that wants to open visualisation in browser

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -24,8 +24,9 @@ ADVANCED_EXAMPLES = sorted(ADVANCED_EXAMPLES)
 
 SKIPPED_EXAMPLES = [
                     "ocus_explanations.py", # waiting for issues to be resolved 
-                    "psplib.py" # randomly fails on github due to file creation
-                    "nurserostering.py"
+                    "psplib.py", # randomly fails on github due to file creation
+                    "nurserostering.py",
+                    "psplib.py", # wants to open visualisations in the browser
                     ]
 
 SKIP_MIP = ['npuzzle.py', 'tst_likevrp.py', 'sudoku_', 'pareto_optimal.py',
