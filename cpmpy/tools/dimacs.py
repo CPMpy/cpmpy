888f29db43c78949979b6dfc2dc42789c9c370d0
Henk Bierlee <bierlee.henk@gmail.com>
2026-01-08 09:43:47 +0100

    Expand CNF using encoding back-end (#782)
    * Expand `to_cnf` transformation using pindakaas back-end
    
    * Minimize number of free var clauses (x | ~x)
    
    * Bypass `pindakaas` for simple clauses
    
    * Handle pindakaas dependency
    
    * Fix import
    
    * Remove return of `ivarmap`
    
    * add encoding parameter to cnf transform
    
    * update dimacs writer test
    
    * pass encoding arg to cnf from dimacs writer
    
    ---------
    
    Co-authored-by: Ignace Bleukx <ignace.bleukx@kuleuven.be>

--- cpmpy/tools/dimacs.py
+++ cpmpy/tools/dimacs.py
@@ -25,7 +25,7 @@ from cpmpy.transformations.get_variables import get_variables
 import re
 
 
-def write_dimacs(model, fname=None):
+def write_dimacs(model, fname=None, encoding="auto"):
     """
         Writes CPMpy model to DIMACS format
         Uses the "to_cnf" transformation from CPMpy
@@ -35,10 +35,11 @@ def write_dimacs(model, fname=None):
 
         :param model: a CPMpy model
         :param fname: optional, file name to write the DIMACS output to
+        :param encoding: the encoding used for `int2bool`, choose from ("auto", "direct", "order", or "binary")
     """
 
     constraints = toplevel_list(model.constraints)
-    constraints = to_cnf(constraints)
+    constraints = to_cnf(constraints, encoding=encoding)
 
     vars = get_variables(constraints)
     mapping = {v : i+1 for i, v in enumerate(vars)}
