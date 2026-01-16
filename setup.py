ca9c00e6e1617000df7c5c4a97d2c90bcc7c6452
ThomSerg <thomas.s2000@hotmail.com>
2026-01-16 19:09:54 +0100

    drop support for eol 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -71,8 +71,6 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
@@ -80,5 +78,5 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8'
+    python_requires='>=3.10'
 )
