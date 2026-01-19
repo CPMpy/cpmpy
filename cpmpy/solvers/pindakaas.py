24dee1911037e3d767d9fd9c55c0fceea71de14a
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2026-01-19 10:28:35 +0100

    use linear_decomp in pindakaas

--- cpmpy/solvers/pindakaas.py
+++ cpmpy/solvers/pindakaas.py
@@ -49,7 +49,7 @@ from ..transformations.decompose_global import decompose_in_tree
 from ..transformations.flatten_model import flatten_constraint
 from ..transformations.get_variables import get_variables
 from ..transformations.int2bool import _decide_encoding, _encode_int_var, int2bool
-from ..transformations.linearize import linearize_constraint
+from ..transformations.linearize import linearize_constraint, decompose_linear
 from ..transformations.normalize import simplify_boolean, toplevel_list
 from ..transformations.reification import only_bv_reifies, only_implies
 from ..transformations.safening import no_partial_functions
@@ -236,11 +236,12 @@ class CPM_pindakaas(SolverInterface):
     def transform(self, cpm_expr):
         cpm_cons = toplevel_list(cpm_expr)
         cpm_cons = no_partial_functions(cpm_cons, safen_toplevel={"div", "mod", "element"})
-        cpm_cons = decompose_in_tree(
+        cpm_cons = decompose_linear(
             cpm_cons,
-            supported=self.supported_global_constraints | {"alldifferent"},  # alldiff has a specialized MIP decomp in linearize
+            supported=self.supported_global_constraints,
             supported_reified=self.supported_reified_global_constraints,
             csemap=self._csemap,
+            ivarmap=self.ivarmap
         )
         cpm_cons = simplify_boolean(cpm_cons)
         cpm_cons = flatten_constraint(cpm_cons, csemap=self._csemap)  # flat normal form
