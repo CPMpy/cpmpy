f5d40c555d3346c3e701e72290676021f0ef0fe2
ThomSerg <thomas.s2000@hotmail.com>
2026-01-29 10:44:34 +0100

    Setup add io deps

--- setup.py
+++ setup.py
@@ -36,6 +36,12 @@ solver_dependencies = {
 }
 solver_dependencies["all"] = list({pkg for group in solver_dependencies.values() for pkg in group}) 
 
+format_dependencies = {
+    "io.mps": ["pyscipopt"],
+    "io.scip": ["pyscipopt"],
+    "io.dimacs": ["pyscipopt"],
+}
+
 setup(
     name='cpmpy',
     version=get_version("cpmpy/__init__.py"),
@@ -58,6 +64,7 @@ setup(
     extras_require={
         # Solvers
         **solver_dependencies,
+        **format_dependencies,
         # Tools
         "xcsp3": ["pycsp3", "requests", "tqdm", "matplotlib", "psutil", "filelock", "gnureadline; platform_system != 'Windows'", "pyreadline3; platform_system == 'Windows'"], # didn't add CLI-specific req since some are not cross-platform
         # Other
