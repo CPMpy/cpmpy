ae75200620384f1de2ddab679cb31002ee2966de
ThomSerg <thomas.s2000@hotmail.com>
2026-01-29 11:41:00 +0100

    add tools to IO module

--- cpmpy/tools/io/__init__.py
+++ cpmpy/tools/io/__init__.py
@@ -1,3 +1,11 @@
 from .writer import write, write_formats
 from .reader import read, read_formats
 from .utils import get_extension, get_format
+
+from .jsplib import read_jsplib
+from .mps import read_mps, write_mps
+from .nurserostering import read_nurserostering
+from .opb import read_opb
+from .rcpsp import read_rcpsp
+from .scip import read_scip, write_scip
+from .wcnf import read_wcnf
