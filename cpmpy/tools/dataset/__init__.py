0842d8de76533d6d166772a4330961476d2da0a8
ThomSerg <thomas.s2000@hotmail.com>
2026-01-30 11:36:11 +0100

    Cherry pick dataset files

---
+++ cpmpy/tools/dataset/__init__.py
@@ -0,0 +1,7 @@
+from .miplib import MIPLibDataset
+from .jsplib import JSPLibDataset
+from .psplib import PSPLibDataset
+from .nurserostering import NurseRosteringDataset
+from .xcsp3 import XCSP3Dataset
+from .opb import OPBDataset
+from .mse import MSEDataset
