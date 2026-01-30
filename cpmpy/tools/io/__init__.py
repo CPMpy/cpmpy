52a3948f118970da4469bab42fabb58427f840f5
ThomSerg <thomas.s2000@hotmail.com>
2026-01-30 14:09:30 +0100

    small change to docstring

--- cpmpy/tools/io/__init__.py
+++ cpmpy/tools/io/__init__.py
@@ -5,7 +5,7 @@ This module provides tools to read and write models in various formats.
 Use the generic `read(..., format="...")` and `write(..., format="...")` functions to read and write 
 models in one of the supported formats.
 
-Some formats can be auto-detected from the file extension.
+Some formats can be auto-detected from the file extension, so only a file path is required as argument.
 """
 
 from .writer import write, write_formats
