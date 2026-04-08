a0128222d023afe6e2a79daa64451785c8204e6c
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2026-04-08 16:59:59 +0200

    update template

--- cpmpy/solvers/TEMPLATE.py
+++ cpmpy/solvers/TEMPLATE.py
@@ -64,6 +64,7 @@ from ..transformations.flatten_model import flatten_constraint, flatten_objectiv
 from ..transformations.comparison import only_numexpr_equality
 from ..transformations.reification import reify_rewrite, only_bv_reifies
 from ..transformations.safening import safen_objective
+from ..transformations.negation import push_down_negation
 
 
 class CPM_template(SolverInterface):
@@ -387,6 +388,7 @@ class CPM_template(SolverInterface):
         # XXX chose the transformations your solver needs, see cpmpy/transformations/
         cpm_cons = toplevel_list(cpm_expr)
         cpm_cons = no_partial_functions(cpm_cons)  # to also safen at toplevel, add: `, safen_toplevel={"element", "div", "mod"})`
+        cpm_cons = push_down_negation(cpm_cons)
         cpm_cons = decompose_in_tree(cpm_cons,
                                      supported=self.supported_global_constraints,
                                      supported_reified=self.supported_reified_global_constraints,
