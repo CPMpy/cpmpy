47f0b943106c90dffbe728002608cce0419f3b02
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2026-04-21 16:01:33 +0200

    fix testS

--- tests/test_trans_simplify.py
+++ tests/test_trans_simplify.py
@@ -15,7 +15,7 @@ class TestTransSimplify:
         expr = Operator("or", self.bvs.tolist() + [False])
         assert str(self.transform(expr)) == "[or(bv[0], bv[1], bv[2])]"
         expr = Operator("or", self.bvs.tolist() + [True])
-        assert str(self.transform(expr)) == "[boolval(True)]"
+        assert str(self.transform(expr)) == "[]"
 
         expr = Operator("and", self.bvs.tolist() + [False]) + self.ivs[0] >= 10
         assert str(self.transform(expr)) == "[0 + (iv[0]) >= 10]"
@@ -24,13 +24,13 @@ class TestTransSimplify:
 
 
         expr = Operator("->", [self.bvs[0], True])
-        assert str(self.transform(expr)) == "[boolval(True)]"
+        assert str(self.transform(expr)) == "[]"
         expr = Operator("->", [self.bvs[0], False])
         assert str(self.transform(expr)) == "[~bv[0]]"
         expr = Operator("->", [True, self.bvs[0]])
         assert str(self.transform(expr)) == "[bv[0]]"
         expr = Operator("->", [False, self.bvs[0]])
-        assert str(self.transform(expr)) == "[boolval(True)]"
+        assert str(self.transform(expr)) == "[]"
 
     def test_bool_in_comp(self):
         expr = self.ivs[0] >= False
@@ -45,25 +45,30 @@ class TestTransSimplify:
         assert str(self.transform(expr)) == '[1 + (iv[0]) >= 0]'
 
     def test_boolvar_comps(self):
-        num_args = {"<0": -1, "0": 0, "]0..1[": 0.5, "1": 1, ">0": 2}
+        num_args = {"<0": -1, "0": 0, "1": 1, ">0": 2}
         # test table from github (#add url)
         bv = self.bvs[0]
         test_dict = {
-            "==": {"<0": False,"0": ~bv,  "]0..1[":False,"1": bv,   ">0": False},
-            "!=": {"<0": True, "0": bv,   "]0..1[":True, "1": ~bv,  ">0": True},
-            ">":  {"<0": True, "0": bv,   "]0..1[":bv,   "1": False,">0": False},
-            "<":  {"<0": False,"0": False,"]0..1[":~bv,  "1": ~bv,  ">0": True},
-            ">=": {"<0": True, "0": True, "]0..1[":bv,   "1": bv,   ">0": False},
-            "<=": {"<0": False,"0": ~bv,  "]0..1[":~bv,  "1": True, ">0": True}
+            "==": {"<0": False,"0": ~bv,"1": bv,   ">0": False},
+            "!=": {"<0": True, "0": bv, "1": ~bv,  ">0": True},
+            ">":  {"<0": True, "0": bv, "1": False,">0": False},
+            "<":  {"<0": False,"0": False, "1": ~bv,  ">0": True},
+            ">=": {"<0": True, "0": True, "1": bv,   ">0": False},
+            "<=": {"<0": False,"0": ~bv, "1": True, ">0": True}
         }
 
         for op in test_dict:
             for rhs, val_should in test_dict[op].items():
                 expr = Comparison(op, bv, num_args[rhs])
                 print(expr)
-                expr_should = BoolVal(val_should) if isinstance(val_should, bool) else val_should
-                assert str(self.transform(expr)) == str([expr_should])
+                if val_should is True:
+                    str_val_should = "[]"
+                elif val_should is False:
+                    str_val_should = "[boolval(False)]"
+                else:
+                    str_val_should = str([val_should])
 
+                assert str(self.transform(expr)) == str_val_should
 
     def test_simplify_expressions(self):
         # global constraints
@@ -98,20 +103,20 @@ class TestTransSimplify:
         expr = Operator("and", self.bvs[:1].tolist() + [BoolVal(False)]) == Operator("or", self.bvs)
         assert str(self.transform(expr)) == '[and(~bv[0], ~bv[1], ~bv[2])]'
 
-    # issue #322
-    def test_with_floats(self):
-        expr = self.bvs[0] == 1.0
-        assert str(self.transform(expr)) == "[bv[0]]"
-        expr = cp.AllDifferent(self.ivs) == 4.2
-        assert str(self.transform(expr)) == '[boolval(False)]'
-        expr = 0.0 == cp.AllDifferent(self.ivs)
-        assert str(self.transform(expr)) == '[not(alldifferent(iv[0],iv[1],iv[2]))]'
-
-        expr = (self.ivs[0] <= self.ivs[1]) < 0.8
-        assert str(self.transform(expr)) == '[(iv[0]) > (iv[1])]'
-
-        expr = (self.ivs[0] == self.ivs[1]) == 1.0
-        assert str(self.transform(expr)) == '[(iv[0]) == (iv[1])]'
+    # # issue #322
+    # def test_with_floats(self): -> Deprecated, we don't support floats anymore
+    #     expr = self.bvs[0] == 1.0
+    #     assert str(self.transform(expr)) == "[bv[0]]"
+    #     expr = cp.AllDifferent(self.ivs) == 4.2
+    #     assert str(self.transform(expr)) == '[boolval(False)]'
+    #     expr = 0.0 == cp.AllDifferent(self.ivs)
+    #     assert str(self.transform(expr)) == '[not(alldifferent(iv[0],iv[1],iv[2]))]'
+    #
+    #     expr = (self.ivs[0] <= self.ivs[1]) < 0.8
+    #     assert str(self.transform(expr)) == '[(iv[0]) > (iv[1])]'
+    #
+    #     expr = (self.ivs[0] == self.ivs[1]) == 1.0
+    #     assert str(self.transform(expr)) == '[(iv[0]) == (iv[1])]'
 
 
     def test_nested_boolval(self):
