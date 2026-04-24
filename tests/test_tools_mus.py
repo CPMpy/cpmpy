892faec6e3513021e92826670f0428571fd8fe9c
OrestisLomis <orestis.lomisg7@gmail.com>
2026-04-24 17:14:23 +0200

    update

--- tests/test_tools_mus.py
+++ tests/test_tools_mus.py
@@ -89,7 +89,7 @@ class TestMus:
 class TestNativeMus(TestMus):
     # use solver from conftest.py
     @pytest.fixture(autouse=True)
-    def setup_method(self, solver):
+    def setup_solver(self, solver):
         self.mus_func = lambda soft, hard=[], solver=solver: mus_native(soft, hard=hard, solver=solver)
         self.naive_func = mus_naive
 
