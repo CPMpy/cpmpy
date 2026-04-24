fcea9f049d5d021954aabeae7ed96635230f537c
OrestisLomis <orestis.lomisg7@gmail.com>
2026-04-24 15:34:10 +0200

    add extra assert False

--- tests/test_tools_mus.py
+++ tests/test_tools_mus.py
@@ -24,6 +24,8 @@ class TestMus:
 
         assert set(self.mus_func(cons)) == set(cons[:3])
         assert set(self.naive_func(cons)) == set(cons[:3])
+        assert False
+
 
     def test_bug_191(self):
         """
