e34ac073a27e1181b97cf3061f250b1f2d67a7d4
Tias Guns <tias@ulyssis.org>
2026-04-25 22:09:11 +0200

    simplify_boolean in the new pattern (9s -> 6s flatten)

--- cpmpy/transformations/normalize.py
+++ cpmpy/transformations/normalize.py
@@ -3,9 +3,9 @@
 """
 
 import copy
+from typing import Any
 
 import numpy as np
-import cpmpy as cp
 
 from ..expressions.core import BoolVal, Expression, Comparison, Operator
 from ..expressions.globalfunctions import GlobalFunction
@@ -13,6 +13,7 @@ from ..expressions.utils import eval_comparison, is_false_cst, is_true_cst, is_b
 from ..expressions.variables import _BoolVarImpl
 from ..exceptions import NotSupportedError
 from ..expressions.globalconstraints import GlobalConstraint
+from .negation import recurse_negation
 
 
 def toplevel_list(cpm_expr, merge_and=True):
@@ -49,7 +50,7 @@ def toplevel_list(cpm_expr, merge_and=True):
     return newlist
 
 
-def simplify_boolean(lst_of_expr, num_context=False):
+def simplify_boolean(lst_of_expr: list[Expression], num_context=False) -> list[Expression]:
     """
     Removes boolean constants from all CPMpy expressions, except for constants in global constraints/functions.
     Solver interfaces are expected to implement special cases of typing for global constraints themselves.
@@ -62,183 +63,250 @@ def simplify_boolean(lst_of_expr, num_context=False):
         list_of_expr: list of CPMpy expressions
     """
 
-    newlist = []
+    newlist: list[Expression] = []
     for expr in lst_of_expr:
-
-        if isinstance(expr, Operator):
-            if expr.has_subexpr():
-                expr_args = simplify_boolean(expr.args, num_context=not expr.is_bool())
-            else:
-                expr_args = expr.args
-
-            args = expr_args
-            if expr.name == "or":
-                i = 0
-                while i < len(args):
-                    a = args[i]
-                    if isinstance(a, _BoolVarImpl):
-                        i += 1 # default path
-                    elif is_true_cst(a):
-                        break
-                    elif is_false_cst(a):
-                        if args is expr_args: # will remove this one, need to copy args...
-                            args = list(args)  # takes copy
-                        args.pop(i)
-                    else:
-                        i += 1
-
-                if i != len(args): # found "True" along the way, early exit
-                    newlist.append(1 if num_context else BoolVal(True))
-                elif args is not expr.args: # removed something, or changed due to subexpr
-                    newexpr = copy.copy(expr)
-                    newexpr.update_args(args)
-                    newlist.append(newexpr)
-                else: # no changes
-                    newlist.append(expr)
-
-            elif expr.name == "and":
-                # filter out True/False constants
-                i = 0
-                while i < len(args):
-                    a = args[i]
-                    if isinstance(a, _BoolVarImpl):
-                        i += 1 # default path
-                    elif is_false_cst(a):
-                        break
-                    elif is_true_cst(a):
-                        if args is expr_args:  # will remove this one, need to copy args...
-                            args = list(args)  # takes copy
-                        args.pop(i)
-                    else:  # subexpression, should not happen here...
-                        i += 1
-
-                if i != len(args): # found "False" along the way, early exit
-                    newlist.append(0 if num_context else BoolVal(False))
-                elif args is not expr.args: # removed something, or changed due to subexpr
-                    newexpr = copy.copy(expr)
-                    newexpr.update_args(args)
-                    newlist.append(newexpr)
-                else: # no changes
-                    newlist.append(expr)
-
-            elif expr.name == "->":
-                cond, bool_expr = args
-                if is_false_cst(cond) or is_true_cst(bool_expr):
-                    newlist.append(1 if num_context else BoolVal(True))
-                elif is_true_cst(cond):
-                    newlist.append(bool_expr)
-                elif is_false_cst(bool_expr):
-                    newlist += simplify_boolean([cp.transformations.negation.recurse_negation(cond)])
-                else:
-                    newlist.append(cond.implies(bool_expr))
-
-            elif expr.name == "not":
-                if isinstance(args[0], _BoolVarImpl): # fast path
-                    newlist.append(~args[0])
-                elif is_true_cst(args[0]):
-                    newlist.append(0 if num_context else BoolVal(False))
-                elif is_false_cst(args[0]):
-                    newlist.append(1 if num_context else BoolVal(True))
-                elif args is not expr.args:
-                    expr = copy.copy(expr)
-                    expr.update_args(expr_args)
-                    newlist.append(expr)
-                else: # nothing changed
-                    newlist.append(expr)
-
-            # numerical expressions
-            elif expr.name == "wsum":
-                weights, vars = args
-                newvars = [int(v) if is_bool(v) else v for v in vars]
-                if args is not expr.args or any(v1 is not v2 for v1,v2 in zip(vars, newvars)):
-                    newexpr = copy.copy(expr)
-                    newexpr.update_args([weights, newvars])
-                    newlist.append(newexpr)
-                else:
-                    newlist.append(expr)
-
-            else: # default case, check if anything is Bool that should be int
-                newargs = [int(a) if is_bool(a) else a for a in args]
-                if expr_args is not expr.args or any(v1 is not v2 for v1, v2 in zip(args, newargs)):
-                    newexpr = copy.copy(expr)
-                    newexpr.update_args(newargs)
-                    newlist.append(newexpr)
-                else:
-                    newlist.append(expr)
-
-        elif isinstance(expr, Comparison):
-            lhs, rhs = simplify_boolean(expr.args, num_context=True)
-            name = expr.name
-            if is_num(lhs) and is_boolexpr(rhs):  # flip arguments of comparison to reduct nb of cases
-                if name == "<":    name = ">"
-                elif name == ">":  name = "<"
-                elif name == "<=": name = ">="
-                elif name == ">=": name = "<="
-                lhs, rhs = rhs, lhs
-            """
-            Simplify expressions according to this table:
-            x  | <0	 0  0.n	 1	>1
-            ----------------------
-            == |  F	~x	 F   x	 F
-            != |  T	 x	 T  ~x	 T
-            >  |  T	 x	 x   F	 F
-            <  |  F	 F	~x  ~x	 T
-            >= |  T	 T	 x   x	 F   
-            <= |  F	~x	~x   T	 T
-            """
-            if is_boolexpr(lhs) and is_num(rhs):
-                # direct simplification of boolean comparisons
-                if isinstance(rhs, BoolVal):
-                    rhs = int(rhs.value())
-                if rhs < 0:
-                    newlist.append(BoolVal(name in  {"!=", ">", ">="})) # all other operators evaluate to False
-                elif rhs == 0:
-                    if name == "!=" or name == ">":
-                        newlist.append(lhs)
-                    if name == "==" or name == "<=":
-                        newlist.append(cp.transformations.negation.recurse_negation(lhs))
-                    if name == "<":
-                        newlist.append(0 if num_context else BoolVal(False))
-                    if name == ">=":
-                        newlist.append(1 if num_context else BoolVal(True))
-                elif 0 < rhs < 1:
-                    # a floating point value
-                    if name == "==":
-                        newlist.append(0 if num_context else BoolVal(False))
-                    if name == "!=":
-                        newlist.append(1 if num_context else BoolVal(True))
-                    if name == "<" or name == "<=":
-                        newlist.append(cp.transformations.negation.recurse_negation(lhs))
-                    if name == ">" or name == ">=":
-                        newlist.append(lhs)
-                elif rhs == 1:
-                    if name == "==" or name == ">=":
-                        newlist.append(lhs)
-                    if name == "!=" or name == "<":
-                        newlist.append(cp.transformations.negation.recurse_negation(lhs))
-                    if name == ">":
-                        newlist.append(0 if num_context else BoolVal(False))
-                    if name == "<=":
-                        newlist.append(1 if num_context else BoolVal(True))
-                elif rhs > 1:
-                    newlist.append(BoolVal(name in {"!=", "<", "<="})) # all other operators evaluate to False
-            else:
-                res = eval_comparison(name, lhs, rhs)
-                if is_bool(res): # Result is a Boolean constant
-                    newlist.append(int(res) if num_context else BoolVal(res))
-                else: # Result is an expression
-                    newlist.append(res)    
-
-        elif isinstance(expr, (GlobalConstraint, GlobalFunction)):
-            newargs = simplify_boolean(expr.args) # TODO: how to determine which are Bool/int?
-            if any(a1 is not a2 for a1,a2 in zip(expr.args, newargs)):
-                expr = copy.copy(expr)
-                expr.update_args(newargs)
-            newlist.append(expr)
-        elif is_bool(expr):  # unlikely base-case (Boolean constant)
-            newlist.append(int(expr) if num_context else BoolVal(expr))
-        elif isinstance(expr, list):  # nested list in args (like for wsum)
-            newlist.append(simplify_boolean(expr))
-        else:  # variables/constants/direct constraints
+        changed, newexpr = _simplify_boolean_expr(expr, num_context=num_context)
+        if changed:
+            assert not isinstance(newexpr, int)
+            newlist.append(newexpr)
+        else:
             newlist.append(expr)
     return newlist
+
+
+def _simplify_boolean_expr(expr: Expression, num_context=False) -> tuple[bool, Expression|int]:
+    changed = False
+    if isinstance(expr, Operator):
+        args = expr.args
+        if expr.has_subexpr():
+            changed, rec_args = _simplify_boolean_args(args, num_context=not expr.is_bool())
+            if changed:
+                args = rec_args
+
+        if expr.name == "or":
+            newargs = None
+            for i, a in enumerate(args):
+                if is_true_cst(a):
+                    return True, BoolVal(True)
+                elif is_false_cst(a):
+                    changed = True
+                    if newargs is None:
+                        newargs = list(args[:i])
+                elif newargs is not None:
+                    newargs.append(a)
+
+            if changed:
+                if newargs is None:
+                    newargs = list(args)
+                newexpr = copy.copy(expr)
+                newexpr.update_args(newargs)
+                return True, newexpr
+            return False, expr
+
+        elif expr.name == "and":
+            newargs = None
+            for i, a in enumerate(args):
+                if is_false_cst(a):
+                    return True, BoolVal(False)
+                elif is_true_cst(a):
+                    changed = True
+                    if newargs is None:
+                        newargs = list(args[:i])
+                elif newargs is not None:
+                    newargs.append(a)
+
+            if changed:
+                if newargs is None:
+                    newargs = list(args)
+                newexpr = copy.copy(expr)
+                newexpr.update_args(newargs)
+                return True, newexpr
+            return False, expr
+
+        elif expr.name == "->":
+            cond, bool_expr = args
+            if is_false_cst(cond) or is_true_cst(bool_expr):
+                return True, BoolVal(True)
+            elif is_true_cst(cond):
+                if is_bool(bool_expr):
+                    return True, BoolVal(bool_expr)
+                return True, bool_expr
+            elif is_false_cst(bool_expr):
+                _, newcond = _simplify_boolean_expr(recurse_negation(cond), num_context=False)
+                # always changed, because negated cond
+                return True, newcond
+
+            if changed:
+                return True, cond.implies(bool_expr)
+            return False, expr
+
+        elif expr.name == "not":
+            if isinstance(args[0], _BoolVarImpl):
+                return True, ~args[0]
+            elif is_true_cst(args[0]):
+                return True, BoolVal(False)
+            elif is_false_cst(args[0]):
+                return True, BoolVal(True)
+
+            if changed:
+                newexpr = copy.copy(expr)
+                newexpr.update_args(args)
+                return True, newexpr
+            return False, expr
+
+        elif expr.name == "wsum":
+            weights, vars = args
+            newvars = None
+            for i, v in enumerate(vars):
+                if is_bool(v):
+                    if newvars is None:
+                        newvars = list(vars[:i])
+                    changed = True
+                    newvars.append(int(v))
+                elif newvars is not None:
+                    newvars.append(v)
+            if changed:
+                if newvars is not None:
+                    vars = newvars
+                newexpr = copy.copy(expr)
+                newexpr.update_args((weights, vars))
+                return True, newexpr
+            return False, expr
+
+        # numerical operators and default operator case:
+        # cast booleans to integers in numeric context
+        newargs = None
+        for i, a in enumerate(args):
+            if is_bool(a):
+                if newargs is None:
+                    newargs = list(args[:i])
+                changed = True
+                newargs.append(int(a))
+            elif newargs is not None:
+                newargs.append(a)
+        if changed:
+            if newargs is None:
+                newargs = list(args)
+            newexpr = copy.copy(expr)
+            newexpr.update_args(newargs)
+            return True, newexpr
+
+    elif isinstance(expr, Comparison):
+        changed, (lhs, rhs) = _simplify_boolean_args(expr.args, num_context=True)
+        name = expr.name
+        if is_num(lhs) and is_boolexpr(rhs):  # flip arguments of comparison to reduct nb of cases
+            if name == "<":    name = ">"
+            elif name == ">":  name = "<"
+            elif name == "<=": name = ">="
+            elif name == ">=": name = "<="
+            lhs, rhs = rhs, lhs
+            changed = True
+        """
+        Simplify expressions according to this table:
+        x  | <0	 0  0.n	 1	>1
+        ----------------------
+        == |  F	~x	 F   x	 F
+        != |  T	 x	 T  ~x	 T
+        >  |  T	 x	 x   F	 F
+        <  |  F	 F	~x  ~x	 T
+        >= |  T	 T	 x   x	 F   
+        <= |  F	~x	~x   T	 T
+        """
+        if isinstance(lhs, Expression) and lhs.is_bool() and is_num(rhs):
+            # direct simplification of boolean comparisons
+            if isinstance(rhs, BoolVal):
+                rhs = int(rhs.value())
+            if rhs < 0:
+                return True, BoolVal(name in  {"!=", ">", ">="}) # all other operators evaluate to False
+            elif rhs == 0:
+                if name == "!=" or name == ">":
+                    return True, lhs
+                if name == "==" or name == "<=":
+                    return True, recurse_negation(lhs)
+                if name == "<":
+                    return True, (0 if num_context else BoolVal(False))
+                if name == ">=":
+                    return True, (1 if num_context else BoolVal(True))
+            elif 0 < rhs < 1:
+                # a floating point value
+                if name == "==":
+                    return True, (0 if num_context else BoolVal(False))
+                if name == "!=":
+                    return True, (1 if num_context else BoolVal(True))
+                if name == "<" or name == "<=":
+                    return True, recurse_negation(lhs)
+                if name == ">" or name == ">=":
+                    return True, lhs
+            elif rhs == 1:
+                if name == "==" or name == ">=":
+                    return True, lhs
+                if name == "!=" or name == "<":
+                    return True, recurse_negation(lhs)
+                if name == ">":
+                    return True, (0 if num_context else BoolVal(False))
+                if name == "<=":
+                    return True, (1 if num_context else BoolVal(True))
+            elif rhs > 1:
+                return True, BoolVal(name in {"!=", "<", "<="}) # all other operators evaluate to False
+
+        # normalize comparison orientation to keep expression on lhs
+        if is_num(lhs) and isinstance(rhs, Expression):
+            if name == "<":
+                name = ">"
+            elif name == ">":
+                name = "<"
+            elif name == "<=":
+                name = ">="
+            elif name == ">=":
+                name = "<="
+            lhs, rhs = rhs, lhs
+            changed = True
+
+        if changed or (is_num(lhs) and is_num(rhs)):
+            newexpr = eval_comparison(name, lhs, rhs)
+            if is_bool(newexpr): # Result is a Boolean constant
+                return True, (int(newexpr) if num_context else BoolVal(newexpr))
+            else: # Result is an expression
+                return True, newexpr
+
+    elif isinstance(expr, (GlobalConstraint, GlobalFunction)):
+        rec_changed, rec_args = _simplify_boolean_args(expr.args, num_context=False)
+        if rec_changed:
+            newexpr = copy.copy(expr)
+            newexpr.update_args(rec_args)
+            return True, newexpr
+
+    return False, expr
+
+
+def _simplify_boolean_args(args: list[Any]|tuple[Any, ...], num_context=False) -> tuple[bool, list[Any]|tuple[Any, ...]]:
+    changed = False
+    newargs: list[Any] = []
+
+    for arg in args:
+        if isinstance(arg, Expression):
+            if isinstance(arg, BoolVal):
+                changed = True
+                newargs.append(int(arg.value()) if num_context else arg)
+                continue
+            rec_changed, rec_arg = _simplify_boolean_expr(arg, num_context=num_context)
+            if rec_changed:
+                changed = True
+                newargs.append(rec_arg)
+                continue
+        elif isinstance(arg, (list, tuple)):
+            rec_changed, rec_arg = _simplify_boolean_args(arg, num_context=num_context)
+            if rec_changed:
+                changed = True
+                newargs.append(rec_arg)
+                continue
+        elif is_bool(arg):
+            changed = True
+            newargs.append(int(arg) if num_context else BoolVal(arg))
+            continue
+
+        newargs.append(arg)
+
+    if changed:
+        return True, newargs
+    return False, args
