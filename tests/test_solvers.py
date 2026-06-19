ae16634b36810a86618ef84356670c3d897362aa
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2026-06-19 17:52:57 +0200

    cleanup merge artifacts

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -1,6 +1,5 @@
 import inspect
 import importlib
-import inspect
 
 import re
 import tempfile
@@ -1018,7 +1017,7 @@ class TestSupportedSolvers:
         solver_obj = cp.SolverLookup.get(solver)
         if "display" not in inspect.signature(solver_obj.solve).parameters:
             pytest.skip(f"{solver} does not support solution callbacking")
-        if solver == "z3":
+        if solver in ("z3", "hexaly"):
             kwargs['time_limit'] = 10 # z3 is too slow otherwise, cannot find more than one solution in time limit
 
 
@@ -1276,44 +1275,6 @@ def test_highs_basic_ilp():
     assert x.value() == 0
     assert y.value() == 5
     assert s.objective_value_ == 5
-
-
-@pytest.mark.requires_solver("gurobi", "cplex", "scip", "highs", "hexaly")
-class TestRound:
-    def test_gurobi_read_integers_issue_858(self, solver):
-        x = cp.intvar(1, 3, name="x")
-        p = cp.intvar(0, 1, shape=3, name="p")
-        q = cp.intvar(0, 1, shape=3, name="q")
-        m = cp.Model()
-        m += cp.sum([p[0], p[1], p[2]]) == 1
-        m += cp.sum([3, 3, 1, -1] * cp.cpm_array([q[0], q[1], q[2], x])) == 0
-        m += cp.sum([q[0], q[1], q[2]]) == 1
-
-        def check():
-            print(x, x.value())
-            assert (x.value() >= 1), f"{x}={x.value()}"
-
-        m.solveAll(solver=solver, solution_limit=1000, time_limit=10, display=check)
-
-
-
-@pytest.mark.requires_solver("gurobi", "cplex", "scip", "highs", "hexaly")
-class TestRound:
-    def test_gurobi_read_integers_issue_858(self, solver):
-        x = cp.intvar(1, 3, name="x")
-        p = cp.intvar(0, 1, shape=3, name="p")
-        q = cp.intvar(0, 1, shape=3, name="q")
-        m = cp.Model()
-        m += cp.sum([p[0], p[1], p[2]]) == 1
-        m += cp.sum([3, 3, 1, -1] * cp.cpm_array([q[0], q[1], q[2], x])) == 0
-        m += cp.sum([q[0], q[1], q[2]]) == 1
-
-        def check():
-            print(x, x.value())
-            assert (x.value() >= 1), f"{x}={x.value()}"
-
-        m.solveAll(solver=solver, solution_limit=1000, time_limit=10, display=check)
-
 
 
 @pytest.mark.requires_solver("gurobi", "cplex", "scip", "highs", "hexaly")
