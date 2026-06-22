8531aa6b681873f686858230aa1c313972d6615d
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2026-06-22 22:44:06 +0200

    remove unsupported global funcs from toplevel safening

--- cpmpy/solvers/pindakaas.py
+++ cpmpy/solvers/pindakaas.py
@@ -255,7 +255,7 @@ class CPM_pindakaas(SolverInterface):
 
     def transform(self, cpm_expr):
         cpm_cons = toplevel_list(cpm_expr)
-        cpm_cons = no_partial_functions(cpm_cons, safen_toplevel={"div", "mod", "element"})
+        cpm_cons = no_partial_functions(cpm_cons, safen_toplevel=frozenset())
         cpm_cons = push_down_negation(cpm_cons)
         cpm_cons = decompose_linear(
             cpm_cons,
--- cpmpy/solvers/pysat.py
+++ cpmpy/solvers/pysat.py
@@ -368,7 +368,7 @@ class CPM_pysat(SolverInterface):
             :return: list of Expression
         """
         cpm_cons = toplevel_list(cpm_expr)
-        cpm_cons = no_partial_functions(cpm_cons, safen_toplevel={"div", "mod", "element"})
+        cpm_cons = no_partial_functions(cpm_cons, safen_toplevel=frozenset())
         cpm_cons = push_down_negation(cpm_cons)
         cpm_cons = decompose_linear(
             cpm_cons,
