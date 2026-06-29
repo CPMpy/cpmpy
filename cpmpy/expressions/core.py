0d57f8947213406cbea58218e0eda05687cb8a10
Tias Guns <tias@ulyssis.org>
2026-06-29 11:34:57 +0200

    NestedBoolExprLike with ListLike

--- cpmpy/expressions/core.py
+++ cpmpy/expressions/core.py
@@ -105,7 +105,7 @@ T = TypeVar("T")
 ListLike: TypeAlias = Union[Sequence[T], np.ndarray]  # similar to is_any_list() check  (Sequence a bit more general than list/tuple)
 ExprLike: TypeAlias = Union["Expression", int, np.integer, np.bool_]  # expression or int (incl np variants, e.g. user facing)
 BoolExprLike: TypeAlias = Union["Expression", bool, np.bool_]  # subtype of ExprLike (bool subtype int)
-NestedBoolExprLike: TypeAlias = Union[BoolExprLike, Sequence["NestedBoolExprLike"], np.ndarray]  # nested version of ListLike[BoolExprLike]
+NestedBoolExprLike: TypeAlias = Union[BoolExprLike, ListLike["NestedBoolExprLike"]]  # single or (nested) ListLike of BoolExprLike
 
 class Expression(object):
     """
