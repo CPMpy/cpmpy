57ecec20c7d0eea5577ea0fa930b80874de2eddd
Tias Guns <tias@ulyssis.org>
2026-07-01 17:18:01 +0200

    Fix 'Expr OP NDArr' by doing 'NDArr ROP Expr'
    Fixes:
    ```
    >>> x * y
    NDVarArray([(a) * (y), (b) * (y), (c) * (y)], dtype=object)
    >>> y * x
    (y) * ([a b c])
    ```
    from #1034
    
    Curiously its actually a typing gap... that you can also get an
    expression as second argument.
    
    So with this now fixed, we might as well just go ahead and type all
    these operators...

--- cpmpy/expressions/core.py
+++ cpmpy/expressions/core.py
@@ -289,6 +289,8 @@ class Expression(object):
 
     # Comparisons
     def __eq__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__eq__(self)
         # BoolExpr == 1|true|0|false, common case, simply BoolExpr
         if self.is_bool() and is_num(other):
             if other is True or other == 1:
@@ -298,23 +300,35 @@ class Expression(object):
         return Comparison("==", self, other)
 
     def __ne__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__ne__(self)
         return Comparison("!=", self, other)
 
     def __lt__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__gt__(self)
         return Comparison("<", self, other)
 
     def __le__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__ge__(self)
         return Comparison("<=", self, other)
 
     def __gt__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__lt__(self)
         return Comparison(">", self, other)
 
     def __ge__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__le__(self)
         return Comparison(">=", self, other)
 
     # Boolean Operators
     # Implements bitwise operations & | ^ and ~ (and, or, xor, not)
     def __and__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__rand__(self)
         # some simple constant removal
         if is_true_cst(other):
             return self
@@ -325,6 +339,8 @@ class Expression(object):
         return Operator("and", [self, other])
 
     def __rand__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__and__(self)
         # some simple constant removal
         if is_true_cst(other):
             return self
@@ -335,6 +351,8 @@ class Expression(object):
         return Operator("and", [other, self])
 
     def __or__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__ror__(self)
         # some simple constant removal
         if is_false_cst(other):
             return self
@@ -345,6 +363,8 @@ class Expression(object):
         return Operator("or", [self, other])
 
     def __ror__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__or__(self)
         # some simple constant removal
         if is_false_cst(other):
             return self
@@ -355,6 +375,8 @@ class Expression(object):
         return Operator("or", [other, self])
 
     def __xor__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__rxor__(self)
         # some simple constant removal
         if is_true_cst(other):
             return ~self
@@ -363,6 +385,8 @@ class Expression(object):
         return cp.Xor([self, other])
 
     def __rxor__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__xor__(self)
         # some simple constant removal
         if is_true_cst(other):
             return ~self
@@ -373,23 +397,31 @@ class Expression(object):
     # Mathematical Operators, including 'r'everse if it exists
     # Addition
     def __add__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__radd__(self)
         if is_num(other) and other == 0:
             return self
         return Operator("sum", [self, other])
 
     def __radd__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__add__(self)
         if is_num(other) and other == 0:
             return self
         return Operator("sum", [other, self])
 
     # subtraction
     def __sub__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__rsub__(self)
         # if is_num(other) and other == 0:
         #     return self
         # return Operator("sub", [self, other])
         return self.__add__(-other)
 
     def __rsub__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__sub__(self)
         # if is_num(other) and other == 0:
         #     return -self
         # return Operator("sub", [other, self])
@@ -397,11 +429,15 @@ class Expression(object):
     
     # multiplication: use GlobalFunction Multiplication so it can be decomposed (e.g. to linear)
     def __mul__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__rmul__(self)
         if is_num(other) and other == 1:
             return self
         return cp.Multiplication(self, other)
 
     def __rmul__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__mul__(self)
         if is_num(other) and other == 1:
             return self
         return cp.Multiplication(other, self)
@@ -419,17 +455,25 @@ class Expression(object):
         return self.__rfloordiv__(other)
 
     def __floordiv__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__rfloordiv__(self)
         if is_num(other) and other == 1:
             return self
         return cp.Division(self, other)
 
     def __rfloordiv__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__floordiv__(self)
         return cp.Division(other, self)
 
     def __mod__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__rmod__(self)
         return cp.Modulo(self, other)
 
     def __rmod__(self, other):
+        if isinstance(other, cp.expressions.variables.NDVarArray):
+            return other.__mod__(self)
         return cp.Modulo(other, self)
 
     def __pow__(self, other: Any, modulo: Optional[int] = None):
