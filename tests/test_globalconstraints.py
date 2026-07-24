640ccdb3184bf0449ebad1a8a2bdda1f7ededb34
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2026-07-24 16:51:12 +0100

    tweaks

--- tests/test_globalconstraints.py
+++ tests/test_globalconstraints.py
@@ -1985,14 +1985,14 @@ def test_issue801_expr_in_cumulative(solver):
     bv = cp.boolvar(shape=3,name="bv")
 
     assert cp.Model(cp.NoOverlap(bv * start, dur, end)).solve(solver=solver)
-    if solver != "pumpkin": # Pumpkin does not support variables as duration
+    if solver not in ("pumpkin", "hermax"): #  only support integers as duration, not variables
         assert cp.Model(cp.NoOverlap(bv * start,bv * dur, end)).solve(solver=solver)
     assert cp.Model(cp.NoOverlap(bv * start, dur, bv * end)).solve(solver=solver)
 
     # also for cumulative
     assert cp.Model(cp.Cumulative(bv * start, dur, end,1, 3)).solve(solver=solver)
     assert cp.Model(cp.Cumulative(bv * start, dur, bv * end, 1, 3)).solve(solver=solver)
-    if solver != "pumpkin": # Pumpkin does not support variables as duration, demand or capacity
+    if solver not in ("pumpkin", "hermax"): # only support integers as duration, demand or capacity, not variables
         assert cp.Model(cp.Cumulative(bv * start,bv * dur, end, 1, 3)).solve(solver=solver)
         assert cp.Model(cp.Cumulative(bv * start, dur, end, bv * [2, 3, 4], 3 * bv[0])).solve(solver=solver)
         assert cp.Model(cp.Cumulative(bv * start, dur, end, 1, 3 * bv[0])).solve(solver=solver)
