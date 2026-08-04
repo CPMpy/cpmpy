a446a0c330f1b53d9fa3afea9c1ce2f59c648201
OrestisLomis <orestis.lomisg7@gmail.com>
2026-08-04 13:13:05 +0200

    address comments

--- examples/advanced/hyperparameter_search.py
+++ examples/advanced/hyperparameter_search.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
 """
 Example of gridsearch over solver parameters
 
 Generally applicable, and demonstrated on n-queens with ortools
 """
 import cpmpy as cp
-from cpmpy.tools.tune_solver import ParameterTuner, GridSearchTuner
+from cpmpy.tools.tune_solver import ParameterTuner
+import numpy as np
 
 def main():
     model = nqueens(n=125)
@@ -16,14 +18,14 @@ def main():
         'symmetry_level': [0, 1, 2],
     }
     
-    # Defaults are required if all_params is custom
+    # defaults are required if all_params is custom
     defaults = {
         'cp_model_probing_level': 2,
         'linearization_level': 1,
         'symmetry_level': 2
     }
 
-    # initialize tuner; can use ParameterTuner (SMBO-based) or GridSearchTuner (Exhaustive)
+    # initialize tuner instance
     tuner = ParameterTuner(
         solvername="ortools", 
         model=model, 
@@ -31,18 +33,8 @@ def main():
         defaults=defaults
     )
 
-    # Uncomment to use GridSearchTuner instead of ParameterTuner
-    # tuner = GridSearchTuner(
-    #     solvername="ortools", 
-    #     model=model, 
-    #     all_params=all_params, 
-    #     defaults=defaults
-    # )
-
     
-    # 4. Run Tuning
-    # time_limit: total budget for the whole process
-    # max_tries: limit number of configs to test
+    # run tuner with 60s total budget
     best_config = tuner.tune(time_limit=60, verbose=1)
 
     print("\n--- Tuning Results ---")
@@ -56,8 +48,8 @@ def nqueens(n=8):
     queens = cp.intvar(1, n, shape=n)
     return cp.Model(
         cp.AllDifferent(queens),
-        cp.AllDifferent([queens[i] + i for i in range(n)]),
-        cp.AllDifferent([queens[i] - i for i in range(n)]),
+        cp.AllDifferent(queens + np.arange(n)),
+        cp.AllDifferent(queens - np.arange(n)),
     )
 
 
