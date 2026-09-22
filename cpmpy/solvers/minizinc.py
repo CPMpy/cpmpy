d367b3bb12298c12dec8599282c50bbd135230ab
Ignace Bleukx <ignace.bleukx@kuleuven.be>
2026-09-22 09:45:41 +0200

    fix multiplication brackets

--- cpmpy/solvers/minizinc.py
+++ cpmpy/solvers/minizinc.py
@@ -906,9 +906,9 @@ class CPM_minizinc(SolverInterface):
         elif expr.name == "mul":
             assert isinstance(expr, Multiplication)
             if expr.is_lhs_num:
-                return "{}*({})".format(args_str[1], args_str[0])
+                return "{}*({})".format(args_str[0], args_str[1])
             else:
-                return "({}) * ({})".format(args_str[1], args_str[0])
+                return "({}) * ({})".format(args_str[0], args_str[1])
 
         elif expr.name == "count":
             vars, val = expr.args
